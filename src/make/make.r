812880e8583b22d1013777bf4302489dbb4026a0
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-05 14:44:20 +0000

    better make

--- src/make/make.r
+++ src/make/make.r
@@ -11,35 +11,39 @@ source("src/files_rd.r")
 make <- function(only.allowed.rds=TRUE, build=TRUE, check=TRUE, binary=FALSE) {
 	src.dir  <<- file.path(project.dir, "src")
 	pkg.dir  <<- file.path(project.dir, "pkg")
-	skel.dir  <<- file.path(pkg.dir, "mlr.skel")
+	
 	build.dir  <<- file.path(pkg.dir, "mlr")
-	r.skel.dir    <<- file.path(skel.dir, "R") 
+	rox.dir  <<- file.path(pkg.dir, "mlr.roxygen")
 	r.build.dir    <<- file.path(build.dir, "R") 
-	man.skel.dir  <<- file.path(skel.dir, "man") 
+	r.rox.dir    <<- file.path(rox.dir, "R") 
 	man.build.dir  <<- file.path(build.dir, "man") 
+	man.rox.dir  <<- file.path(rox.dir, "man") 
 	#html.dir <- file.path(project.dir, "html") 
 	
 	cat("Building mlr to :", pkg.dir, "...\n")
 	cat("Clean up:\n")
 	
-	if( unlink(file.path(r.skel.dir, list.files(r.skel.dir))) != 0) 
+	if( unlink(file.path(r.build.dir, list.files(r.build.dir))) != 0) 
 		stop("could not delete r dir!")		
-	if( unlink(file.path(man.skel.dir, list.files(man.skel.dir))) != 0) 
-		stop("could not delete man dir!")		
 	if( unlink(file.path(man.build.dir, list.files(man.build.dir))) != 0) 
 		stop("could not delete man dir!")		
 	
 	code.files <- file.path(src.dir, c(base.files, classif.files, regr.files))
 	
-	cat("Build skeleton for:\n")
-	print(code.files)
-	package.skeleton("mlr.skel", code_files=code.files, path = pkg.dir, namespace=FALSE, force=TRUE)
+	package.skeleton("mlr", code_files=code.files, path = pkg.dir, namespace=FALSE, force=TRUE)
 	
-	desc.file <- file.path(skel.dir, "DESCRIPTION")
+	desc.file <- file.path(build.dir, "DESCRIPTION")
 	rev.nr <- get.rev.nr()
 	write.desc(desc.file, rev.nr)
+
+	roxygenize(package.dir=build.dir, use.Rd2=TRUE, unlink.target=TRUE)
+
+
+	file.copy(from=man.rox.dir, to=build.dir, recursive = TRUE) 
+	file.copy(from=file.path(rox.dir, "NAMESPACE"), to=build.dir, overwrite = TRUE) 
+	file.copy(from=file.path(rox.dir, "DESCRIPTION"), to=build.dir, overwrite = TRUE) 
 	
-	roxygenize(package.dir=skel.dir, roxygen.dir=build.dir, use.Rd2=TRUE, copy.package=FALSE)
+	
 	
 	if (only.allowed.rds) {
 		rds <- list.files(man.build.dir)
@@ -49,7 +53,6 @@ make <- function(only.allowed.rds=TRUE, build=TRUE, check=TRUE, binary=FALSE) {
 			}
 		}
 	}	
-	file.copy(from=code.files, to=r.build.dir, overwrite = TRUE) 
 	
 #rds <- "*.Rd"
 #wd <- getwd()
