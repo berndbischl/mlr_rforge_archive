169166ca5200ac96b7b8b4fa3d2016763af46a38
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-05 15:59:33 +0000

    better test script

--- src/test.r
+++ src/test.r
@@ -1,92 +1,29 @@
-library(RUnit)
-library(MASS)
-library(rpart)
-library(e1071)
-library(boot)
-library(roxygen)
-library(kernlab)
-library(adabag)
-library(kknn)
-library(randomForest)
-library(mda)
-library(mlbench)
-library(reshape)
-library(klaR)
-library(snowfall)
 
-source("src/testsuite.config.r")
 
-source("src/log/log.r")
-
-source("src/base/learner.props.r")
-source("src/base/learner.props.classif.r")
-source("src/base/learner.props.regr.r")
-source("src/base/data.desc.r")
-
-source("src/base/wrapped.learner.r")
-source("src/base/wrapped.learner.classif.r")
-source("src/base/wrapped.learner.regr.r")
-
-source("src/base/task.learn.r")
-source("src/base/task.classif.r")
-source("src/base/task.classif.make.r")
-source("src/base/task.regr.r")
-source("src/base/task.regr.make.r")
-
-source("src/base/check.task.classif.r")
-
-source("src/base/wrapped.model.r")
-source("src/base/learner.failure.r")
-
-source("src/base/train.learner.r")
-source("src/base/train.task.r")
-source("src/base/train.task.classif.r")
-source("src/base/train.task.regr.r")
-
-source("src/base/predict.classif.r")
-source("src/base/predict.regr.r")
-source("src/base/conf.matrix.r")
-
-source("src/base/prediction.r")
-source("src/base/perf.measure.r")
-
-source("src/base/resample.desc.r")
-source("src/base/cv.desc.r")
-source("src/base/bs.desc.r")
-source("src/base/subsample.desc.r")
-source("src/base/resample.instance.r")
-source("src/base/cv.instance.r")
-source("src/base/bs.instance.r")
-source("src/base/subsample.instance.r")
-
-source("src/base/resample.result.r")
-source("src/base/resample.fit.r")
-source("src/base/resample.fit.iter.r")
-source("src/base/resample.performance.r")
-
-source("src/base/tune.r")
-
-source("src/base/parallel.r")
-
-source("src/models/classification/lda.r")
-source("src/models/classification/rpart.r")
-source("src/models/classification/svm.r")
-source("src/models/regression/gbm.r")
-source("src/models/regression/kknn.r")
+source("src/files.r")
+load.all.libs()
+load.all.sources("src")
 
 logger.define(level="error", global=T)
 
 parallel.setup(global=TRUE)
 
-data(BostonHousing)
-rt <- make.regr.task("kknn.regr", data=BostonHousing, formula=medv~.)
-rin <- make.cv.instance(iters=10, size=nrow(BostonHousing))
-rr <- resample.fit(rt, rin)
-print(rr)
+data(iris)
 
-print(resample.performance(rt,rin, rr, measure="mse"))
-tr <- tune(rt, rin, ranges=list(k=2:5), measure="mse")
-print(tr)
+# set up the learning task and parameter grid
+ct1 <- make.classif.task("kernlab.svm.classif", data=iris, formula=Species~.)
+ranges1 <- list(C=1:5)
+rin <- make.cv.instance(iters=10, size=nrow(iris))					
+rdesc <- make.cv.desc(iters=3)
+br <- benchmark(learn.task=ct1, ranges=ranges1, rin, rdesc)
+
+# result
+
+#print(rr)
+
+#print(resample.performance(rt,rin, rr, measure="mse"))
+#tr <- tune(rt, rin, ranges=list(k=2:5), measure="mse")
+#print(tr)
 
 #print(tr)
 
