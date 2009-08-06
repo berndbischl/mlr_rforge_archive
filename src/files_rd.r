5d20c58b3b0d14138f28bd10d6e64973cf86647b
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-06 12:15:59 +0000

    allowed rds

--- src/files_rd.r
+++ src/files_rd.r
@@ -1,14 +1,17 @@
 allowed.rd.files <<- c( 		
+		"learn.task.Rd",
+		
 		"resample.desc-class.Rd",
-		"tune.Rd",
+		"resample.instance-class.Rd",
 		"cv.instance-class.Rd",
 		"bs.instance-class.Rd",
 		"subsample.instance-class.Rd",
 		"make.cv.instance.Rd",
 		"make.bs.instance.Rd",
 		"make.subsample.instance.Rd",
+		
 		"resample.fit.Rd",
-		"learn.task.Rd"
+		"tune.Rd"
 )		
 
 	
