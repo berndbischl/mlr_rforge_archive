1f4c910c14dee7df806405bdf8bc0cb1cb135f45
borg <borg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-06 12:52:44 +0000

    docs

--- src/base/task.classif.make.r
+++ src/base/task.classif.make.r
@@ -12,7 +12,43 @@ setGeneric(
 		}
 )
 
+
+#' \code{make.classif.task} defines a classification task for a learner and a data set and is the starting point 
+#' for further steps like training, testing, prediciting.
+#' 
+#' \code{make.classif.task} already performs quite a few tasks: It tries to load required package for the 
+#' learner, sets up the learner to deal with a classification problem, gathers information about the features 
+#' of the data set and the method, and compares whether they are compatible 
+#' (e.g. some methods might not handle NAs or facors). And last but not least it converts integers to numerics 
+#' and warns about this. 
+#' 
+#' @param learner [character] \cr 	Specifies the learner. The naming convention is to add the sufixes ".classif" 
+#' 									(or ".regr") to the learner name, if it can handle both types. 
+#' @param formula [formula] \cr		a symbolic description of the model to be fitted, see \link{formula}.
+#' @param data [data.frame] \cr 	an optional data frame containing the variables in the model.
+#' @param weights [numeric] \cr 	an optional vector of weights to be used in the fitting process. Should be NULL or a numeric vector.
+#' @param type [character] \cr 		specifies the type of the predicitons - either probability ("probs") 
+#' 									or class ("class").
+#' 
+
+#' 
+#' @return An object of class \code{\linkS4class{classif.task}}.
+#' 
 #' @export
+#' @rdname make.classif.task
+#' 
+#' @usage make.classif.task(learner, formula, data, weights, type)
+#'
+#' @examples
+#' data(iris) 
+#' # define a classification task for a decision tree (rpart) for the data set iris
+#' ct <- make.classif.task("rpart.classif", data = iris, formula = Species ~.)
+#' 
+#' @seealso \linkS4class{wrapped.learner}, \linkS4class{classif.task}, \link{train}, \link{predict}
+#'  
+#' @title make.classif.task
+
+
 setMethod(
 		f = "make.classif.task",
 		signature = signature(
--- src/base/task.regr.make.r
+++ src/base/task.regr.make.r
@@ -9,7 +9,39 @@ setGeneric(
 		}
 )
 
+#' \code{make.regr.task} defines a regression task for a learner and a data set and is the starting point 
+#' for further steps like training, testing, prediciting.
+#' 
+#' \code{make.regr.task} already performs quite a few tasks: It tries to load required package for the 
+#' learner, sets up the learner to deal with a regression problem, gathers information about the features 
+#' of the data set and the method, and compares whether they are compatible 
+#' (e.g. some methods might not handle NAs or facors). And last but not least it converts integers to numerics 
+#' and warns about this. 
+#' 
+#' @param learner [character] \cr 	Specifies the learner. The naming convention is to add the sufixes ".regr" 
+#' 									(or ".classif") to the learner name, if it can handle both types. 
+#' @param formula [formula] \cr		a symbolic description of the model to be fitted, see \link{formula}.
+#' @param data [data.frame] \cr 	an optional data frame containing the variables in the model.
+#' @param weights [numeric] \cr 	an optional vector of weights to be used in the fitting process. Should be NULL or a numeric vector.
+#' 
+#' @return An object of class \code{\linkS4class{regr.task}}.
+#' 
 #' @export
+#' @rdname make.regr.task
+#' 
+#' @usage make.regr.task(learner, formula, data, weights)
+#'
+#' @examples
+#' library(mlbench)
+#' data(BostonHousing)
+#' # define a regression task for a Gradient Boosting Machine for regression for the data set BostonHousing
+#' rt <- make.regr.task("gbm.regr", data = BostonHousing, formula = medv~.)
+#' 
+#' @seealso \linkS4class{wrapped.learner}, \linkS4class{regr.task}, \link{train}, \link{predict}
+#' 
+#' @title make.regr.task
+
+
 setMethod(
 		f = "make.regr.task",
 		signature = signature(
