2b97f815fb0c3e2b11b04aa7aa45bb329dcfab93
borg <borg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-20 12:00:59 +0000

    test for prep.data

---
+++ src/runit/runit.prepare.r
@@ -0,0 +1,25 @@
+ints <- as.integer(c(1,2,3))
+chars <- c("bli", "bla", "blub")
+
+test.dataset <- data.frame(ints,chars)
+
+
+test.prepare <- function(){ 
+		data = test.dataset
+		desc1 <- make.data.desc(data = test.dataset, target.col = "ints")
+		desc2 <- make.data.desc(data = test.dataset, target.col = "chars")
+
+		ints2nums <- prep.data(data = test.dataset, target.col = "ints", data.desc = desc1, 
+				ints.as.nums = TRUE, ints.as.factors=FALSE, chars.as.factors = TRUE)
+		checkTrue(is.numeric(ints2nums[,1]))
+		
+#		ints2factors <- prep.data(data = test.dataset, target.col = "ints", data.desc = desc1, 
+#				ints.as.nums = FALSE, ints.as.factors=TRUE, chars.as.factors = TRUE)
+#		print(str(ints2factors[,1]))
+#		checkTrue(is.factor(ints2factors[,1]))
+		
+		chars2factors <- prep.data(data = test.dataset, target.col = "chars", data.desc = desc2, 
+				ints.as.nums = FALSE, ints.as.factors=TRUE, chars.as.factors = TRUE)
+		checkTrue(is.factor(chars2factors[,1]))
+
+}
