70916d079e5270cc016c79116fd8236dbe28bcf8
borg <borg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-20 10:19:00 +0000

    uncomment lda-test

--- src/runit/runit.learner.classif.lda.r
+++ src/runit/runit.learner.classif.lda.r
@@ -7,11 +7,11 @@ test.lda <- function() {
   p <- predict(m, newdata=multiclass.test)
 
   simple.test("lda", multiclass.df, multiclass.formula, multiclass.train.inds, p$class)
-#  prob.test  ("lda", multiclass.df, multiclass.formula, multiclass.train.inds, p$posterior)
-#  
-#  tt <- "lda"
-#  tp <- function(model, newdata) predict(model, newdata)$class
-#  
-#  cv.test("lda", multiclass.df, multiclass.formula, tune.train=tt, tune.predict=tp )
+  prob.test  ("lda", multiclass.df, multiclass.formula, multiclass.train.inds, p$posterior)
+  
+  tt <- "lda"
+  tp <- function(model, newdata) predict(model, newdata)$class
+  
+  cv.test("lda", multiclass.df, multiclass.formula, tune.train=tt, tune.predict=tp )
 
 }
