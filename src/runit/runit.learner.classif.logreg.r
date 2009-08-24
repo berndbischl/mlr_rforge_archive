05079a93db8ffadcae5951a00cb37fe5b274ed64
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-24 21:09:49 +0000

    tests

--- src/runit/runit.learner.classif.logreg.r
+++ src/runit/runit.learner.classif.logreg.r
@@ -1,18 +1,22 @@
 test.logreg <- function(){
 	
 	# "did not converge":
-	m <- glm(formula=binaryclass.formula, data=binaryclass.train, family=binomial, maxit=100) 
+	m <- glm(formula=binaryclass.formula, data=binaryclass.train, family=binomial, maxit=500) 
 	
 	p <- predict(m, newdata=binaryclass.test, type="response")
 	
 	p.class <- as.factor(binaryclass.class.levs[ifelse(p > 0.5, 2, 1)])
 	
 	simple.test("logreg", binaryclass.df, binaryclass.formula, binaryclass.train.inds, p.class)
-	
-#	prob.test  ("logreg", binaryclass.df, binaryclass.formula, binaryclass.train.inds, p)
 
-	tt <- "logreg"
-	tp <- function(model, newdata) predict(model, newdata)$class
+	# not done, annoying to build matrix manually
+	#prob.test("logreg", binaryclass.df, binaryclass.formula, binaryclass.train.inds, p)
+
+	tt <- function(formula, data) {glm(formula, data=data, family=binomial, maxit=500)}
+	tp <- function(model, newdata) {
+		p <- predict(model, newdata)
+		as.factor(binaryclass.class.levs[ifelse(p > 0.5, 2, 1)])
+	}
 
 	cv.test("logreg", binaryclass.df, binaryclass.formula, tune.train=tt, tune.predict=tp )
 }
