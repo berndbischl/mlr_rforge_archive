d35b510a468477d34f6bd221d5e77c7f3b7908ec
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-09 12:38:49 +0000

    novars model

--- src/base/novars.model.r
+++ src/base/novars.model.r
@@ -1,17 +1,19 @@
-#' Description class for cross-validation.
-#' @exportClass cv.desc
-#' @title cv.desc
-#' @seealso \code{\link{make.cv.desc}}
-
+#' Abstract base class for a model without variables.
+#' @param targets Observed target values in training set. Used to base predictions on. 
 setClass("novars.model", 
 		representation = representation(
 				targets = "ANY"
 		)
 )                                                     
 
-#' Create description object for cross-validation.
-#' @param iters Number of iterations
 
+#' Model without variables for classification.
+setClass("novars.model.classif", 
+		contains = c("novars.model")
+)                                                     
+
+
+#' Constructor.
 setMethod(
 		f = "initialize",
 		signature = signature("novars.model.classif"),
@@ -21,19 +23,48 @@ setMethod(
 		}
 )
 
-setClass("novars.model.classif", 
+
+#' Predict from model without variables for classification.
+#' Either predict classes randomly weighted by class frequencies in the training set or return these frequencies
+#' as estimated probabilities.  
+setMethod(
+		f = "predict",
+		signature = signature(object="novars.model.classif"),
+		def = function(object, newdata, type="class") {
+			probs <- as.numeric(table(object@targets)) / length(object@targets)
+			if (type == "probs")
+				return(probs)
+			sample(levels(object@targets), nrow(newdata), replace=TRUE, prob=probs)
+		}
+)
+
+#' Model without variables for regression.
+setClass("novars.model.regr", 
 		contains = c("novars.model")
 )                                                     
 
 
+#' Constructor.
 setMethod(
-		f = "predict",
-		signature = signature(object="novars.model.classif"),
-		def = function(object, newdata, type="default") {
-			probs <- as.numeric(table(object@targets)) / length(object@targets)
-			sample(levels(object@targets), nrow(newdata), replace=TRUE, prob=probs)
+		f = "initialize",
+		signature = signature("novars.model.regr"),
+		def = function(.Object, targets) {
+			.Object@targets <- targets 
+			return(.Object)
 		}
 )
+
+
+#' Predict constant from model without variables for regression.
+#' At the moment only the mean is implemented.
+setMethod(
+		f = "predict",
+		signature = signature(object="novars.model.regr"),
+		def = function(object, newdata) {
+			mean(targets)
+		}
+)
+
 
 
 
