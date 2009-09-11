9c0d08b2171b9390893203df061f3635b90f50d0
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-11 09:46:16 +0000

    adding balanced error rate as perf measure

--- src/base/perf.measure.r
+++ src/base/perf.measure.r
@@ -29,25 +29,27 @@ make.measure <- function(name, aggregate=NULL, aggr.name="aggr", spread=NULL, sp
 	if (name=="sae") {
 		ms <- list(fun = function(true.y, pred.y, weights) sum(abs(true.y - pred.y)),
 				aggregate = sum, aggr.name="sum", spread=IQR, spread.name="IQR", minimize=TRUE)
-	}
-	else if (name=="mae") {
+	} else if (name=="mae") {
 		ms <- list(fun = function(true.y, pred.y, weights) median(abs(true.y - pred.y)),
 				aggregate = median, aggr.name="median", spread=IQR, spread.name="IQR", minimize=TRUE)
-	}
-	else if (name=="sse") {
+	} else if (name=="sse") {
 		ms <- list(fun = function(true.y, pred.y, weights) sum((true.y - pred.y)^2),
 				aggregate = sum, aggr.name="sum", spread=sd, spread.name="sd", minimize=TRUE)		
-	}
-	else if (name=="mse") {
+	} else if (name=="mse") {
 		ms <- list(fun = function(true.y, pred.y, weights) mean((true.y - pred.y)^2),
 				aggregate = mean, aggr.name="mean", spread=sd, spread.name="sd", minimize=TRUE)		
-	}
-	else if (name=="smce") {
+	} else if (name=="smce") {
 		ms <- list(fun = function(true.y, pred.y, weights) sum(true.y != pred.y),
 				aggregate = sum, aggr.name="sum", spread=IQR, spread.name="IQR", minimize=TRUE)
-	}
-	else if (name=="mmce") {
+	} else if (name=="mmce") {
 		ms <- list(fun = function(true.y, pred.y, weights) mean(true.y != pred.y),
+				aggregate = mean, aggr.name="mean", spread=sd, spread.name="sd", minimize=TRUE)
+	} else if (name=="mber") {
+		ms <- list(fun = function(true.y, pred.y, weights) {
+					em <- errormatrix(true.y, pred.y, realtive=TRUE)
+					n <- ncol(em)
+					return(sum(em[1:(n-1), n]))
+				},
 				aggregate = mean, aggr.name="mean", spread=sd, spread.name="sd", minimize=TRUE)
 	} else {
 		stop(paste("Measure", name, "does not exist!"))
