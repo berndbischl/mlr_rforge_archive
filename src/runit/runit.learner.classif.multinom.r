0ca8a3ec5a0234a066b1b6f26fe1f45491ad0141
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-02 17:24:19 +0000

    better tests

--- src/runit/runit.learner.classif.multinom.r
+++ src/runit/runit.learner.classif.multinom.r
@@ -2,11 +2,17 @@
 
 test.multinom <- function() {
 	
-	m <- multinom(formula = multiclass.formula, data = multiclass.train)
+	set.seed(debug.seed)
+	capture.output(
+			m <- multinom(formula = multiclass.formula, data = multiclass.train)
+	)
+			
+	set.seed(debug.seed)
 	p <- predict(m, newdata=multiclass.test)
 	
 	simple.test("nnet.multinom", multiclass.df, multiclass.formula, multiclass.train.inds, p)
 	
+	set.seed(debug.seed)
 	p <- predict(m, newdata=multiclass.test, type="probs")
 	prob.test  ("nnet.multinom", multiclass.df, multiclass.formula, multiclass.train.inds, p)
 	
