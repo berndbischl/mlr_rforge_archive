0690e6230e8869b4b93bb591bb93bdf5f2c5c5a2
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-02 16:47:40 +0000

    not maxit in log reg test

--- src/runit/runit.learner.classif.logreg.r
+++ src/runit/runit.learner.classif.logreg.r
@@ -1,7 +1,7 @@
 test.logreg <- function(){
 	
 	# "did not converge":
-	m <- glm(formula=binaryclass.formula, data=binaryclass.train, family=binomial, maxit=500) 
+	m <- glm(formula=binaryclass.formula, data=binaryclass.train, family=binomial) 
 	
 	p <- predict(m, newdata=binaryclass.test, type="response")
 	
@@ -12,7 +12,7 @@ test.logreg <- function(){
 	# not done, annoying to build matrix manually
 	#prob.test("logreg", binaryclass.df, binaryclass.formula, binaryclass.train.inds, p)
 
-	tt <- function(formula, data) {glm(formula, data=data, family=binomial, maxit=500)}
+	tt <- function(formula, data) {glm(formula, data=data, family=binomial)}
 	tp <- function(model, newdata) {
 		p <- predict(model, newdata, type="response")
 		as.factor(binaryclass.class.levs[ifelse(p > 0.5, 2, 1)])
