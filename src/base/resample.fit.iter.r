94eea215b7742729239c3b7451c2f4798a3f6a4a
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-03 11:22:30 +0000

    new learner interface

--- src/base/resample.fit.iter.r
+++ src/base/resample.fit.iter.r
@@ -5,9 +5,9 @@ resample.fit.iter <- function(learn.task, resample.instance, parset, vars, type,
 	test.i <- resample.instance["test.inds", i]
 	m <- train(learn.task, subset=train.i, parset=parset, vars=vars)
 	if (is(learn.task, "classif.task"))
-		p <- predict(learn.task, m, newdata=learn.task@data[test.i,], type=type)
+		p <- predict(m, newdata=learn.task@data[test.i,], type=type)
 	else 
-		p <- predict(learn.task, m, newdata=learn.task@data[test.i,])
+		p <- predict(m, newdata=learn.task@data[test.i,])
 	# faster for parallel 
 	if (!return.model)
 		m <- NULL		
