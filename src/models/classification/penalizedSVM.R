0c3f393c4a2ca185b0088ee71d51c17b8e5a6048
"Stefan" <"Stefan"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-17 14:15:41 +0000

    Erster Versuch, penalized SVM zu implementieren

---
+++ src/models/classification/penalizedSVM.R
@@ -0,0 +1,80 @@
+#' @include wrapped.learner.classif.r
+#' @include train.learner.r
+roxygen()
+
+#' Wrapped learner for penalized Support Vector Machines from package \code{penalizedSVM} for classification problems.
+#' 
+#' \emph{General hyperparameters:}
+#' \describe{
+#' 		\item{\code{lambda1.set}}{set of tuning parameters lambda1}
+#' 		\item{\code{fs.method}}{feature selection method. Availible 'scad' and '1norm'}
+#' 		\item{\code{cross.outer}}{fold of outer cross validation, default is 0, no cv.}
+#' 		\item{\code{nu}}{nu: weighted parameter}
+#' 		\item{\code{calc.class.weights}}{calculate class.weights for SVM, default: FALSE}
+#'   	\item{\code{output}}{0 - no output, 1 - produce output, default is 0}
+#'   	\item{\code{seed}}{seed}
+#' }
+
+#' The kernel type and the hyperparameters are specified in \code{parset}. 
+#' @title kernlab.svm.classif
+#' @seealso \code{\link[penalizedSVM]{ksvm}}, \code{\link[penalizedSVM]{dots}}
+#' @export
+setClass(
+		"svm.fs", 
+		contains = c("wrapped.learner.classif")
+)
+#----------------- constructor ---------------------------------------------------------
+#' Constructor.
+#' @title penalized SVM Constructor
+setMethod(
+		f = "initialize",
+		signature = signature("svm.fs.classif"),
+		def = function(.Object) {
+			
+			desc = new("classif.props",
+					supports.multiclass = TRUE,
+					supports.missing = FALSE,
+					supports.numerics = TRUE,
+					supports.factors = TRUE,
+					supports.characters = TRUE,
+					supports.probs = TRUE, 
+					supports.weights = TRUE,	
+					supports.costs = FALSE 
+			)
+			
+			callNextMethod(.Object, learner.name="svm.fs", learner.pack="penalizedSVM", learner.props=desc)
+		}
+
+setMethod(
+		f = "train.learner",
+		signature = signature(
+				.wrapped.learner="svm.fs.classif", 
+				.targetvar="character", 
+				.data="data.frame", 
+				.weights="numeric", 
+				.costs="matrix", 
+				.type = "character" 
+		),
+		def = function(.wrapped.learner, .targetvar, .data, .weights, .costs, .type,  ...) {
+			f = as.formula(paste(.targetvar, "~."))
+			kpar = list()
+			args = list(...)
+			args.names <- names(args)
+		
+		}
+)
+
+setMethod(
+		f = "predict.learner",
+		signature = signature(
+				.wrapped.learner = "svm.fs.classif", 
+				.wrapped.model = "wrapped.model", 
+				.newdata = "data.frame", 
+				.type = "character" 
+		),
+		
+		def = function(.wrapped.learner, .wrapped.model, .newdata, .type, ...) {
+			.type <- ifelse(.type=="class", "response", "probabilities")
+			predict(.wrapped.model["learner.model"], newdata=.newdata, type=.type, ...)
+		}
+)	
