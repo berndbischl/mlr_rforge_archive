d24aed02413cccae9d1669f2bd0c83d544c735f7
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-15 18:49:12 +0000

    test as.char

--- src/runit/runit.aschar.r
+++ src/runit/runit.aschar.r
@@ -1,6 +1,11 @@
 test.aschar <- function() {
 
-	ct <- make.classif.task(data=binaryclass, target=binarclass.target)
-	print(ct)
+	ct <- make.classif.task(data=binaryclass.df, target=binaryclass.target)
+	as.character(ct)
 	
+	rt <- make.regr.task(data=regr.df, target=regr.target)
+	as.character(rt)
+	
+	wl = make.learner("lda")
+	as.character(wl)
 }
