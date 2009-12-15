7dc3a640473c3b33806021da09c8804ebf1b5be0
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-15 18:45:36 +0000

    check column names for special chars

--- src/base/check.task.r
+++ src/base/check.task.r
@@ -5,5 +5,11 @@ check.task <- function(data, target) {
 	if (!(target %in% cns)) {
 		msg <- paste("Column names of data.frame don't contain target var: ", target)
 	}
+	
+	forbidden = c("[", "]", "(", ")")
+	i = sapply(forbidden, function(x) length(grep(x, cns, fixed=T)) > 0)
+	if (any(i))
+		msg <- paste("Column names should not contain: ", paste(forbidden, collapse=" "))
+	
 	return(msg)
 }
