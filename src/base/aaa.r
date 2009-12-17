52e7c1f8164ae2a176e0aaf58eabc9eea1acbd21
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-17 16:53:34 +0000

    startup message

--- src/base/aaa.r
+++ src/base/aaa.r
@@ -8,4 +8,5 @@ roxygen <- function() NULL
 .onLoad <- function(libname, pkgname) {
 	logger.setup(level="warn")
 	parallel.setup(mode="local")
+	packageStartupMessage("Loading package mlr. Version: ", packageDescription("mlr", fields="Version"))
 }
