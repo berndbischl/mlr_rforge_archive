8fe6c45dec034c9fd63c48f7f2759b211d2e276e
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-17 15:32:49 +0000

    control methods for ps and grid search

---
+++ src/base/tune/control.grid.r
@@ -0,0 +1,14 @@
+#' Control structure for grid search tuning. 
+#' 
+#' @param ranges [\code{\link{list}}] \cr 
+#' 		A list of named vectors/lists of possible values for each hyperparameter. 
+#'      You can also pass a list of such ranges by using [\code{\link{combine.ranges}}] 
+#'      in the rare case when it does not make sense to search a complete cross-product of range values.     
+#' 		    
+#' @return Control structure for tuning.
+#' @export 
+#' @title grid.control 
+
+grid.control <- function(ranges) {
+	list(ranges=ranges)	
+}
