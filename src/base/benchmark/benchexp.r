b5b38af3b531fad8da819653ead36559f6e057a3
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-17 15:43:00 +0000

    docs

--- src/base/benchmark/benchexp.r
+++ src/base/benchmark/benchexp.r
@@ -1,9 +1,44 @@
+#' Complete benchmark experiment for a single learn task.
+#' Allows you to compare a list of learning algorithms by measuring the test error w.r.t. to a given resampling strategy.  
+#' Experiments are paired, meaning always the same training / test sets are used for the different learners.  
+#' 
+#' @param learners [\code{\link{list}} of \code{\linkS4class{wrapped.learner}} or \code{\link{character}}] \cr
+#' 		  Defines the learning algorithms which should be compared.
+#' @param task [\code{\linkS4class{learn.task}}] \cr
+#'   	  Specifies the learning task for the problem.
+#' @param task [\code{\linkS4class{learn.task}}] \cr
+#'        Learning task.
+#' @param resampling [\code{\linkS4class{resample.desc}} or \code{\linkS4class{resample.instance}}] \cr
+#'        Resampling strategy. 
+#' 
+#' @return A matrix of test error. Columns correspond to learners, row to the iteration of the resampling strategy.
+#' 
+#' @usage bench.exp(learners, task, resampling)
+#' 
+#' @note You can also get automatic, internal tuning by using \code{\link{make.tune.wrapper}} with your learner. 
+#' 
+#' @seealso \code{\link{benchmark}}, \code{\link{make.tune.wrapper}} 
 #' @export 
-bench.exp <- function(wrapped.learners, task, resampling) {
-	wrapped.learners = as.list(wrapped.learners)
-	bs = matrix(-1, nrow=resampling["iters"], ncol=length(wrapped.learners))
-	for (i in 1:length(wrapped.learners)) {
-		wl = wrapped.learners[[i]]
+#' @aliases bench.exp 
+#' @title bench.exp 
+#'
+#' @examples
+#' ct <- make.classif.task(data=iris, target="Species")
+#' # very small grid for svm hyperpars 
+#' r <- list(C=2^seq(-1,1), sigma=2^seq(-1,1))
+#' inner.res <- make.cv.desc(iters=3)   
+#' svm.tuner <- make.tune.wrapper("kernlab.svm.classif", method="grid", resampling=inner.res, control=grid.control(ranges=r))
+#' learners <- c("lda", "qda", svm.tuner)
+#' res <- make.cv.instance(iters=5, size=nrow(iris))
+#' bench.exp(learners, ct, res)
+#'  
+#' @title bench.exp
+
+bench.exp <- function(learners, task, resampling) {
+	learners = as.list(learners)
+	bs = matrix(-1, nrow=resampling["iters"], ncol=length(learners))
+	for (i in 1:length(learners)) {
+		wl = learners[[i]]
 		tp = benchmark(wl, task, resampling)$test.perf
 		bs[,i] = tp		
 	}
