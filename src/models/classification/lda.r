a4745518408916973c13043678ea6a2fe9d5bd20
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-14 20:13:07 +0000

    lda cannot use NAs

--- src/models/classification/lda.r
+++ src/models/classification/lda.r
@@ -21,7 +21,7 @@ setMethod(
 			
 			desc = new("classif.props",
 					supports.multiclass = TRUE,
-					supports.missing = TRUE,
+					supports.missing = FALSE,
 					supports.numerics = TRUE,
 					supports.factors = TRUE,
 					supports.characters = TRUE,
