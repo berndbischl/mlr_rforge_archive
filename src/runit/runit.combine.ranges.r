ba3b5a977d816457a4fc4d6e69b74cf4be28f0bd
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-06 16:55:59 +0000

    tests

--- src/runit/runit.combine.ranges.r
+++ src/runit/runit.combine.ranges.r
@@ -3,6 +3,6 @@ test.combine.ranges <- function() {
 	r1 <- list(kernel="polydot", C=c(1,2), degree=c(2,3))
 	r2 <- list(kernel="rbfdot", C=c(1,2), sigma=c(4,6))
 	ranges=combine.ranges(r1,r2)
-	tr<- tune("kernlab.svm.classif", binaryclass.task, cv.i, method="grid", control=list(ranges=ranges))
+	tr<- tune("kernlab.svm.classif", binaryclass.task, cv.i, method="grid", control=grid.control(ranges=ranges))
 }	
 	
--- src/runit/runit.tune.r
+++ src/runit/runit.tune.r
@@ -1,35 +1,35 @@
 test.tune <- function() {
 	
-#	
-#	cp <- c(0.05, 0.9)
-#	minsplit <- c(1:3)
-#	ranges <- list(cp = cp, minsplit=minsplit)
-#	folds = 3
-#	
-#	tr <- tune.rpart(formula=multiclass.formula, data=multiclass.df, cp=cp, minsplit=minsplit,
-#			tunecontrol = tune.control(sampling = "cross", cross = folds))  
-#	
-#	cv.instance <- e1071.cv.to.mlr.cv(tr)
-#	
-#	tr2 <- tune("rpart.classif", multiclass.task, cv.instance, method="grid", control=grid.control(ranges=ranges))
-#	
-#	for(i in 1:nrow(tr$performances)) {
-#		cp <- tr$performances[i,"cp"]
-#		ms <- tr$performances[i,"minsplit"]
-#		j <- which(tr2$all.perfs$cp == cp & tr2$all.perfs$minsplit == ms )
-#		checkEqualsNumeric(tr$performances[i,"error"], tr2$all.perfs[j,"mean"])    
-#		checkEqualsNumeric(tr$performances[i,"dispersion"], tr2$all.perfs[j,"sd"])    
-#	}
-#	
-#	
-#
-#	# check grid and scale
-#	control = grid.control(ranges=list(C=-1:1, sigma=-1:1))
-#	tune("kernlab.svm.classif", multiclass.task, cv.instance, method="grid", control=control, scale=function(x)10^x)
-#	
-#	# check pattern search
-#	control = ps.control(start=list(C=0, sigma=0))
-#	tr3 <- tune("kernlab.svm.classif", multiclass.task, cv.instance, method="pattern", control=control, scale=function(x)10^x)
+	
+	cp <- c(0.05, 0.9)
+	minsplit <- c(1:3)
+	ranges <- list(cp = cp, minsplit=minsplit)
+	folds = 3
+	
+	tr <- tune.rpart(formula=multiclass.formula, data=multiclass.df, cp=cp, minsplit=minsplit,
+			tunecontrol = tune.control(sampling = "cross", cross = folds))  
+	
+	cv.instance <- e1071.cv.to.mlr.cv(tr)
+	
+	tr2 <- tune("rpart.classif", multiclass.task, cv.instance, method="grid", control=grid.control(ranges=ranges))
+	
+	for(i in 1:nrow(tr$performances)) {
+		cp <- tr$performances[i,"cp"]
+		ms <- tr$performances[i,"minsplit"]
+		j <- which(tr2$all.perfs$cp == cp & tr2$all.perfs$minsplit == ms )
+		checkEqualsNumeric(tr$performances[i,"error"], tr2$all.perfs[j,"mean"])    
+		checkEqualsNumeric(tr$performances[i,"dispersion"], tr2$all.perfs[j,"sd"])    
+	}
+	
+	
+
+	# check grid and scale
+	control = grid.control(ranges=list(C=-1:1, sigma=-1:1))
+	tune("kernlab.svm.classif", multiclass.task, cv.instance, method="grid", control=control, scale=function(x)10^x)
+	
+	# check pattern search
+	control = ps.control(start=list(C=0, sigma=0))
+	tr3 <- tune("kernlab.svm.classif", multiclass.task, cv.instance, method="pattern", control=control, scale=function(x)10^x)
 
 	#complex test for tuning
 	
@@ -42,11 +42,11 @@ test.tune <- function() {
 	res = make.res.desc("cv", iters=2)
 	inner = make.res.desc("cv", iters=2)
 	
-#	tr = tune("kernlab.svm.classif", multiclass.task, res, control=control, fixed=list(type="spoc-svc"))
-#	
+	tr = tune("kernlab.svm.classif", multiclass.task, res, control=control, fixed=list(type="spoc-svc"))
+	
 	wl = make.learner("kernlab.svm.classif")			
 	set.train.par(wl, type="spoc-svc")
-#	tr = tune(wl, multiclass.task, res, control=control)
+	tr = tune(wl, multiclass.task, res, control=control)
 	
 	svm.tuner <- make.tune.wrapper("kernlab.svm.classif", resampling=inner, control=grid.control(ranges=r), fixed=list(type="spoc-svc"))
 	bench.exp(svm.tuner, multiclass.task, resampling=res)
@@ -55,15 +55,5 @@ test.tune <- function() {
 	bench.exp(svm.tuner, multiclass.task, resampling=res)
 	
 
-	#svm.tuner <- make.tune.wrapper(svm.multi, resampling=inner, method="grid", control=grid.control(ranges=r))
-	
-	
-	#	print(tr3)
-	
-#  rpart.learn.task <- new("t.rpart", data=data, formula=formula)
-#  rpart.tuned <- tune.cv(rpart.learn.task, cv.instance, ranges)
-	
-#  attr(rpart.tuned$best.parameters,"out.attrs")<- NULL tr2                        Max: reicht das zur Überprüfung?
-#  checkEquals(rpart.tuned$best.parameters, rev(trp$best.parameters))           Wie kann man hier verallgemeinern/Problem beheben?
 }
 
