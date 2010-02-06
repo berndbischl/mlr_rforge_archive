eb80cd728bf30e9eaf411e89a7691c74be3fe21e
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-06 20:33:36 +0000

    ada from ada

--- src/models/classification/ada.r
+++ src/models/classification/ada.r
@@ -39,76 +39,31 @@ setMethod(
 		}
 )
 
-#' Overwritten, to allow direct passing of tree hyperparameters in rpart.control.
-#' Besides that, simply delegates to super method.
-#' @export
 setMethod(
-		f = "train.learner",
-		
-		signature = c(
-				.wrapped.learner="ada", 
-				.targetvar="character", 
-				.data="data.frame", 
-				.weights="numeric", 
-				.costs="matrix", 
-				.parset="list"
-		),
-		
-		def = function(.wrapped.learner, .targetvar, .data, .weights, .costs, .parset) {
+		f = "initialize",
+		signature = signature("ada"),
+		def = function(.Object) {
 			
-			k <- parset$kernel
-			parset.names <- names(parset)
-			kpar = list()
+			desc = new("classif.props",
+					supports.multiclass = FALSE,
+					supports.missing = FALSE,
+					supports.numerics = TRUE,
+					supports.factors = TRUE,
+					supports.characters = FALSE,
+					supports.probs = TRUE,
+					supports.weights = FALSE,
+					supports.costs = FALSE
+			)
 			
-			make.kpar <- function(kernel.pars, kernel.name) {
-				kpar <- list()
-				for (p in kernel.pars) {
-					if (p %in% parset.names)
-						kpar[[p]] <- parset[[p]]
-				}
-				if (kernel.name %in% c("rbfdot", "laplacedot") && 
-						(is.null(kpar$sigma) || kpar$sigma=="automatic")) {
-					return("automatic")
-				} else {
-					return(kpar)
-				}
-			}
-			
-			change.parset <- function(parset, kpar) {
-				for (p in names(kpar))
-					parset[p] <- NULL
-				parset$kpar = kpar
-				return(parset)
-			}
-			
-			if (is.null(k)) 
-				k <- "rbfdot"      
-			if (k == "rbfdot" || k == "laplacedot") 
-				kpar <- make.kpar("sigma", k)
-			if (k == "polydot") 
-				kpar <- make.kpar(c("degree", "offset", "scale"), k)
-			if (k == "tanhdot") 
-				kpar <- make.kpar(c("offset", "scale"), k)
-			if (k == "besseldot") 
-				kpar <- make.kpar(c("degree", "sigma", "order"), k)
-			if (k == "anovadot") 
-				kpar <- make.kpar(c("degree", "sigma"), k)
-			if (k == "anovadot") 
-				kpar <- make.kpar(c("length", "lambda", "normalized"), k)
-			
-			parset <- change.parset(parset, kpar)
-			
-			m <- callNextMethod(wrapped.learner, target, data, weights, parset)
-			return(m)
+			callNextMethod(.Object, learner.name="ada", learner.pack="ada", learner.props=desc)
 		}
 )
 
-#' Overwritten, to allow direct passing of kernel hyperparameters.
 
 setMethod(
 		f = "train.learner",
 		signature = signature(
-				.wrapped.learner="kernlab.svm.classif", 
+				.wrapped.learner="ada", 
 				.targetvar="character", 
 				.data="data.frame", 
 				.weights="numeric", 
@@ -118,68 +73,21 @@ setMethod(
 		
 		def = function(.wrapped.learner, .targetvar, .data, .weights, .costs, .type,  ...) {
 			f = as.formula(paste(.targetvar, "~."))
-			kpar = list()
-			args = list(...)
-			args.names <- names(args)
-			
-			make.kpar <- function(kernel.pars, kernel.name) {
-				kpar <- list()
-				for (p in kernel.pars) {
-					if (p %in% args.names)
-						kpar[[p]] <- args[[p]]
-				}
-				if (kernel.name %in% c("rbfdot", "laplacedot") && 
-						(is.null(kpar$sigma) || kpar$sigma=="automatic")) {
-					return("automatic")
-				} else {
-					return(kpar)
-				}
-			}
-			
-			change.parset <- function(parset, kpar) {
-				for (p in names(kpar))
-					parset[p] <- NULL
-				parset$kpar = kpar
-				return(parset)
-			}
-			
-			if (!("kernel" %in% args.names)) 
-				kernel <- "rbfdot" 
-			else
-				kernel <- args$kernel
-			
-			if (kernel == "rbfdot" || kernel == "laplacedot") 
-				kpar <- make.kpar("sigma", kernel)
-			if (kernel == "polydot") 
-				kpar <- make.kpar(c("degree", "offset", "scale"), kernel)
-			if (kernel == "tanhdot") 
-				kpar <- make.kpar(c("offset", "scale"), kernel)
-			if (kernel == "besseldot") 
-				kpar <- make.kpar(c("degree", "sigma", "order"), kernel)
-			if (kernel == "anovadot") 
-				kpar <- make.kpar(c("degree", "sigma"), kernel)
-			if (kernel == "anovadot") 
-				kpar <- make.kpar(c("length", "lambda", "normalized"), kernel)
-			
-			parset = list(f, data=.data, prob.model = (.type == "prob"), fit=FALSE)
-			parset = c(parset, args)
-			parset <- change.parset(parset, kpar)
-			
-			do.call(ksvm, parset)
+			ada(f, data=.data, ...)
 		}
 )
 
 setMethod(
 		f = "predict.learner",
 		signature = signature(
-				.wrapped.learner = "kernlab.svm.classif", 
+				.wrapped.learner = "ada", 
 				.wrapped.model = "wrapped.model", 
 				.newdata = "data.frame", 
 				.type = "character" 
 		),
 		
 		def = function(.wrapped.learner, .wrapped.model, .newdata, .type, ...) {
-			.type <- ifelse(.type=="class", "response", "probabilities")
+			.type <- ifelse(.type=="class", "vector", "prob")
 			predict(.wrapped.model["learner.model"], newdata=.newdata, type=.type, ...)
 		}
 )	
