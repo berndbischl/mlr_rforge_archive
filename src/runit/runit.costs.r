35688d59f502d8525f567fdc3f22716516bc572a
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-22 09:48:44 +0000

    tests

--- src/runit/runit.costs.r
+++ src/runit/runit.costs.r
@@ -2,6 +2,6 @@ test.costs <- function() {
 	mydata = binaryclass.df
 	mydata[, binaryclass.target] = as.character(mydata[, binaryclass.target]) 
 	ct = make.classif.task(data=binaryclass.df, target= binaryclass.target)
-	checkEquals(ct@costs, matrix(c(0,1,1,0), 2, 2))
+	checkEquals(ct["costs"], matrix(c(0,1,1,0), 2, 2))
 	
 }
