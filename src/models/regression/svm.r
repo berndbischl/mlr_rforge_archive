b14e8286c5c93c78c771591e62c5be0e04c5b1b1
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-25 23:27:46 +0000

    svm regr

---
+++ src/models/regression/svm.r
@@ -0,0 +1,87 @@
+#' @include wrapped.learner.regr.r
+roxygen()
+
+#' @export
+setClass(
+		"kernlab.svm.regr", 
+		contains = c("wrapped.learner.regr")
+)
+
+
+#----------------- constructor ---------------------------------------------------------
+#' Constructor.
+#' @title SVM Constructor
+setMethod(
+		f = "initialize",
+		signature = signature("kernlab.svm.regr"),
+		def = function(.Object, parset) {
+			
+			desc = new("regr.props",
+					supports.missing = FALSE,
+					supports.numerics = TRUE,
+					supports.factors = TRUE,
+					supports.characters = FALSE,
+					supports.weights = FALSE
+			)
+			
+			callNextMethod(.Object, learner.name="SVM", learner.pack="kernlab", learner.props=desc, parset=parset)
+		}
+)
+
+#---------------- train -----------------------------------------------------
+
+
+#' Overwritten, to allow direct passing of kernel hyperparameters.
+
+setMethod(
+		f = "train.learner",
+		signature = signature(
+				.wrapped.learner="kernlab.svm.regr", 
+				.targetvar="character", 
+				.data="data.frame", 
+				.weights="numeric", 
+				.costs="missing", 
+				.type = "missing" 
+		),
+		
+		def = function(.wrapped.learner, .targetvar, .data, .weights, ...) {
+			f = as.formula(paste(.targetvar, "~."))
+			
+			kpar = list()
+			args = list(...)
+			args.names <- names(args)
+			
+			kernel.par.names = c("degree", "offset", "scale", "sigma", "order", "length", "lambda")
+			
+			kpar = list()
+			for (k in kernel.par.names) {
+				x = args[[k]]
+				if (!is.null(x)) {
+					kpar[[k]] = x
+					args[[k]] = NULL
+				}
+			}
+			
+			kargs = list(f, data=.data, kpar=kpar) 
+			kargs = c(kargs, args)
+			do.call(ksvm, kargs)
+		}
+)
+
+
+setMethod(
+		f = "predict.learner",
+		signature = signature(
+				.wrapped.learner = "kernlab.svm.regr", 
+				.wrapped.model = "wrapped.model", 
+				.newdata = "data.frame", 
+				.type = "missing" 
+		),
+		
+		def = function(.wrapped.learner, .wrapped.model, .newdata, ...) {
+			predict(.wrapped.model["learner.model"], newdata=.newdata, ...)[,1]
+		}
+)	
+
+
+
