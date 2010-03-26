f37610b1b477367fb31e81086702f55250502908
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-26 03:11:15 +0000

    refactor

--- src/testsuite.r
+++ src/testsuite.r
@@ -9,10 +9,7 @@ source("src/runit/helpers.r")
 source("src/runit/make.runit.tests.r")
 
 
-
-
 parallel.setup(mode="local")
-
 
 logger.setup(level="error")
 
@@ -20,40 +17,34 @@ data(Sonar)
 
 binaryclass.df <- Sonar
 binaryclass.formula <- Class~.
+binaryclass.target = "Class"
 binaryclass.train.inds <- c(1:50, 100:150)
 binaryclass.test.inds  <- setdiff(1:nrow(binaryclass.df), binaryclass.train.inds)
 binaryclass.train <- binaryclass.df[binaryclass.train.inds, ]
 binaryclass.test  <- binaryclass.df[binaryclass.test.inds, ]
 binaryclass.class.col <- 61
 binaryclass.class.levs <- levels(binaryclass.df[, binaryclass.class.col])
-
+binaryclass.task = make.classif.task("binary", data=binaryclass.df, target=binaryclass.target)  
 
 multiclass.df <- iris
 multiclass.formula <- Species~.
+multiclass.target = "Species"
 multiclass.train.inds <- c(1:30, 51:80, 101:130)
 multiclass.test.inds  <- setdiff(1:150, multiclass.train.inds)
 multiclass.train <- multiclass.df[multiclass.train.inds, ]
 multiclass.test  <- multiclass.df[multiclass.test.inds, ]
 multiclass.class.col <- 5
-
-
-#fr <- mlbench.friedman1(150)
-#fr2 <- as.data.frame(fr$x)
-#fr2$y <- fr$y 
-#regr.df <- fr2  
-#regr.formula <- y ~ . 
-#regr.train.inds <- c(1:30, 51:80, 101:130)
-#regr.test.inds  <- setdiff(1:150, regr.train.inds)
-#regr.train <- regr.df[regr.train.inds, ]
-#regr.test  <- regr.df[regr.test.inds, ]
+multiclass.task = make.classif.task("multiclass", data=multiclass.df, target=multiclass.target)  
 
 data(BostonHousing)
 regr.df <- BostonHousing  
 regr.formula <- medv ~ . 
-regr.train.inds <- seq(1, 506, 2)
+regr.target = "medv"
+regr.train.inds <- seq(1, 506, 3)
 regr.test.inds  <- setdiff(1:nrow(regr.df), regr.train.inds)
 regr.train <- regr.df[regr.train.inds, ]
 regr.test  <- regr.df[regr.test.inds, ]
+regr.task <- make.regr.task("regrtask", data=BostonHousing, target="medv")  
 
 
 .mlr.local$debug.seed <- 12345
