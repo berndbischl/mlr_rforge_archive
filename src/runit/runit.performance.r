18362300020183664a76453f916e1c0d68072037
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-02 13:43:00 +0000

    tests for predict and perf

--- src/runit/runit.performance.r
+++ src/runit/runit.performance.r
@@ -1,3 +1,33 @@
 test.performance <- function() {
+	
+	res = make.res.desc("holdout")
+	rf = resample.fit("rpart", task=binaryclass.task, resampling=res)
+	performance(rf, measures=c("acc", "time"))
 
-}
+	res = make.res.desc("bs", iters=3)
+	rf = resample.fit("rpart", task=binaryclass.task, resampling=res)
+	performance(rf, measures=c("acc", "time"))
+
+	# custom measure
+	res = make.res.desc("cv", iters=3)
+	rf = resample.fit("rpart", task=binaryclass.task, resampling=res)
+	
+	mymeasure = function(x, task) {
+		# normal test error
+		e1 = mean(x["truth"] != x["response"])
+		# we do this manually 
+		id = x["id"]
+		tn = x["task.desc"]["target"]
+		print(tn)
+		t2 = task["data"][id, tn]
+		e2 = mean(t2 != x["response"])
+		print(c(e1, e2))
+		checkEquals(e1, e2)
+
+	}
+	attr(mymeasure, "name") = "mym"
+	
+	performance(rf, measures=mymeasure, task=binaryclass.task)
+	
+	
+}	
--- src/runit/runit.predict.r
+++ src/runit/runit.predict.r
@@ -13,11 +13,17 @@ test.predict <- function() {
 	checkEquals(cp2["response"], pred2)
 	
 	cm3 <- train("lda", multiclass.task, subset=inds)
-	cp3 <- predict(cm2, newdata=data[-inds,])
+	cp3 <- predict(cm3, newdata=data[multiclass.test.inds,])
 	ext3 <- lda(formula, data=data[inds,])
-	pred3 <- predict(ext2,newdata=data[-inds,])$class
+	pred3 <- predict(ext3,newdata=data[multiclass.test.inds,])$class
 	
 	checkEquals(cp3["response"], pred3)
+	
+	cp4 <- predict(cm3, task=multiclass.task, subset=multiclass.test.inds)
+	checkEquals(cp4["response"], pred3)
+	checkEquals(cp4["truth"], data[multiclass.test.inds, multiclass.target])
+	checkEquals(cp4["id"], multiclass.test.inds)
+	
 	
 	# find a good test for pred.fct.pars....  
 	
