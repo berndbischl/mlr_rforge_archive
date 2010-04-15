326fc1ff0a49a08d57b7de2fd6d3edd5820a794d
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-15 01:15:02 +0000

    varsel

--- src/base/varsel/varsel.control.r
+++ src/base/varsel/varsel.control.r
@@ -1,8 +1,8 @@
 #' @export
 varsel.control <- function(compare="diff", maxit=100, max.vars=Inf, 
-	alpha=0.01, beta=0.01, gamma=NA, delta=NA) {
+	alpha=0.01, beta=0.01, gamma=NA, delta=NA, epsilon=NA) {
 	list(compare=compare, max.vars=max.vars, minimize=TRUE, maxit=maxit,
-	 	alpha=alpha, beta=beta, gamma=gamma, delta=delta 
+	 	alpha=alpha, beta=beta, gamma=gamma, delta=delta, epsilon=epsilon 
 	)
 }
 
--- src/base/varsel/varsel.hybrid.r
+++ src/base/varsel/varsel.hybrid.r
@@ -7,26 +7,34 @@ varsel.hybrid = function(learner, task, resampling, measures, aggr, method, cont
 	cors = abs(cor(data, data))
 	diag(cors) = NA
 	m = length(all.vars) 
-	p = control$delta
+	flip.rate = control$epsilon
+	p01 = control$delta
+	# look back for adaption of flip.rate (step.size)
+	win.size = 10
 	
 	start = all.vars[as.logical(rbinom(m, 1, 0.5))]
 	#cat("start:", start, "\n")
 	state = eval.state(learner, task, resampling, measures, aggr, vars=start, "start")
 	path = add.path(path, state, T)		
 	#print(get.perf(state))
-	# use alpha for mut
-	ctrl2 = control
-	ctrl2$alpha = 0
-			
 	
 	# big loop for mut + local
+	mut.succ = c()
 	while (.mlr.vareval < control$maxit) {
 		
 		# mutate til successful
 		vs.bin = all.vars %in% state$vars
 		#print(vs.bin)
 		while (.mlr.vareval < control$maxit) {
-			probs = abs(as.numeric(vs.bin) - control$delta) * 16 / m
+			# we look back win.size mutations and maybe adapt the step.size
+			if (length(mut.succ) > win.size) {
+				mean.succ = mean(mut.succ[(length(mut.succ)-win.size+1):length(mut.succ)])
+				if (mean.succ > 0.3)
+					flip.rate = flip.rate/1.2
+				if (mean.succ < 0.1)
+					flip.rate = flip.rate*1.2
+			}
+			probs = abs(as.numeric(vs.bin) - p01) * flip.rate / m
 			
 			mut = as.logical(rbinom(m, 1, prob=probs))
 			#cat("mut:", mut, "\n")
@@ -37,9 +45,10 @@ varsel.hybrid = function(learner, task, resampling, measures, aggr, method, cont
 			#cat("new.vars:", new.vars, "\n")
 			new.state = eval.state(learner, task, resampling, measures, aggr, vars=new.vars, "mutate")
 			#print(get.perf(new.state))
-			cc = compare.diff(state, new.state, ctrl2, measures, aggr, threshold=control$gamma)	&& 
+			cc = compare.diff(state, new.state, control, measures, aggr, threshold=control$gamma)	&& 
 					(length(new.state$vars) > 0)
-			path = add.path(path, new.state, cc)		
+			path = add.path(path, new.state, cc)
+			mut.succ = c(mut.succ, as.numeric(cc))
 			if (cc) {
 				#print("accept")
 				state=new.state
