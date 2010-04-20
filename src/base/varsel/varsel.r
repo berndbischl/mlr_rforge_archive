e36c0ba5ee747ede12b0dd2232d26e5f74b058dc
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-20 15:43:11 +0000

    varsel

--- src/base/varsel/varsel.r
+++ src/base/varsel/varsel.r
@@ -10,18 +10,21 @@ varsel <- function(learner, task, resampling, method="forward", control=NULL, me
 		aggr = default.aggr(task)
 	aggr = make.aggrs(aggr)
 	
-	sel.func = switch(method,
-			sfs = varsel.seq,
-			sbs = varsel.seq,
-			sffs = varsel.seq,
-			sfbs = varsel.seq,
-			random = varsel.random,
-			bestcor = varsel.bestcor,
-			hybrid = varsel.hybrid,
-			hybrid2 = varsel.hybrid2,
-			stop(paste("Method", method, "does not exist!"))
-	)
-	
+	if (is.character(method)) {
+		sel.func = switch(method,
+				sfs = varsel.seq,
+				sbs = varsel.seq,
+				sffs = varsel.seq,
+				sfbs = varsel.seq,
+				random = varsel.random,
+				bestcor = varsel.bestcor,
+				hybrid = varsel.hybrid,
+				hybrid2 = varsel.hybrid2,
+				stop(paste("Method", method, "does not exist!"))
+		)
+	} else {
+		sel.func = method
+	}	
 	assign(".mlr.vareval", 0, envir=.GlobalEnv)
 	
 	op = sel.func(learner=learner, task=task, resampling=resampling, 
