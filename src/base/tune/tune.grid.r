b9f878da802e7c29ab0fb4ff1b10ee55316f3740
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-11 19:17:10 +0000

    control + type + threshold

--- src/base/tune/tune.grid.r
+++ src/base/tune/tune.grid.r
@@ -1,32 +1,33 @@
-tune.grid <- function(learner, task, resampling, measures, aggr, control) {
-	ranges = control$ranges
+tune.grid <- function(learner, task, resampling, type, measures, aggr, control) {
+	ranges = control["ranges"]
 	# if theres more than one ranges 
-	if(all((names(ranges) == "ranges"))) {
-		ors = lapply(ranges, function(r) {tune.1(learner, task, resampling, ranges, measures, aggr, control)})
+	if(length(ranges) > 0 && all((names(ranges) == "ranges"))) {
+		ors = lapply(ranges, function(r) {tune.1(learner, task, resampling, type, ranges, measures, aggr, control)})
 		
 		ps = lapply(ors, function(x) x@path)
 		ps = Reduce(c, ps)
 		perfs = sapply(ors, function(x) x@opt$perf[1])
-		if (control$minimize)
+		if (control["minimize"])
 			i = which.min(perfs)
 		else				
 			i = which.max(perfs)
 		new("opt.result", opt=ors[[i]]@opt,  path=ps)
 	}else {
-		tune.1(learner, task, resampling, ranges, measures, aggr, control)
+		tune.1(learner, task, resampling, type, ranges, measures, aggr, control)
 	}
 }
 
 
 
-tune.1 <- function(learner, task, resampling, ranges, measures, aggr, control) {
+tune.1 <- function(learner, task, resampling, type, ranges, measures, aggr, control) {
 	check.ranges(ranges)
 
 	grid = expand.grid(ranges, KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE)
-	parsets = lapply(1:nrow(grid), function(i) as.list(grid[i,,drop=FALSE]))	
-
-	es = eval.states.tune(learner=learner, task=task, resampling=resampling, measures=measures, aggr=aggr, 
-			pars=parsets, ps.scale=control$scale, ps.names=names(ranges), event="grid")
+	
+	parsets = lapply(seq(length=nrow(grid)), function(i) as.list(grid[i,,drop=FALSE]))	
+	es = eval.states.tune(learner=learner, task=task, resampling=resampling, type=type, 
+			measures=measures, aggr=aggr, control=control, 
+			pars=parsets, event="grid")
 	
 	bs = select.best.state(es, control)
 	path = add.path.els.tune(path=list(), ess=es, best=bs)
