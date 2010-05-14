ac004965b29a56a1bf0def7613c307b361a2c170
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-14 00:25:00 +0000

    getter

--- src/base/task.desc.r
+++ src/base/task.desc.r
@@ -21,6 +21,12 @@ setMethod(
 		f = "[",
 		signature = signature("task.desc"),
 		def = function(x,i,j,...,drop) {
+			if (i == "is.classif") {
+				return(x@task.class == "classif.task")
+			}
+			if (i == "is.regr") {
+				return(x@task.class == "regr.task")
+			}
 			callNextMethod()
 		}
 )
