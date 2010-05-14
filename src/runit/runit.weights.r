119b811ece76891092d9efa1fc36f4ccc30cb698
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-14 13:25:14 +0000

    fix weights test

--- src/runit/runit.weights.r
+++ src/runit/runit.weights.r
@@ -1,16 +1,22 @@
 test.weights <- function() {
-	
-	m = train("stat.lm", task=regr.task)
-	pred = predict(m, newdata=regr.df)
-	p = performance
+	ws = 1:nrow(regr.df)
+	rt = make.task(target=regr.target, data=regr.df, weights=ws)
+	m = train("regr.lm", task=rt)
+	p = predict(m, task=rt, subset=30:100)
 	df = as.data.frame(p)
+	cns = colnames(df)
+	checkEquals(cns, c("id", "truth", "response"))
 
-	#	cp = predict(cm5, task=ct5, subset=30:100)
+	m2 = lm(regr.formula, data=regr.df, weights=ws)
+	p2 = predict(m2, newdata=regr.df[30:100,])
+	checkEquals(p2, p["response"], checkNames=F)
+	
+	
+	
+	#	cp = predict(cm5, task=ct5, )
 #	cp["weights"] = 30:100
 #	
 #	df = as.data.frame(cp)
-#	cn = colnames(df)
-#	checkEquals(cn4, c("id", "response", "truth", "weights"))
 	
 	
 }
