9ca4496a5b36203c54de369a05e53ddce650b097
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-26 18:47:49 +0000

    novars

--- src/runit/runit.novars.r
+++ src/runit/runit.novars.r
@@ -11,11 +11,14 @@ test.novars <- function() {
 	checkEquals(m["learner"]["predict.type"], "prob")	
 	p = predict(m, newdata=multiclass.df)
 	checkTrue(setequal(colnames(as.data.frame(p)), c("prob.setosa", "prob.virginica", "prob.versicolor", "truth", "response")))	
+	res = make.res.desc("cv", iter=2)
+	rf = resample.fit(wl, multiclass.task, res, vars=c())
+	checkTrue(setequal(colnames(as.data.frame(p)), c("prob.setosa", "prob.virginica", "prob.versicolor", "truth", "response")))	
+	
 	
 	m = train("regr.lm", regr.task, vars=c())
 	p = predict(m, newdata=multiclass.df)
 	checkTrue(all(p["response"] == mean(p["response"]))) 
-	res = make.res.desc("cv", iter=2)
 	rf = resample.fit("regr.lm", regr.task, res, vars=c())
 	checkEquals(length(unique(rf["response"])), 2) 
 }
--- src/runit/runit.varsel.r
+++ src/runit/runit.varsel.r
@@ -2,6 +2,9 @@ test.varsel <- function() {
 	inner = make.res.desc("cv", iter=2)
 	ctrl = varsel.control(alpha=0.01)
 	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, method="sfs", control=ctrl)
+
+	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, method="sbs", control=ctrl)
+	checkTrue(length(vr["path"]) > 1) 
 	
 	wl = make.varsel.wrapper("classif.lda", resampling=inner, method="sfs", control=ctrl)
 	outer = make.res.desc("cv", iter=2)
