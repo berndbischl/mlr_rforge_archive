d6842aa95e495ca1555411ac180f8a5a933a6fed
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-26 22:10:10 +0000

    depends

--- src/make/desc.r
+++ src/make/desc.r
@@ -15,8 +15,8 @@ write.desc <- function(desc.path, rev.nr) {
 		"Description: no\n",
 		"License: GPL (>= 2)\n",
 		"LazyLoad: yes\n",
-		"Depends: R (>= 2.8.0), methods, reshape, abind, boot, klaR\n",
-		"Suggests: MASS, e1071, snowfall, mlbench, ROCR, cmaes, kernlab, rpart, randomForest, adabag, kknn, ada, adabag, party, mboost, mda, gbm, nnet, penalized, RWeka\n",
+		"Depends: R (>= 2.8.0), methods, reshape, abind, boot, klaR, e1071\n",
+		"Suggests: MASS, snowfall, mlbench, ROCR, cmaes, kernlab, rpart, randomForest, adabag, kknn, ada, adabag, party, mboost, mda, gbm, nnet, penalized, RWeka\n",
 		sep=""
 	)
 	
