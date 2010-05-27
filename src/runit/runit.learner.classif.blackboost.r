23400acae3445e05a66d86dca9754ce16cddad42
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-27 15:33:21 +0000

    more tests

--- src/runit/runit.learner.classif.blackboost.r
+++ src/runit/runit.learner.classif.blackboost.r
@@ -1,11 +1,33 @@
 test.blackboost.classif <- function(){
 	
 	
-	m = blackboost(formula=binaryclass.formula, data=binaryclass.train, family=AdaExp()) 
-	p.class = predict(m, newdata=binaryclass.test, type="class")
+	parset.list1 = list(
+			list(family=Binomial(), tree_controls=ctree_control(maxdepth=2)),
+			list(family=Binomial(), tree_controls=ctree_control(maxdepth=4), control=boost_control(nu=0.03))
+	)
 	
-	simple.test("classif.blackboost", binaryclass.df, binaryclass.formula, binaryclass.train.inds, p.class)
+	parset.list2 = list(
+			list(family=Binomial(), maxdepth=2),
+			list(family=Binomial(), maxdepth=4, nu=0.03)
+	)
 	
+	old.predicts.list = list()
+	old.probs.list = list()
+	
+	for (i in 1:length(parset.list1)) {
+		parset = parset.list1[[i]]
+		pars = list(binaryclass.formula, data=binaryclass.train)
+		pars = c(pars, parset)
+		set.seed(debug.seed)
+		m = do.call(blackboost, pars)
+		set.seed(debug.seed)
+		old.predicts.list[[i]] = predict(m, newdata=binaryclass.test, type="class")
+		set.seed(debug.seed)
+		old.probs.list[[i]] = predict(m, newdata=binaryclass.test, type="response")[,1]
+	}
+	
+	simple.test.parsets("classif.blackboost", binaryclass.df, binaryclass.formula, binaryclass.train.inds, old.predicts.list, parset.list2)
+	prob.test.parsets("classif.blackboost", binaryclass.df, binaryclass.formula, binaryclass.train.inds, old.probs.list, parset.list2)	
 }
 
 
---
+++ src/runit/runit.learner.classif.glmboost.r
@@ -0,0 +1,34 @@
+test.glmboost.classif <- function(){
+	
+	parset.list1 = list(
+			list(family=Binomial(), control=boost_control(nu=0.03)),
+			list(family=Binomial(), control=boost_control(mstop=600), center=T)
+	)
+	
+	parset.list2 = list(
+			list(family=Binomial(), nu=0.03),
+			list(family=Binomial(), mstop=600, center=T)
+	)
+	
+	old.predicts.list = list()
+	old.probs.list = list()
+	
+	for (i in 1:length(parset.list1)) {
+		parset = parset.list1[[i]]
+		pars = list(binaryclass.formula, data=binaryclass.train)
+		pars = c(pars, parset)
+		set.seed(debug.seed)
+		m = do.call(glmboost, pars)
+		set.seed(debug.seed)
+		old.predicts.list[[i]] = predict(m, newdata=binaryclass.test, type="class")
+		set.seed(debug.seed)
+		old.probs.list[[i]] = predict(m, newdata=binaryclass.test, type="response")[,1]
+	}
+	
+	simple.test.parsets("classif.glmboost", binaryclass.df, binaryclass.formula, binaryclass.train.inds, old.predicts.list, parset.list2)
+	prob.test.parsets("classif.glmboost", binaryclass.df, binaryclass.formula, binaryclass.train.inds, old.probs.list, parset.list2)
+	
+}
+
+
+
