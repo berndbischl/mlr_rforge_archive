49d9e0282f70b3dd07386c0ab59b9fee244fa4fe
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-03 22:38:41 +0000

    remove obsolete getter code

--- src/base/resample/resample.desc.r
+++ src/base/resample/resample.desc.r
@@ -1,7 +1,14 @@
 #' Base class for description of resampling algorithms.
 #' A description of a resampling algorithm contains all necessary information to provide a resampling.instance, 
 #' when given the size of the data set.
-
+#' 
+#' Getter.
+#' 
+#' \describe{
+#' 	\item{instance.class [character]}{S4 class name of the corresponding resample.instance}
+#' 	\item{name [character]}{Name of this resampling algorithm}
+#' 	\item{iters [numeric]}{Number of iterations}
+#' } 
 #' @exportClass resample.desc 
 #' @title resample.desc
 
@@ -15,27 +22,6 @@ setClass(
 				iters="numeric"
 		)
 )
-
-
-#----------------- getter ---------------------------------------------------------
-
-#' Getter.
-#' 
-#' \describe{
-#' 	\item{instance.class [character]}{S4 class name of the corresponding resample.instance}
-#' 	\item{name [character]}{Name of this resampling algorithm}
-#' 	\item{iters [numeric]}{Number of iterations}
-#' } 
-#' 
-#' @rdname resample.desc-class
-
-setMethod(f="[",
-          signature = signature("resample.desc"),
-          def = function(x,i,j,...,drop) {
-            if (i %in% slotNames(x))
-              return(slot(x, i))
-            return(NULL)            
-          })
 
 #' @rdname to.string
 
