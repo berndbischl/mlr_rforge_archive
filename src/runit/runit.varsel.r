707370dc29cd06829b3685778715cc0220cfaa05
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-08 20:05:48 +0000

    check empty model

--- src/runit/runit.varsel.r
+++ src/runit/runit.varsel.r
@@ -6,6 +6,13 @@ test.varsel <- function() {
 	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, method="sbs", control=ctrl)
 	checkTrue(length(vr["path"]) > 1) 
 	
+	# check empty model
+	ctrl = varsel.control(alpha=10)
+	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, control=ctrl, model=T)
+	checkEquals(vr["par"], character(0)) 
+	checkTrue(is(vr["model"], "wrapped.model")) 
+	checkTrue(is(vr["model"]["learner.model"], "empty.model")) 
+	
 	wl = make.varsel.wrapper("classif.lda", resampling=inner, method="sfs", control=ctrl)
 	outer = make.res.desc("cv", iter=2)
 	be = bench.exp(wl, task=multiclass.task, resampling=outer)
