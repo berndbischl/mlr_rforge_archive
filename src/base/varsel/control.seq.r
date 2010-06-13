61f378945d78c55a4271597a04e1ad0892644c9f
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-13 21:25:13 +0000

    bla

--- src/base/varsel/control.seq.r
+++ src/base/varsel/control.seq.r
@@ -1,26 +1,26 @@
-#' @include control.varsel.r
-roxygen()
-
-setClass(
-		"seq.control",
-		contains = c("varsel.control"),
-		representation = representation(
-				alpha = "numeric", 
-				beta = "numeric" 
-		)
-)
-
-
-setMethod(
-		f = "initialize",
-		signature = signature("seq.control"),
-		def = function(.Object, method, minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
-			.Object = callNextMethod(.Object, method, minimize, tune.threshold, thresholds, maxit=maxit, max.vars)
-			.Object@alpha = alpha 			
-			.Object@beta = beta 			
-			return(.Object)
-		}
-)
+##' @include control.varsel.r
+#roxygen()
+#
+#setClass(
+#		"seq.control",
+#		contains = c("varsel.control"),
+#		representation = representation(
+#				alpha = "numeric", 
+#				beta = "numeric" 
+#		)
+#)
+#
+#
+#setMethod(
+#		f = "initialize",
+#		signature = signature("seq.control"),
+#		def = function(.Object, method, minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
+#			.Object = callNextMethod(.Object, method, minimize, tune.threshold, thresholds, maxit=maxit, max.vars)
+#			.Object@alpha = alpha 			
+#			.Object@beta = beta 			
+#			return(.Object)
+#		}
+#)
 
 
 #' Control structure for sequential variable selection. 
@@ -45,32 +45,7 @@ setMethod(
 #' @rdname seq.control 
 #' @title Control structure for sequential variable selection. 
 
-seq.control = function(minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
-			if (missing(minimize))
-				minimize=TRUE
-			if (missing(tune.threshold))
-				tune.threshold=FALSE
-			if (missing(thresholds))
-				thresholds=10
-			if (missing(maxit))
-				maxit = .Machine$integer.max
-			if (missing(max.vars))
-				max.vars = .Machine$integer.max
-			if (missing(alpha))
-				alpha=0.01
-			if (missing(beta))
-				beta=0.01
-	new("seq.control", method="seq", minimize=minimize, 
-			tune.threshold=tune.threshold, thresholds=thresholds, 
-			maxit=maxit, max.vars=max.vars, alpha=alpha, beta=beta)
-}
-
-
-
-
-#setGeneric(
-#		name = "seq.control",
-#		def = function(minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
+#seq.control = function(minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
 #			if (missing(minimize))
 #				minimize=TRUE
 #			if (missing(tune.threshold))
@@ -85,22 +60,47 @@ seq.control = function(minimize, tune.threshold, thresholds, maxit, max.vars, al
 #				alpha=0.01
 #			if (missing(beta))
 #				beta=0.01
-#			standardGeneric("seq.control")
-#		}
-#)
-#
-#
-#
-#setMethod(
-#		f = "seq.control",
-#		signature = signature(minimize="logical", tune.threshold="logical", thresholds="numeric", 
-#				maxit="numeric", max.vars="numeric", alpha="numeric", beta="numeric"),
-#		def = function(minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
+#	new("seq.control", method="seq", minimize=minimize, 
+#			tune.threshold=tune.threshold, thresholds=thresholds, 
+#			maxit=maxit, max.vars=max.vars, alpha=alpha, beta=beta)
+#}
+
+
+
+setGeneric(
+		name = "seq.control",
+		def = function(minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
+			if (missing(minimize))
+				minimize=TRUE
+			if (missing(tune.threshold))
+				tune.threshold=FALSE
+			if (missing(thresholds))
+				thresholds=10
+			if (missing(maxit))
+				maxit = .Machine$integer.max
+			if (missing(max.vars))
+				max.vars = .Machine$integer.max
+			if (missing(alpha))
+				alpha=0.01
+			if (missing(beta))
+				beta=0.01
+			standardGeneric("seq.control")
+		}
+)
+
+
+
+setMethod(
+		f = "seq.control",
+		signature = signature(minimize="logical", tune.threshold="logical", thresholds="numeric", 
+				maxit="numeric", max.vars="numeric", alpha="numeric", beta="numeric"),
+		def = function(minimize, tune.threshold, thresholds, maxit, max.vars, alpha, beta) {
 #			new("seq.control", method="seq", minimize=minimize, 
 #					tune.threshold=tune.threshold, thresholds=thresholds, 
 #					maxit=maxit, max.vars=max.vars, alpha=alpha, beta=beta)
-#		}
-#)
+			1
+		}
+)
 
 
 
