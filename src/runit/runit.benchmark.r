1cc8a238f3cb4db7c23bacbea5082b2f2435e964
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-16 22:45:08 +0000

    cleaning up ctrl structures

--- src/runit/runit.benchmark.r
+++ src/runit/runit.benchmark.r
@@ -1,21 +1,40 @@
 test.benchmark <- function() {
-	
-
-	ct <- make.task("iris", data=iris, target="Species")
-	outer <- make.res.instance("cv", ct, iters=5) 
-	inner <- new("cv.desc", iters=3)
+	ct = make.task("iris", data=iris, target="Species")
+	outer = make.res.desc("holdout") 
+	inner = new("cv.desc", iters=3)
 	
 	# check empty ranges 
-	cbr <- benchmark("classif.rpart", task=ct, resampling=outer, models=T, opts=T, paths=T)
+	cbr <- benchmark("classif.rpart", task=ct, resampling=outer, models=T, paths=T)
 
 	# normal benchmark - one par
 	ranges <- list(minsplit=seq(3,10,2))
 	wl = make.tune.wrapper("classif.rpart", resampling=inner, control=grid.control(ranges=ranges))
-	cbr = benchmark(wl, ct, outer, models=T, opts=T, paths=T)
-
+	bm = benchmark(wl, ct, outer, models=T, paths=T)
+	checkTrue(is.list(bm$ors))
+	checkEquals(length(bm$ors), 1)
+	checkTrue(is.list(bm$ors[[1]]@path))
+	checkTrue(length(bm$ors[[1]]@path) > 1)
+	checkTrue(is.list(bm$models))
+	checkEquals(length(bm$models), 1)
+	checkTrue(is(bm$models[[1]], "wrapped.model"))
+	checkTrue(is(bm$models[[1]]["learner.model"], "rpart"))
+	
+	# dont save models and paths
+	wl = make.tune.wrapper("classif.rpart", resampling=inner, control=grid.control(ranges=ranges))
+	bm = benchmark(wl, ct, outer, models=F, paths=F)
+	checkTrue(is.list(bm$ors))
+	checkEquals(length(bm$ors), 1)
+	checkTrue(is.list(bm$ors[[1]]@opt$par))
+	checkEquals(names(bm$ors[[1]]@opt$par), "minsplit")
+	checkEquals(names(bm$ors[[1]]@opt$perf), c("mean.mmce", "sd.mmce"))
+	checkTrue(is.list(bm$ors[[1]]@path))
+	checkTrue(length(bm$ors[[1]]@path) == 0)
+	checkTrue(is.null(bm$models))
+	
+	
 	# normal benchmark - 2 par
 	ranges <- list(minsplit=seq(3,10,2), cp=c(0.1, 0.11 , 0.09))
 	wl = make.tune.wrapper("classif.rpart", resampling=inner, control=grid.control(ranges=ranges))
-	cbr = benchmark(wl, ct, outer, models=F, opts=F, paths=F)
+	cbr = benchmark(wl, ct, outer, models=F, paths=F)
 	
 }
