2c3fab3db1f9b4e4bc0ef62978564a18dd45c26e
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-17 19:16:30 +0000

    monte carlo varsel

--- src/runit/runit.varsel.r
+++ src/runit/runit.varsel.r
@@ -1,6 +1,8 @@
 test.varsel <- function() {
 	inner = make.res.desc("cv", iter=2)
 
+	# check all methods
+	
 	ctrl = sequential.control(method="sfs", alpha=0.01)
 	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, control=ctrl, path=T)
 	checkTrue(length(vr["path"]) > 1) 
@@ -8,6 +10,19 @@ test.varsel <- function() {
 	ctrl = sequential.control(method="sbs", beta=0.01)
 	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, control=ctrl, path=T)
 	checkTrue(length(vr["path"]) > 1) 
+
+	ctrl = sequential.control(method="sffs", alpha=0.01)
+	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, control=ctrl, path=T)
+	checkTrue(length(vr["path"]) > 1) 
+	
+	ctrl = sequential.control(method="sfbs", beta=0.01)
+	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, control=ctrl, path=T)
+	checkTrue(length(vr["path"]) > 1) 
+	
+	ctrl = randomvarsel.control(maxit=10)
+	vr = varsel("classif.lda", task=multiclass.task, resampling=inner, control=ctrl, path=T)
+	checkTrue(length(vr["path"]) > 1) 
+	
 	
 	# check empty model
 	ctrl = sequential.control(method="sfs", alpha=10)
@@ -19,5 +34,6 @@ test.varsel <- function() {
 	wl = make.varsel.wrapper("classif.lda", resampling=inner, control=ctrl)
 	outer = make.res.desc("cv", iter=2)
 	be = bench.exp(wl, task=multiclass.task, resampling=outer)
+	
 }
 
