7ed6cff9b4855cf94db2927ab8edd00f2db67a4f
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-28 11:42:28 +0000

    refactor desc objects

--- src/base/task.desc.r
+++ src/base/task.desc.r
@@ -1,20 +1,28 @@
 #' @include object.r
 roxygen()
 
+#' Description object for task.
+#' 
+#' Getter.\cr
+#' 
+#' \describe{
+#'  \item{id [string]}{Id string of task.}
+#'	\item{label [string]}{Label string of task.}
+#'  \item{costs [matrix]}{Cost matrix, of dimension (0,0) if not available.}
+#'  \item{positive [string]}{Positive class label for binary classification, NA else.}
+#'  \item{negative [string]}{Negative class label for binary classification,, NA else.}
+#' }
+#' @exportClass task.desc
+#' @title Description object for task. 
+#' 
 
 setClass(
 		"task.desc",
 		contains = c("object"),
 		representation = representation(
 				task.class = "character",
-				id = "character",
-				label = "character",
-				target = "character",
-				excluded = "character",
-				costs = "matrix",
-				positive = "character",
-				negative = "character"
-	)
+				props = "list"
+		)
 )
 
 #' @rdname task.desc-class
@@ -22,12 +30,55 @@ setMethod(
 		f = "[",
 		signature = signature("task.desc"),
 		def = function(x,i,j,...,drop) {
-			if (i == "is.classif") {
+			if (i == "is.classif")
 				return(x@task.class == "classif.task")
-			}
-			if (i == "is.regr") {
+			if (i == "is.regr")
 				return(x@task.class == "regr.task")
-			}
+			if (i == "id") 
+				return(x@props$id)
+			if (i == "label") 
+				return(x@props$label)
+			if (i == "costs") 
+				return(x@props$costs)
+			if (i == "positive") 
+				return(x@props$positive)
+			if (i == "negative") 
+				return(x@props$negative)
+			callNextMethod()
+		}
+)
+
+
+setMethod(
+		f = "initialize",
+		signature = signature("task.desc"),
+		def = function(.Object, task.class, id, label, costs, positive, negative) {
+			.Object@task.class = task.class
+			.Object@props$id = id
+			.Object@props$label = label
+			.Object@props$costs = costs
+			.Object@props$positive = positive
+			.Object@props$negative = negative
+			return(.Object)
+		}
+)
+
+
+#' @rdname task.desc-class
+setMethod(
+		f = "[",
+		signature = signature("task.desc"),
+		def = function(x,i,j,...,drop) {
+			if (i == "id") 
+				return(x@props$id)
+			if (i == "label") 
+				return(x@props$label)
+			if (i == "costs") 
+				return(x@props$costs)
+			if (i == "positive") 
+				return(x@props$positive)
+			if (i == "negative") 
+				return(x@props$negative)
 			callNextMethod()
 		}
 )
