d3b55784576acae44f27f2ddbd8d6078cfe15cfd
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-28 11:39:59 +0000

    refactor desc objects

--- src/base/data.desc.r
+++ src/base/data.desc.r
@@ -1,6 +1,29 @@
 #' @include object.r
 roxygen()
 
+#' Description object for data.
+#' 
+#' Getter.\cr
+#' 
+#' \describe{
+#'  \item{size [integer]}{Number of cases.}
+#'  \item{dim [integer]}{Number of covariates.}
+#'  \item{excluded [character]}{Names of excluded variables.}
+#'  \item{n.num [integer]}{Number of numerical covariates.}
+#'  \item{n.int [integer]}{Number of integer covariates.}
+#'  \item{n.fact [integer]}{Number of factor covariates.}
+#'  \item{n.char [integer]}{Number of character covariates.}
+#' 	\item{has.missing [boolean]}{Are missing values present?}
+#'  \item{is.classif [logical]}{Factor target variable?}
+#' 	\item{is.regr [boolean]}{Numerical target variable?}
+#'  \item{class.levels [character]}{Possible classes. NA if not classification.}
+#'  \item{class.nr [integer]}{Number of classes. NA if not classification.}
+#'  \item{class.dist [integer]}{Class distribution. Named vector. NA if not classification.}
+#'	\item{is.binary [logical]}{Binary classification?. NA if not classification.}
+#' }
+#' @exportClass data.desc
+#' @title Description object for data. 
+#' 
 
 setClass(
 		"data.desc",
@@ -12,25 +35,29 @@ setClass(
 
 
 setMethod(
-  f = "initialize",
-  signature = signature("data.desc"),
-  def = function(.Object, data, target, excluded) {
-	  i = which(colnames(data) %in% c(target, excluded))
-	  df2 = data[, -i, drop=F]
-	  
-	  .Object@props$target = target 
-	  .Object@props$obs = nrow(data)
-	  .Object@props$is.classification <- is.factor(data[, target])
-	  .Object@props$rows.with.missings <- sum(apply(df2, 1, function(x) any(is.na(x))))
-	  .Object@props$cols.with.missings <- sum(apply(df2, 2, function(x) any(is.na(x))))
-	  .Object@props$numerics <- sum(sapply(df2, is.numeric))
-      .Object@props$integers <- sum(sapply(df2, is.integer))
-      .Object@props$factors <- sum(sapply(df2, is.factor))
-      .Object@props$characters <- sum(sapply(df2, is.character))
-	  if(.Object@props$is.classification)	
-	  	.Object@props$classes = levels(data[, target])
-	  return(.Object)
-  }
+		f = "initialize",
+		signature = signature("data.desc"),
+		def = function(.Object, data, target, excluded) {
+			i = which(colnames(data) %in% c(target, excluded))
+			df2 = data[, -i, drop=F]
+			.Object@props$target = target 
+			.Object@props$obs = nrow(data)
+			inputs = c()
+			inputs = c(
+					n.num = sum(sapply(df2, is.numeric)), 
+					n.int  = sum(sapply(df2, is.integer)),
+					n.fact = sum(sapply(df2, is.factor)),
+					n.char = sum(sapply(df2, is.character))
+			)
+			.Object@props$inputs = inputs
+			.Object@props$has.missing = any(is.na(iris))
+			y = data[, target]
+			if(is.factor(y))
+				.Object@props$classes =	{tab=table(y);cl=as.integer(tab); names(cl)=names(tab);cl}
+			else
+				.Object@props$classes =	as.integer(NA)
+			return(.Object)
+		}
 )
 
 
@@ -39,46 +66,41 @@ setMethod(
 		f = "[",
 		signature = signature("data.desc"),
 		def = function(x,i,j,...,drop) {
-			if (i == "size") {
-				return(x["obs"])
-			}
-			if (i == "class.levels") {
-				return(x["classes"])
-			}
-			if (i == "class.nr") {
-				return(length(x["class.levels"]))
-			}
-			if (i == "is.binary") {
-				return(x["class.nr"] == 2)
-			}
-			if (i == "has.missing") {
-				return(x["rows.with.missings"] > 0)
-			}
-			return(x@props[[i]])
-			#callNextMethod()
+			if (i == "size") 
+				return(x@props$obs)
+			if (i == "dim") 
+				return(sum(x@props$inputs))
+			if (i == "excluded") 
+				return(x@props$exlcuded)
+			if (i == "n.num") 
+				return(as.integer(x@props$inputs["n.num"]))
+			if (i == "n.int") 
+				return(as.integer(x@props$inputs["n.int"]))
+			if (i == "n.fact") 
+				return(as.integer(x@props$inputs["n.fact"]))
+			if (i == "n.char") 
+				return(as.integer(x@props$inputs["n.char"]))
+			if (i == "has.missing") 
+				return(length(x["rows.with.missings"]) > 0)
+			if (i == "is.classif") 
+				return(all(!is.na(x@props$classes)))
+			if (i == "is.regr") 
+				return(!x["is.classif"])
+			if (i == "class.levels") 
+				if(x["is.classif"]) return(names(x@props$classes)) else return(as.character(NA))
+			if (i == "class.nr") 
+				if(x["is.classif"]) return(length(x@props$classes)) else return(as.integer(NA))
+			if (i == "class.dist") 
+				if(x["is.classif"]) return(x@props$classes) else return(as.integer(NA))
+			if (i == "is.binary") 
+				if(x["is.classif"]) return(x["class.nr"] == 2) else return(as.logical(NA))
+			callNextMethod()
 		}
 )
 
 
 
 
-
-#' @rdname to.string
-setMethod(
-		f = "to.string",
-		signature = signature("data.desc"),
-		def = function(x) {
-			return(
-					paste( 
-							"Features Nums:", x["numerics"], " Ints:", x["integers"], " Factors:", x["factors"], " Chars:", x["characters"], "\n",
-							"Observations: ", x["obs"] , "\n",
-							"Missings: ", x["has.missing"], "\n", 
-							ifelse(x["has.missing"], paste("in", x["rows.with.missings"], "observations and", x["cols.with.missings"], "features\n"), ""), 
-							sep=""
-					)
-			)
-		}
-)
 
 
 
