baadc6c21c44cc470b24b2bde0c70a2e97af6071
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-02 22:52:26 +0000

    getter for learner + docs

--- src/base/optml/opt.result.r
+++ src/base/optml/opt.result.r
@@ -7,13 +7,14 @@
 #' Getter.\cr
 #' 
 #' \describe{
-#'   \item{par [list | character]}{Named list of hyperparameter values
-#'     or character vector of variables, id#entified as optimal.}
-#'   \item{perf [numeric]}{Performance values of 'par'.}
-#'   \item{path [list | data.frame]. Optional parameters: as.data.frame}{
-#'      Optimization path. Can be converte#d to a data.frame if as.data.frame is TRUE.}
-#'   \item{model [wrapped.model]}{Model fitted with settings in 'par'. Will
-#'     be NULL, if fitting was not req#uested.}
+#'  \item{opt.type [string]}{Currently 'tune' or 'varsel'.}
+#'  \item{par [list | character]}{Named list of hyperparameter values or character vector of variables, identified as optimal.}
+#'	\item{tuned.par [list]}{If tuning was performed, best found set of hyperparameters.}
+#'	\item{sel.vars [character]}{If variable selection was performed, best found set of variables.}
+#'  \item{perf [numeric]}{Performance values of 'par'.}
+#'  \item{path [list | data.frame]. Optional parameters: as.data.frame}{Optimization path. Can be converted to a data.frame if as.data.frame is TRUE.}
+#'  \item{model [\code{\linkS4class{wrapped.model}}]}{Model fitted with settings in 'par'. Will be NULL, if fitting was not requested.}
+#'  \item{learner [\code{\linkS4class{wrapped.model}}]}{Learner with settings in 'par'. Currently only supported for hyperparameter tuning.}
 #' }
 #' 
 #' @exportClass opt.result
@@ -71,15 +72,24 @@ setMethod(
 			if (i == "perf") {
 				return(x@opt$perf)
 			}
-      if (i == "learner") {
-        return(make.learner(x@learner, parset=x@opt$par))
-      }
+			if (i == "learner") {
+				if (x["opt.type"] != "tune")
+					return(NULL)
+				wl = set.hyper.pars(x@learner, x["tuned.par"])
+				return(wl)
+			}
 			if (i == "path") {
 				ys = x@path
 				if (!is.null(args$as.data.frame) && args$as.data.frame) {
 					ys = path2dataframe(ys)			
 				}
 				return(ys)
+			}
+			if (i == "model"){
+				# todo: bad style to determine no model fit was requested
+				if (is.null(x@model@data.desc))
+					return(NULL)
+				return(x@model)
 			}
 			callNextMethod(x,i,j,...,drop=drop)
 		}
