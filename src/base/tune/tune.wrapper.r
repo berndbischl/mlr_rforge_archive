11c88b2e4e4a9b4c57a66d48ad07a90aedbb931b
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-06 22:49:45 +0000

    set label and id

--- src/base/tune/tune.wrapper.r
+++ src/base/tune/tune.wrapper.r
@@ -6,6 +6,10 @@
 #'
 #' @param learner [\code{\linkS4class{learner}} or string]\cr 
 #'        Learning algorithm. See \code{\link{learners}}.  
+#' @param id [string] \cr
+#'        Id for resulting learner object. If missing, id of "learner" argument is used.
+#' @param label [string] \cr
+#'        Label for resulting learner object. If missing, label of "learner" argument is used.
 #' @param resampling [\code{\linkS4class{resample.instance}}] or [\code{\linkS4class{resample.desc}}]\cr
 #'        Resampling strategy to evaluate points in hyperparameter space.
 #' @param control 
@@ -23,7 +27,7 @@
 #'   
 #' @title Fuse learner with tuning.
 
-make.tune.wrapper <- function(learner, resampling, control, measures, aggr) {
-	make.opt.wrapper(learner, resampling, control, measures, aggr)
+make.tune.wrapper <- function(learner, id=as.character(NA), label=as.character(NA), resampling, control, measures, aggr) {
+	make.opt.wrapper(learner, id, label, resampling, control, measures, aggr)
 }
 
