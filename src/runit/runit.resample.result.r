0679abc73182c67ea88e99f2ff82d7df7b9ec282
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-17 17:41:48 +0000

    better test for resampling

--- src/runit/runit.resample.result.r
+++ src/runit/runit.resample.result.r
@@ -5,40 +5,28 @@ test.resample.prediction = function() {
 	rin2 <- make.res.instance("cv", multiclass.task, iters=7)
 	rin3 <- make.res.instance("subsample", multiclass.task, iters=2)
 	
-	ct <- make.task(data=multiclass.df, target=multiclass.target)
+	p1 = resample.fit("classif.lda", multiclass.task, rin1)       
+	p2 = resample.fit("classif.lda", multiclass.task, rin2)       
+	p3 = resample.fit("classif.lda", multiclass.task, rin3)       
 	
-	result1 <- resample.fit("classif.lda", ct, rin1)       
-	result2 <- resample.fit("classif.lda", ct, rin2)       
-	result3 <- resample.fit("classif.lda", ct, rin3)       
-	
-	checkEquals(result1["iters"], 4)
-	checkEquals(result2["iters"], 7)
-	checkEquals(result3["iters"], 2)
+	checkEquals(p1["iters"], 4)
+	checkEquals(p2["iters"], 7)
+	checkEquals(p3["iters"], 2)
 
-	checkEquals(result3["iter"], rep(c(1,2), each=50))
+	checkEquals(p3["iter"], rep(c(1,2), each=50))
 	
-	cc <- ct["target.col"]
-	
-#	for (i in 1:rin1["iters"]) {
-#		checkEquals(rin1["train.inds", i], result1["train.inds", i])
-#		checkEquals(rin1["test.inds", i], result1["test.inds", i])
-#		checkEquals(result1["targets.train", i], multiclass.df[result1["train.inds", i], cc])
-#		checkEquals(result1["targets.test", i], multiclass.df[result1["test.inds", i], cc])
-#	}		
-#		
-#	for (i in 1:rin2["iters"]) {
-#		checkEquals(rin2["train.inds", i], result2["train.inds", i])
-#		checkEquals(rin2["test.inds", i],  result2["test.inds", i])
-#		checkEquals(result2["targets.train", i], multiclass.df[result2["train.inds", i], cc])
-#		checkEquals(result2["targets.test", i], multiclass.df[result2["test.inds", i], cc])
-#	}		
-#		
-#	for (i in 1:rin3["iters"]) {
-#		checkEquals(rin3["train.inds", i], result3["train.inds", i])
-#		checkEquals(rin3["test.inds", i],  result3["test.inds", i])
-#		checkEquals(result3["targets.train", i], multiclass.df[result3["train.inds", i], cc])
-#		checkEquals(result3["targets.test", i], multiclass.df[result3["test.inds", i], cc])
-#	}		
+	inds = Reduce(c, sapply(1:p1["iters"], function(i) get.test.set(rin1, i)$inds))
+	y = multiclass.task["targets", row=inds]
+	checkEquals(p1@df$id, inds)
+	checkEquals(p1@df$truth, y)
+	inds = Reduce(c, sapply(1:p2["iters"], function(i) get.test.set(rin2, i)$inds))
+	y = multiclass.task["targets", row=inds]
+	checkEquals(p2@df$id, inds)
+	checkEquals(p2@df$truth, y)
+	inds = Reduce(c, sapply(1:p3["iters"], function(i) get.test.set(rin3, i)$inds))
+	y = multiclass.task["targets", row=inds]
+	checkEquals(p3@df$id, inds)
+	checkEquals(p3@df$truth, y)
 }
 
 
