7e9d49d968b0c7ebabf853a0709e5079356bcae8
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-18 18:14:02 +0000

    docs

--- src/base/errorhandler.r
+++ src/base/errorhandler.r
@@ -1,15 +1,23 @@
-#' Sets up the errorhandler system of mlr. 
+#' Sets up the error handling system of mlr. 
 #' 
 #' @param on.learner.error [string] \cr
-#'   What should happen if an error in an underlying learning algorithm is caught: 'stop', 'warn' or quiet. Default is 'warn'. 
+#' What should happen if an error in an underlying learning algorithm is caught: 'stop', 'warn' or 'quiet'.\cr 
+#' 'stop': R exception is generated.\cr
+#' 'warn': Model \code{\linkS4class{learner.failure}} will be created, which predicts only NAs and a warning will be generated.\cr 
+#' 'quiet': Same as 'warn' but withou the warning.\cr
+#' Default is 'warn'. 
 #' @param on.par.without.desc [string] \cr
-#'   What should happen if a parameter of a learner is set to a value, but no parameter description object exists, indicating a possibly wrong name: 'stop', 'warn' or quiet. Default is 'warn'. 
+#' What should happen if a parameter of a learner is set to a value, but no parameter description object exists, indicating a possibly wrong name: 'stop', 'warn' or quiet.\cr
+#' 'stop': R exception is generated.\cr
+#' 'warn': Warning, but parameter is still passed along to learner.\cr 
+#' 'quiet': Same as 'warn' but without the warning.\cr
+#' Default is 'warn'. 
 #' @param on.convert.var [string] \cr
-#'    What should happen if an a variable is converted during the creation of a \code{\linkS4class{learn.task}}: 'warn' or 'quiet'. Default is 'warn'.
-#' 
-#' on.learner.error: 'stop': R exception is generated. 'warn': Model \code{\linkS4class{learner.failure}} will be created, which predicts only NAs and a warning will be generated. 'quiet': Same as 'warn' but withou the warning.\cr  
-#' on.par.without.desc: What should happen if a parameter of a learner is set to a value, but no parameter description object exists, indicating a possibly wrong name. 'stop': R exception is generated. 'warn': Warning, but parameter is still passed along to learner. 'quiet': Same as 'warn' but withou the warning.\cr  
-#' on.convert.var: In some cases variables are converted during the creation of the learn.task. Currently, integers are converted to numerics, characters to factors and for classification, target levels without corresponding observation are dropped. 'warn': Warning on conversion. 'quiet': Conversion done quietly.\cr
+#' What should happen if an a variable is converted during the creation of a \code{\linkS4class{learn.task}}: 'warn' or 'quiet'.\cr 
+#' Currently, integers are converted to numerics, characters to factors and for classification, target levels without corresponding observation are dropped.\cr 
+#' 'warn': Warning on conversion.\cr
+#' 'quiet': Conversion done quietly.\cr
+#' Default is 'warn'.
 #'  
 #' @return NULL.
 #' @export
