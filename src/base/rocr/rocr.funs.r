0e3c721406ba2b1a8b6590911d899cf4bb15a83c
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-19 16:08:11 +0000

    rocr stuff

--- src/base/rocr/rocr.funs.r
+++
@@ -1,6 +0,0 @@
-
-ROCR.performance = function(prediction.obj, measure, x.measure = "cutoff", ...) {
-	f = get("performance", envir=getNamespace("ROCR"))
-	f(prediction.obj, measure, x.measure, ...)	
-} 
-	
