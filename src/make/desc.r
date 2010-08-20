a5a05ed10d3514e8014dea8ec6db962731bcba41
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-20 18:20:15 +0000

    remove rocr dependency

--- src/make/desc.r
+++ src/make/desc.r
@@ -15,7 +15,7 @@ write.desc <- function(desc.path, rev.nr) {
 		"Description: no\n",
 		"License: GPL (>= 2)\n",
 		"LazyLoad: yes\n",
-		"Depends: R (>= 2.8.0), methods, reshape, abind, boot, klaR, e1071, digest, ROCR\n",
+		"Depends: R (>= 2.8.0), methods, reshape, abind, boot, klaR, e1071, digest\n",
 		"Suggests: MASS, snowfall, multicore, mlbench, cmaes, kernlab, rpart, randomForest, adabag, kknn, ada, adabag, party, mboost, mda, gbm, nnet, penalized, RWeka, grplasso, earth, sda, pls, foreign\n",
 		sep=""
 	)
