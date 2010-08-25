f9c8dd70e01676cf866987d7c3298b5864387797
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-25 13:55:29 +0000

    fix bugs in helpers: tests

--- src/runit/runit.helpers.r
+++ src/runit/runit.helpers.r
@@ -10,7 +10,16 @@ test.helpers = function() {
 	checkTrue(check.list.type(xs, c("character", "numeric")))
 	checkException(check.list.type(xs, c("learner", "logical")))
 	
-
 	xs = list(make.learner("classif.rpart"))
 	checkTrue(check.list.type(xs, c("rlearner")))
+	
+	checkTrue(all.names(list()))
+	checkTrue(all.names(list(x=1)))
+	checkTrue(all.names(list(x=1, y=2)))
+	checkTrue(!all.names(list(1,2)))
+	xs = list(1,2)
+	names(xs)[1] = "a"
+	checkTrue(!all.names(xs))
+	names(xs)[2] = "b"
+	checkTrue(all.names(xs))
 }
