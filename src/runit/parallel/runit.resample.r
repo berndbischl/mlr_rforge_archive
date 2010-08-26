67fe907b5dbe4954d893de07c24d95788f2b906f
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-26 21:12:37 +0000

    fix test

--- src/runit/parallel/runit.resample.r
+++ src/runit/parallel/runit.resample.r
@@ -1,11 +1,14 @@
 
-wl = make.learner("classif.lda", predict.type="prob")
-res = make.res.instance("subsample", iters=4, task=binaryclass.task)
 
-parallel.setup(mode="local")
-p1 = resample.fit(wl, binaryclass.task, res)
-parallel.setup(mode="multicore", level="resample", cpus=2)
-p2 = resample.fit(wl, binaryclass.task, res)
+test.multicore.resample = function() {
+  wl = make.learner("classif.lda", predict.type="prob")
+  res = make.res.instance("subsample", iters=4, task=binaryclass.task)
 
-checkEquals(p1["response"], p2["response"])
+  parallel.setup(mode="local")
+  p1 = resample.fit(wl, binaryclass.task, res)
+  parallel.setup(mode="multicore", level="resample", cpus=2)
+  p2 = resample.fit(wl, binaryclass.task, res)
+
+  checkEquals(p1["response"], p2["response"])
+}
 
