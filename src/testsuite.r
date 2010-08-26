3d925d6ae52efd390590c8fd0887e82385afcd76
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-26 17:19:37 +0000

    unit tests for multicore

--- src/testsuite.r
+++ src/testsuite.r
@@ -3,65 +3,13 @@
 source("src/runit/helpers.r")
 source("src/runit/make.runit.tests.r")
 
-
-do.testsuite = function(dirs, regexp) {
-  parallel.setup(mode="local")
-  logger.setup(level="error")
-  errorhandler.setup()
-  
-  data(Sonar, BreastCancer)
-  
-  binaryclass.df <- Sonar
-  binaryclass.formula <- Class~.
-  binaryclass.target = "Class"
-  binaryclass.train.inds <- c(1:50, 100:150)
-  binaryclass.test.inds  <- setdiff(1:nrow(binaryclass.df), binaryclass.train.inds)
-  binaryclass.train <- binaryclass.df[binaryclass.train.inds, ]
-  binaryclass.test  <- binaryclass.df[binaryclass.test.inds, ]
-  binaryclass.class.col <- 61
-  binaryclass.class.levs <- levels(binaryclass.df[, binaryclass.class.col])
-  binaryclass.task = make.task("binary", data=binaryclass.df, target=binaryclass.target)  
-  
-  multiclass.df <- iris
-  multiclass.formula <- Species~.
-  multiclass.target = "Species"
-  multiclass.train.inds <- c(1:30, 51:80, 101:130)
-  multiclass.test.inds  <- setdiff(1:150, multiclass.train.inds)
-  multiclass.train <- multiclass.df[multiclass.train.inds, ]
-  multiclass.test  <- multiclass.df[multiclass.test.inds, ]
-  multiclass.class.col <- 5
-  multiclass.task = make.task("multiclass", data=multiclass.df, target=multiclass.target)  
-  
-  data(BostonHousing)
-  regr.df <- BostonHousing  
-  regr.formula <- medv ~ . 
-  regr.target = "medv"
-  regr.train.inds <- seq(1, 506, 3)
-  regr.test.inds  <- setdiff(1:nrow(regr.df), regr.train.inds)
-  regr.train <- regr.df[regr.train.inds, ]
-  regr.test  <- regr.df[regr.test.inds, ]
-  regr.task <- make.task("regrtask", data=BostonHousing, target="medv")  
-  
-  .mlr.local$debug.seed <- 12345
-  debug.seed <<- .mlr.local$debug.seed
-  
-  testsuite.mlr <- defineTestSuite("mlr",
-      dirs = dirs,  
-      testFileRegexp = regexp
-  )
-  
-  testResult <- runTestSuite(testsuite.mlr)
-  
-  printTextProtocol(testResult)
-}
-
-
 if (use.package) {
   message("Using installed copy of mlr for tests!")
   require("mlr")
   require("mlbench")
   require("RUnit")
-  do.testsuite(c("src/runit", "src/runit/parallel"),  "^runit.*")
+  ts.dirs = c("src/runit", "src/runit/parallel")  
+  ts.file.regexp = "^runit.*"
 } else {
   if (file.exists("src/testsuite.config.r")) {
     source("src/testsuite.config.r")    
@@ -77,30 +25,51 @@ if (use.package) {
 } 
 
 
+parallel.setup(mode="local")
+logger.setup(level="error")
+errorhandler.setup()
 
+data(Sonar, BreastCancer)
 
-#data(Glass)
-#testsuite.df <- Glass
-#testsuite.formula <- Type~.
+binaryclass.df <- Sonar
+binaryclass.formula <- Class~.
+binaryclass.target = "Class"
+binaryclass.train.inds <- c(1:50, 100:150)
+binaryclass.test.inds  <- setdiff(1:nrow(binaryclass.df), binaryclass.train.inds)
+binaryclass.train <- binaryclass.df[binaryclass.train.inds, ]
+binaryclass.test  <- binaryclass.df[binaryclass.test.inds, ]
+binaryclass.class.col <- 61
+binaryclass.class.levs <- levels(binaryclass.df[, binaryclass.class.col])
+binaryclass.task = make.task("binary", data=binaryclass.df, target=binaryclass.target)  
 
-#testsuite.train.inds <- seq(1L,214L, 2L)
-#testsuite.test.inds  <- seq(2L,214L, 2L)
+multiclass.df <- iris
+multiclass.formula <- Species~.
+multiclass.target = "Species"
+multiclass.train.inds <- c(1:30, 51:80, 101:130)
+multiclass.test.inds  <- setdiff(1:150, multiclass.train.inds)
+multiclass.train <- multiclass.df[multiclass.train.inds, ]
+multiclass.test  <- multiclass.df[multiclass.test.inds, ]
+multiclass.class.col <- 5
+multiclass.task = make.task("multiclass", data=multiclass.df, target=multiclass.target)  
 
+data(BostonHousing)
+regr.df <- BostonHousing  
+regr.formula <- medv ~ . 
+regr.target = "medv"
+regr.train.inds <- seq(1, 506, 3)
+regr.test.inds  <- setdiff(1:nrow(regr.df), regr.train.inds)
+regr.train <- regr.df[regr.train.inds, ]
+regr.test  <- regr.df[regr.test.inds, ]
+regr.task <- make.task("regrtask", data=BostonHousing, target="medv")  
 
-#
-#testsuite.train <- testsuite.df[testsuite.train.inds, ]
-#testsuite.test  <- testsuite.df[testsuite.test.inds, ]
-#
-#testsuite.class.col <- 10
-#
-#debug.seed <- 12345
-#
-#testsuite.mlr <- defineTestSuite("mlr",
-#  dirs = ts.dirs,  
-#  testFileRegexp = ts.file.regexp
-#)
-#
-#testResult <- runTestSuite(testsuite.mlr)
-#
-#
-#printTextProtocol(testResult)
+.mlr.local$debug.seed <- 12345
+debug.seed <<- .mlr.local$debug.seed
+
+testsuite.mlr <- defineTestSuite("mlr",
+    dirs = ts.dirs,  
+    testFileRegexp = ts.file.regexp
+)
+
+testResult <- runTestSuite(testsuite.mlr)
+
+printTextProtocol(testResult)
