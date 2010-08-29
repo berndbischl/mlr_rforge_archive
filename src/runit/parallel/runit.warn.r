4f40a2cffbe27cd35bb347fbccb875636b02d13e
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-29 14:26:10 +0000

    runit: parallel test warnings

--- src/runit/parallel/runit.warn.r
+++ src/runit/parallel/runit.warn.r
@@ -2,18 +2,21 @@
 
 test.parallel.warn = function() {
   res = make.res.instance("cv", iters=2, task=binaryclass.task)
-
-  #todo: for some reasons i get strange erorrs here 
+  level = .mlr.local$logger.setup$global.level
+  logger.setup(level="warn")
+  opwd = .mlr.local$errorhandler.setup$on.par.without.desc
   
+  #todo: for some reasons i get strange erorrs here 
 #  parallel.setup(mode="multicore", level="resample", cpus=2)
 #  checkWarning(
 #      resample.fit("classif.rpart", binaryclass.task, res, par.vals=list(foo=1)),
 #      "Setting par foo without"  
 #  )
   
+  errorhandler.setup(on.par.without.desc="warn")
   parallel.setup(mode="snowfall", level="resample", cpus=2)
   checkWarning(
-      resample.fit("classif.rpart", binaryclass.task, res, par.vals=list(foo=1)),
+      resample.fit("classif.ksvm", binaryclass.task, res, par.vals=list(foo=1)),
       "Setting par foo without"  
   )
   
@@ -27,10 +30,12 @@ test.parallel.warn = function() {
   
   parallel.setup(mode="snowfall", level="resample", cpus=2)
   checkWarning(
-      resample.fit("classif.rpart", binaryclass.task, res, par.vals=list(foo=1)),
-      "Setting par foo without"  
+    resample.fit("classif.ksvm", binaryclass.task, res, par.vals=list(foo=1)),
+     F
   )
+  
+  logger.setup(level=level)
+  errorhandler.setup(on.par.without.desc=opwd)
   parallel.setup(mode="local")
-  errorhandler.setup()
 }
 
