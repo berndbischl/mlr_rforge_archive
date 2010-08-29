32fbbb6de765b2c07562a62f773e8e07f8d9787e
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-29 18:02:50 +0000

    parallelization of cmaes

--- src/base/tune/tune.cmaes.r
+++ src/base/tune/tune.cmaes.r
@@ -13,15 +13,31 @@ tune.cmaes = function(learner, task, resampling, measures, aggr, control) {
 		logger.info(level="tune", paste(ns, "=", p), ":", perf)
 		return(perf)
 	}
-	
+
+  g2 = function(p) {
+    p2 = as.list(as.data.frame(p))
+    p2 = lapply(p2, function(x) {x=as.list(x);names(x)=ns;x})
+    es = eval.states.tune(learner, task, resampling, measures, aggr, control, p2, "optim")
+    path <<- add.path.els.tune(path=list(), ess=es, best=NULL)
+    perf = sapply(es, get.perf)
+    return(perf)
+  }
+  
 	args = control@extra.args
 	
 	ns = names(control["start"])
 	start = as.numeric(control["start"])
 	
-	or = cma_es(par=start, fn=g, lower=control["lower"], upper=control["upper"], control=args)
+  if (.mlr.local$parallel.setup$mode != "local" && .mlr.local$parallel.setup$level == "tune") {
+    g=g2
+    args$vectorized=TRUE    
+  }  
+
+  or = cma_es(par=start, fn=g, lower=control["lower"], upper=control["upper"], control=args)
 	par = as.list(or$par)
 	names(par) = ns
+  print(par)
 	opt = get.path.el(path, par)
+  print(opt)
 	new("opt.result", control=control, opt=opt, path=path)
 }
