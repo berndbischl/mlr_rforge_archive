764a2e966221b07fce153b266f011e5071049ac7
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-01 03:15:41 +0000

    more par.descs for mda

--- src/models/classification/mda.r
+++ src/models/classification/mda.r
@@ -9,75 +9,81 @@ roxygen()
 
 
 setClass(
-		"classif.mda", 
-		contains = c("rlearner.classif")
+  "classif.mda", 
+  contains = c("rlearner.classif")
 )
 
 
 setMethod(
-		f = "initialize",
-		signature = signature("classif.mda"),
-		def = function(.Object) {
-			
-			desc = new("learner.desc.classif",
-					oneclass = FALSE,
-					twoclass = TRUE,
-					multiclass = TRUE,
-					missings = FALSE,
-					numerics = TRUE,
-					factors = TRUE,
-					characters = FALSE,
-					decision = FALSE,
-					probs = TRUE,
-					weights = FALSE,
-					costs = FALSE
-			)
+  f = "initialize",
+  signature = signature("classif.mda"),
+  def = function(.Object) {
+    
+    desc = new("learner.desc.classif",
+      oneclass = FALSE,
+      twoclass = TRUE,
+      multiclass = TRUE,
+      missings = FALSE,
+      numerics = TRUE,
+      factors = TRUE,
+      characters = FALSE,
+      decision = FALSE,
+      probs = TRUE,
+      weights = FALSE,
+      costs = FALSE
+    )
+ 
+    x = callNextMethod(.Object, label="mda", pack="mda", desc=desc, par.descs=par.descs)
+    
+    par.descs <- list(
+      new("par.desc.unknown", par.name="subclasses", default=2L),
+      new("par.desc.num", par.name="iter", default=5L, lower=1L),
+      new("par.desc.num", par.name="dimension", lower=1L),
+      new("par.desc.disc", par.name="method", default="polyreg", 
+        vals=list(polyreg=polyreg, mars=mars, bruto=bruto, gen.ridge=gen.ridge))
+    )
 
-			par.descs <- list(
-				new("par.desc.unknown", par.name="subclasses", default=2L),
-                new("par.desc.num", par.name="iter", when="train", default=5L, lower=1L)
-            )
-			
-			callNextMethod(.Object, label="mda", pack="mda", desc=desc, par.descs=par.descs)
-		}
+    x@par.descs = par.descs
+    return(x)
+  }
 )
 
 #' @rdname train.learner
 
 
 setMethod(
-		f = "train.learner",
-		signature = signature(
-				.learner="classif.mda", 
-				.targetvar="character", 
-				.data="data.frame", 
-				.data.desc="data.desc", 
-				.task.desc="task.desc", 
-				.weights="numeric", 
-				.costs="matrix" 
-		),
-		
-		def = function(.learner, .targetvar, .data, .data.desc, .task.desc, .weights, .costs,  ...) {
-			f = as.formula(paste(.targetvar, "~."))
-			mda(f, data=.data, ...)
-		}
+  f = "train.learner",
+  signature = signature(
+    .learner="classif.mda", 
+    .targetvar="character", 
+    .data="data.frame", 
+    .data.desc="data.desc", 
+    .task.desc="task.desc", 
+    .weights="numeric", 
+    .costs="matrix" 
+  ),
+  
+  def = function(.learner, .targetvar, .data, .data.desc, .task.desc, .weights, .costs,  ...) {
+    f = as.formula(paste(.targetvar, "~."))
+    mda(f, data=.data, ...)
+  }
 )
 
 #' @rdname pred.learner
 
 setMethod(
-		f = "pred.learner",
-		signature = signature(
-				.learner = "classif.mda", 
-				.model = "wrapped.model", 
-				.newdata = "data.frame", 
-				.type = "character" 
-		),
-		
-		def = function(.learner, .model, .newdata, .type, ...) {
-			.type <- ifelse(.type=="response", "class", "posterior")
-			predict(.model["learner.model"], newdata=.newdata, type=.type, ...)
-		}
+  f = "pred.learner",
+  signature = signature(
+    .learner = "classif.mda", 
+    .model = "wrapped.model", 
+    .newdata = "data.frame", 
+    .type = "character" 
+  ),
+  
+  def = function(.learner, .model, .newdata, .type, ...) {
+    .type <- ifelse(.type=="response", "class", "posterior")
+    predict(.model["learner.model"], newdata=.newdata, type=.type, ...)
+  }
 )	
 
 
