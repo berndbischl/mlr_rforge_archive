c8763bc2850594655315a16e96794ec6675debb2
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-06 02:16:17 +0000

    src dir for optimization stuff. currently not integrated

---
+++ src/base/optimize/ea1pluslambda.r
@@ -0,0 +1,27 @@
+ea1pluslambda = function(f, par.descs, control) {
+  m = length(par.descs)
+  curx = list()
+  cury = f(curx)
+  step.sizes = list()
+  for (i in 1:m) {
+    pd = par.descs[[i]]
+    if (is(pd, "par.desc.num")) 
+      step.sizes[[pd["par.name"]]] = (pd["upper"] - pd["lower"]) / 100
+  }
+  step.sizes = as.numeric(step.sizes)
+  print(step.sizes)
+  
+  offspring = list()
+  for (i in 1:m) {
+    pd = par.descs[[i]]
+    if (is(pd, "par.desc.num")) 
+      step.sizes[[pd["par.name"]]] = (pd["upper"] - pd["lower"]) / 100
+  }
+  #ys = sapply(1:nrow(offspring), function(i) do.call(f, data.frame.row.to.list(offspring, i)))
+  ys = f(offspring)
+  j = which.min(ys)
+  if (ys[j] < cury) {
+    cury = ys[j]
+    curx = data.frame.row.to.list(offspring, i)
+  }
+}
