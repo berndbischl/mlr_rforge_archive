6ccaaa35126352d04c66061e3d0fd47b8869e23d
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-06 04:45:12 +0000

    refactor myspo so it works not only for learner tuning but arbitrary funs

---
+++ src/base/optimize/myspo/control.myspo.opt.r
@@ -0,0 +1,8 @@
+myspo.optcontrol = function(minimize=TRUE, par.descs, scale=identity, 
+  meta.learner="regr.randomForest", constr.learner="classif.randomForest", 
+  init.des.points=50L, seq.des.points=10000L, seq.loops=500, ...) {
+  list(minimize=minimize, par.descs=par.descs, scale=identity, 
+    meta.learner=meta.learner, constr.learner=constr.learner, 
+    init.des.points=init.des.points, seq.des.points=seq.des.points, seq.loops=seq.loops)
+}
+
