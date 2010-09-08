99d4737142273b2768ce811ab4c54fd4ab32e714
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-08 12:03:44 +0000

    stub code for data methods

---
+++ src/base/data/arff.to.task.r
@@ -0,0 +1,8 @@
+# file: filepath or url
+# target: can be more than on. first one is selected, rest excluded
+# ids = names
+# handlde.handle.ids = remove / exlucde option
+# handlde.2nd.targets = remove / exlucde option
+
+arff.to.task = function(file, target, ids, handle.ids, handle.2nd.targets)
+  
---
+++ src/base/data/get.uci.task.r
@@ -0,0 +1,1 @@
+#code to make default tasks
