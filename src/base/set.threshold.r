0b735381e6016625d131dd512af0c071e26778ff
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-29 13:29:37 +0000

    refactor resampling

--- src/base/set.threshold.r
+++ src/base/set.threshold.r
@@ -1,34 +1,55 @@
-#' Set threshold of prediction object. 
+#' Set threshold of prediction object (if prediction was for classification). 
+#' Creates corresponding discrete class response for the newly set threshold. 
+#' For binary classification: The positive class is predicted if the probability or decision value exceeds the threshold. 
+#' For multiclass: Probabilities/decision values are divided by corresponding thresholds and the class with maximum resulting value is selected. 
 #' 
-#' @param learner [\code{\linkS4class{learner}}]\cr 
-#'        Learner object.   
-#' @param id [string] \cr
-#'       New id.
+#' @param pred [\code{\linkS4class{prediction}}] \cr
+#'   Prediction object.
+#' @param threshold [numeric] \cr
+#'   Threshold to produce class labels. For multiclass classification has to be a named vector, where names correspond to class labels.
+#'   For binary classification it should be a single numerical value, but the former format for multiclass is also allowed. 
 #' 		    
-#' @return \code{\linkS4class{learner}} with changed id.
-#' @exportMethod set.id
-#' @title Set id of learner object.
-#' @rdname set.id 
+#' @return \code{\linkS4class{prediction}} with changed threshold and corresponding response.
+#' @exportMethod set.threshold
+#' @title Set threshold of prediction object.
+#' @rdname set.threshold 
 
 setGeneric(
-  name = "set.id",
-  def = function(learner, id) {
-    standardGeneric("set.id")			
+  name = "set.threshold",
+  def = function(pred, threshold) {
+    standardGeneric("set.threshold")			
   }
 )
 
-#' @rdname set.id 
+#' @rdname set.threshold 
 setMethod(
-  f = "set.id",
+  f = "set.threshold",
   
   signature = signature(
-    learner="learner", 
-    id="character" 
+    pred = "prediction", 
+    threshold = "numeric" 
   ),
   
-  def = function(learner, id) {
-    learner@id = id
-    return(learner)
+  def = function(pred, threshold) {
+    dd = pred@data.desc
+    td = pred@data.desc
+    if (!td["is.classif"])
+      stop("Threshold can only be set for classification predictions!")
+    if (pred["type"] != "prob")
+      stop("Threshold can currently only be set for type 'prob'!")
+    levs = dd["class.levels"]
+    if (length(levs) == 2 && is.numeric(threshold) && length(threshold) == 1) {
+      threshold = c(threshold, 1-threshold)
+      names(threshold) = c(td["positive"], td["negative"])   
+    }
+    p = pred["prob", classes=levs]
+    # resort so we have same order in threshold and p
+    threshold = treshold[levs] 
+    p = apply(p, 1, function(x) x/threshold)
+    resp = sapply(1:nrow(y), function(i) vote.max.val(p[i,]/threshold, levs))
+    resp = factor(resp, levels=levs)
+    pred@df$response = resp
+    pred@threshold = threshold
   } 
 )
 
