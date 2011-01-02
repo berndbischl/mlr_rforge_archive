ec51901f6999375e821c148ccd92805713b929a0
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 04:01:25 +0000

    tune.control: to.string method

--- src/base/tune/control.tune.r
+++ src/base/tune/control.tune.r
@@ -47,14 +47,55 @@ setMethod(
 			}
 			if (i == "lower") {
 				y = sapply(pds, function(y) ifelse(is(y, "par.desc.num"), y@lower, NA))
-				names(y) = x["par.names"] 
-				return(y)
-			}
+				names(y) = x["par.names"]
+        return(Filter(function(w) !is.na(w), y))
+      }
 			if (i == "upper") {
 				y = sapply(pds, function(y) ifelse(is(y, "par.desc.num"), y@upper, NA))
 				names(y) = x["par.names"] 
-				return(y)
-			}		
-			callNextMethod(x,i,j,...,drop=drop)
+        return(Filter(function(w) !is.na(w), y))
+      }		
+      if (i == "vals") {
+        y = lapply(pds, function(y) if(is(y, "par.desc.disc")) y@vals else NA)
+        names(y) = x["par.names"] 
+        return(Filter(function(w) !identical(w, NA), y))
+      }   
+      callNextMethod(x,i,j,...,drop=drop)
 		}
 )
+
+
+#' @rdname to.string
+setMethod(
+  f = "to.string",
+  signature = signature("tune.control"),
+  def = function(x) {
+    ns = paste(x["par.names"], collapse=",")    
+    lo = paste(x["lower"], collapse=",")    
+    up = paste(x["upper"], collapse=",")
+    ss = unlist(x["start"])
+    ss =  paste(x["start"], collapse=",")    
+    vals = x["vals"]
+    if (length(vals) > 0) {
+      vals = sapply(vals, function(y) paste("has ", length(y), " vals", sep=""))
+      vals = paste(names(vals), vals)
+      vals = paste("Discrete vals:", paste(vals, collapse=","))
+    } else {
+      vals = ""
+    }
+    
+    return(
+      paste(
+        "Control object for tuning of class: ", class(x), "\n",
+        "Optimized parameters: ", ns, "\n",
+        "Scaling function used: ", !identical(x@scale, identity), "\n",
+        "Lower bounds: ", lo, "\n",
+        "Upper bounds: ", up, "\n",
+        "Start values: ", ss, "\n",
+        vals,       
+        sep=""
+      )
+    )
+  }
+)
+
