0aece361f255b6d5d46d498e58770fd375ce03df
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 02:44:31 +0000

    export aggrs

--- src/base/aggr.r
+++ src/base/aggr.r
@@ -23,26 +23,31 @@ setMethod(
 )
 
 
+#' @export test.mean
 test.mean = new("aggr",
   id = "test.mean",
   fun = function(perf.test, perf.train, group, pred) mean(perf.test)
 )
 
+#' @export test.sd
 test.sd = new("aggr",
   id = "test.sd",
   fun = function(perf.test, perf.train, group, pred) sd(perf.test)
 )
 
+#' @export test.median
 test.median = new("aggr",
   id = "test.median",
   fun = function(perf.test, perf.train, group, pred) median(perf.test)
 )
 
+#' @export test.min
 test.min = new("aggr",
   id = "test.min",
   fun = function(perf.test, perf.train, group, pred) min(perf.test)
 )
 
+#' @export test.max
 test.max = new("aggr",
   id = "test.max",
   fun = function(perf.test, perf.train, group, pred) max(perf.test)
