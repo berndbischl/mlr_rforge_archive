6b0aae00c0158f16754e50115c7a226beeac2a31
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 01:32:55 +0000

    bugs

--- src/base/tune/tune.optim.r
+++ src/base/tune/tune.optim.r
@@ -1,15 +1,18 @@
+# todo: add optimize if only 1 par
 tune.optim = function(learner, task, resampling, measures, control) {
-	start = control["start"]
-	ns = names(start)
-	
-	path = list()
+  path = list()
+  ns = control["par.names"]
+  start = unlist(control["start"])[ns]
+  low = control["lower"]
+  up = control["upper"]
 	
 	g = function(p) {
-		p2 = as.list(p)
+    p2 = as.list(p)
+    names(p2) = ns
 		es = eval.state.tune(learner, task, resampling, measures, control, p2, "optim")
 		path <<- add.path.tune(path, es, accept=TRUE)		
 		perf = get.perf(es)
-		logger.info(level="tune", paste(names(p), "=", p), ":", perf)
+		logger.info(level="tune", paste(ns, "=", p), ":", perf)
 		return(perf)
 	}
 		
@@ -19,10 +22,11 @@ tune.optim = function(learner, task, resampling, measures, control) {
 		method = "Nelder-Mead"
 	args$method = NULL
 	
-	if (method == "L-BFGS-B") {
-		or = optim(par=start, f=g, method=method, lower=control["lower"], upper=control["upper"], control=args)
-	} else 
-		or = optim(par=start, f=g, method=method, control=args)
+  if (method == "L-BFGS-B") {
+    or = optim(par=start, f=g, method=method, lower=low, upper=up, control=args)
+  } else 
+    or = optim(par=start, f=g, method=method, control=args)
+  }
 	par = as.list(or$par)
 	opt = get.path.el(path, par)
 	new("opt.result", control=control, opt=opt, path=path)
