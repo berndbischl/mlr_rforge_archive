894c9fc9fb601097960f3939ea9c956e21fb87c4
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 01:38:55 +0000

    bugs

--- src/base/aggr.r
+++ src/base/aggr.r
@@ -48,25 +48,4 @@ test.max = new("aggr",
   fun = function(perf.test, perf.train, group, pred) max(perf.test)
 )
 
-#' Set how this measure will be aggregated after resampling. 
-#' 
-#' Possible are: test.mean, test.sd, test.median, test.min, test.max
-#' They all calculate the respective statistic of the test set performances. 
-#' 
-#' @param measure [\code{\linkS4class{measure}}]\cr 
-#'        Performance measure.   
-#' @param aggr [\code{\linkS4class{aggr}}] \cr
-#'        Aggregation function.
-#'        
-#' @return \code{\linkS4class{measure}} with changed aggregation behaviour.
-#' @exportMethod set.aggr
-#' @title Set aggregation functions of measure. 
-#' @rdname set.aggr.pars 
-
-set.aggr = function(measure, aggr) {
-  if (!is.list(aggr))
-    aggr = list(aggr)
-  x@aggr = aggr
-  return(x)
-}
 
---
+++ src/base/set.aggr.r
@@ -0,0 +1,40 @@
+#' Set how this measure will be aggregated after resampling. 
+#' 
+#' Possible are: test.mean, test.sd, test.median, test.min, test.max
+#' They all calculate the respective statistic of the test set performances. 
+#' 
+#' @param measure [\code{\linkS4class{measure}}]\cr 
+#'        Performance measure.   
+#' @param aggrs [list of \code{\linkS4class{aggr}}] \cr
+#'        Aggregation functions.
+#'        
+#' @return \code{\linkS4class{measure}} with changed aggregation behaviour.
+#' @exportMethod set.aggr
+#' @title Set aggregation functions of measure. 
+#' @rdname set.aggr 
+
+setGeneric(
+  name = "set.aggr",
+  def = function(measure, aggrs) {
+    if (!is.list(aggrs))
+      aggrs = list(aggrs)
+    standardGeneric("set.aggr")
+  }
+)
+
+#' @rdname set.hyper.pars 
+setMethod(
+  f = "set.aggr",
+  
+  signature = signature(
+    measure="measure", 
+    x="list" 
+  ),
+  
+  def = function(measure, aggrs) {
+    measure@aggr = aggrs
+    return(measure)
+  } 
+)
+
+
