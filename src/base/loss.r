2106d99d34260a771c42fa12550655e047871dcb
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 04:41:03 +0000

    remove losses

--- src/base/loss.r
+++ src/base/loss.r
@@ -1,10 +1,49 @@
-#if (length(losses) > 0) {
-#  ls = lapply(losses, function(f) f(x, task=task))
-#  ls = as.data.frame(Reduce(cbind, ls))
-#  colnames(ls) = names(losses)
-#  if (!is.null(x["id"]))
-#    ls = cbind(id=x["id"], ls)
-#}
+##' @include prediction.r
+#roxygen()
+##' @include measure.r
+#roxygen()
 #
-#if (length(losses) > 0)
-#  return(list(measures=ms, losses=ls))
+##' Measures the quality of predictions w.r.t. some performance measures.
+##' 
+##' @param pred [\code{\linkS4class{prediction}}] \cr
+##'   Prediction object to evaluate.
+##' 
+##' @return A single numerical performance value.
+##' 
+##' @exportMethod performance
+##' @rdname performance
+##'
+##' @title Measure performance.
+#
+#
+#
+#setGeneric(
+#  name = "loss",
+#  def = function(pred, fun, type) {
+#    if (missing(type))
+#      type = "response"
+#    standardGeneric("loss")
+#  }
+#)
+#
+##' @rdname performance
+#
+#setMethod(
+#  f = "loss",
+#  signature = signature(pred="prediction", fun="function", type="character"),
+#  def = function(pred, fun, type) {
+#    if (type == "response")
+#      fun(pred["truth"], pred["response"])
+#    else if (type == "prob")
+#      fun(pred["truth"], pred["prob"])
+#    else if (type == "decision")
+#      fun(pred["truth"], pred["decision"])
+#    else
+#      stop("Wrong type!")
+#  }
+#)
+#
+#
+#
+#
+
--- src/runit/runit.losses.r
+++ src/runit/runit.losses.r
@@ -1,38 +1,39 @@
-test.losses <- function() {
-
-	costs = matrix(c(0,1,2,0), 2, 2)
-	rownames(costs) = colnames(costs) = c("M", "R")
-	ct = make.task(data=binaryclass.df, target=binaryclass.target, costs=costs, positive="M")
-
-	m = train("classif.lda", task=ct, subset=binaryclass.train.inds)
-	pred = predict(m, task=ct, subset=binaryclass.test.inds)
-	perf1 = performance(pred, losses="zero-one")
-	checkEquals(perf1$measures["mmce"], mean(perf1$losses[, "zero-one"]), checkNames=FALSE)
-	perf2 = performance(pred, task=ct, measures=c("mmce", "fp", "fn", "costs"), losses=c("zero-one", "costs"))
-	checkEquals(perf2$measures["mmce"], mean(perf2$losses[, "zero-one"]), checkNames=FALSE)
-	checkEquals((perf2$measures["fp"] + perf2$measures["fn"]) / length(binaryclass.test.inds), perf2$measures["mmce"], checkNames=FALSE)
-	checkEquals(1*perf2$measures["fp"] + 2*perf2$measures["fn"], perf2$measures["costs"], checkNames=FALSE)
-	
-	m = train("regr.lm", task=regr.task, subset=regr.train.inds)
-	pred = predict(m, task=regr.task, subset=regr.test.inds)
-	perf3 = performance(pred, measures=c("sse", "mse", "medse", "sae", "mae", "medae"), losses=c("squared", "abs", "residual"))
-	checkEquals(perf3$measures["sse"], sum(perf3$losses[, "squared"]), checkNames=FALSE)
-	checkEquals(perf3$measures["sae"], sum(perf3$losses[, "abs"]), checkNames=FALSE)
-	checkEquals(perf3$measures["mse"], mean(perf3$losses[, "squared"]), checkNames=FALSE)
-	checkEquals(perf3$measures["mae"], mean(perf3$losses[, "abs"]), checkNames=FALSE)
-	checkEquals(perf3$measures["medse"], median(perf3$losses[, "squared"]), checkNames=FALSE)
-	checkEquals(perf3$measures["medae"], median(perf3$losses[, "abs"]), checkNames=FALSE)
-	checkEquals(perf3$losses[, "abs"], abs(perf3$losses[, "residual"]), checkNames=FALSE)
-	
-	
-	
-	
+#test.losses <- function() {
+#
+#	costs = matrix(c(0,1,2,0), 2, 2)
+#	rownames(costs) = colnames(costs) = c("M", "R")
+#	ct = make.task(data=binaryclass.df, target=binaryclass.target, costs=costs, positive="M")
+#
+#	m = train("classif.lda", task=ct, subset=binaryclass.train.inds)
+#	pred = predict(m, task=ct, subset=binaryclass.test.inds)
+#	perf1 = performance(pred, losses="zero-one")
+#  loss1 = loss(pred, function(x,y) x!=y)
+#  checkEquals(perf1$measures["mmce"], mean(perf1$losses[, "zero-one"]), checkNames=FALSE)
+#	perf2 = performance(pred, task=ct, measures=c("mmce", "fp", "fn", "costs"), losses=c("zero-one", "costs"))
+#	checkEquals(perf2$measures["mmce"], mean(perf2$losses[, "zero-one"]), checkNames=FALSE)
+#	checkEquals((perf2$measures["fp"] + perf2$measures["fn"]) / length(binaryclass.test.inds), perf2$measures["mmce"], checkNames=FALSE)
+#	checkEquals(1*perf2$measures["fp"] + 2*perf2$measures["fn"], perf2$measures["costs"], checkNames=FALSE)
 #	
-#	res = make.res.desc("cv", iters=3)
+#	m = train("regr.lm", task=regr.task, subset=regr.train.inds)
+#	pred = predict(m, task=regr.task, subset=regr.test.inds)
+#	perf3 = performance(pred, measures=c("sse", "mse", "medse", "sae", "mae", "medae"), losses=c("squared", "abs", "residual"))
+#	checkEquals(perf3$measures["sse"], sum(perf3$losses[, "squared"]), checkNames=FALSE)
+#	checkEquals(perf3$measures["sae"], sum(perf3$losses[, "abs"]), checkNames=FALSE)
+#	checkEquals(perf3$measures["mse"], mean(perf3$losses[, "squared"]), checkNames=FALSE)
+#	checkEquals(perf3$measures["mae"], mean(perf3$losses[, "abs"]), checkNames=FALSE)
+#	checkEquals(perf3$measures["medse"], median(perf3$losses[, "squared"]), checkNames=FALSE)
+#	checkEquals(perf3$measures["medae"], median(perf3$losses[, "abs"]), checkNames=FALSE)
+#	checkEquals(perf3$losses[, "abs"], abs(perf3$losses[, "residual"]), checkNames=FALSE)
 #	
-#	perf = performance(pred, measures=ms)
-#	print(perf)
-#	ms = c("mmce", bla=function(x, task) 1)
-#	perf = performance(pred, measures=ms)
-#	checkEquals(names(perf$measures), c("mmce", "bla"))
-}
+#	
+#	
+#	
+##	
+##	res = make.res.desc("cv", iters=3)
+##	
+##	perf = performance(pred, measures=ms)
+##	print(perf)
+##	ms = c("mmce", bla=function(x, task) 1)
+##	perf = performance(pred, measures=ms)
+##	checkEquals(names(perf$measures), c("mmce", "bla"))
+#}
