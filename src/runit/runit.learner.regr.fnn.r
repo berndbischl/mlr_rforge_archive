c267a67a419b182b9169405a30a90926a082bc04
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 03:01:27 +0000

    bugs

--- src/runit/runit.learner.regr.fnn.r
+++ src/runit/runit.learner.regr.fnn.r
@@ -7,20 +7,23 @@ test.fnn.regr <- function() {
     list(k=10)
   )
   
-  # todo: does not work as test task has factors...
+  rfd = regr.df[,-4]
+  rtrain = regr.train[,-4]
+  rtest = regr.test[,-4]
+  rtask = make.task("regrtask", data=rdf, target="medv")  
   
   old.predicts.list1 = list()
   
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
-    j = which(colnames(regr.train) == regr.target)
-    pars = list(train=regr.train[,-c(j,4)], test=regr.test[,-c(j,4)], y=regr.train[,j])
+    j = which(colnames(rtrain) == regr.target)
+    pars = list(train=rtrain[,-j], test=rtest[,-j], y=rtrain[,j])
     pars = c(pars, parset)
     set.seed(debug.seed)
     old.predicts.list1[[i]] = do.call(FNN::knn.reg, pars)
   }
   
-  simple.test.parsets("regr.fnn", regr.df, regr.target, regr.train.inds, old.predicts.list1, parset.list)
+  simple.test.parsets("regr.fnn", rdf, regr.target, regr.train.inds, old.predicts.list1, parset.list)
   
   tt <- function (formula, data, k=1) {
     j = which(colnames(data) == as.character(formula)[2])
@@ -31,5 +34,5 @@ test.fnn.regr <- function() {
     FNN::knn(train=model$train, test=newdata, y=model$y, k=model$k)
   }
   
-  cv.test.parsets("regr.fnn", regr.df, regr.target, tune.train=tt, tune.predict=tp, parset.list=parset.list)
+  cv.test.parsets("regr.fnn", rdf, regr.target, tune.train=tt, tune.predict=tp, parset.list=parset.list)
 }
