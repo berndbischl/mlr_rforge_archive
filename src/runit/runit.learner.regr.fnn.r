78934fea6b2fc9f2435cbb59334ea7669eb1c319
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-02 11:45:43 +0000

    bugs

--- src/runit/runit.learner.regr.fnn.r
+++ src/runit/runit.learner.regr.fnn.r
@@ -20,18 +20,18 @@ test.fnn.regr <- function() {
     pars = list(train=rtrain[,-j], test=rtest[,-j], y=rtrain[,j])
     pars = c(pars, parset)
     set.seed(debug.seed)
-    old.predicts.list1[[i]] = do.call(FNN::knn.reg, pars)
+    old.predicts.list1[[i]] = do.call(FNN::knn.reg, pars)$pred
   }
   
   simple.test.parsets("regr.fnn", rdf, regr.target, regr.train.inds, old.predicts.list1, parset.list)
   
-  tt <- function (formula, data, k=1) {
+  tt <- function (formula, data, k=3) {
     j = which(colnames(data) == as.character(formula)[2])
     list(train=data[,-j], y=data[,j], k=k, target=j)
   }
   tp <- function(model, newdata) {
     newdata = newdata[, -model$target]
-    FNN::knn(train=model$train, test=newdata, y=model$y, k=model$k)
+    FNN::knn.reg(train=model$train, test=newdata, y=model$y, k=model$k)$pred
   }
   
   cv.test.parsets("regr.fnn", rdf, regr.target, tune.train=tt, tune.predict=tp, parset.list=parset.list)
