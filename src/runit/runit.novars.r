20fd84eb37d5640baab1328ce6cbbd45ab24a0f9
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-04 01:42:28 +0000

    bugs

--- src/runit/runit.novars.r
+++ src/runit/runit.novars.r
@@ -5,15 +5,16 @@ test.novars <- function() {
   checkTrue(is(m["learner.model"], "novars"))	
   checkTrue(is(m["learner"], "classif.lda"))	
 	checkEquals(m["learner"]["predict.type"], "prob")	
-	p = predict(m, newdata=multiclass.task)
+	p = predict(m, newdata=multiclass.df)
 	checkTrue(setequal(colnames(as.data.frame(p)), c("prob.setosa", "prob.virginica", "prob.versicolor", "truth", "response")))	
 	res = make.res.desc("cv", iter=2)
-	rf = resample(wl, multiclass.task, res, vars=c())
+	rf = resample(wl, ct, res)
 	checkTrue(setequal(colnames(as.data.frame(p)), c("prob.setosa", "prob.virginica", "prob.versicolor", "truth", "response")))	
 	
-	m = train("regr.lm", regr.task, vars=c())
-	p = predict(m, newdata=multiclass.df)
+	rt = subset.task(regr.task, vars=c())
+  m = train("regr.lm", rt)
+	p = predict(m, newdata=regr.df)
 	checkTrue(all(p["response"] == mean(p["response"]))) 
-  rf = resample("regr.lm", regr.task, res, vars=c())$pred
+  rf = resample("regr.lm", rt, res)$pred
 	checkEquals(length(unique(rf["response"])), 2) 
 }
