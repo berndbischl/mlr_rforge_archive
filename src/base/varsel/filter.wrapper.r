d85bf05f20f8029f6d61a55923ea3dcdec2c918c
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-04 09:21:15 +0000

    bugs

--- src/base/varsel/filter.wrapper.r
+++ src/base/varsel/filter.wrapper.r
@@ -18,35 +18,40 @@ setClass(
 setMethod(
 		f = "initialize",
 		signature = signature("filter.wrapper"),
-		def = function(.Object, learner, filter.method, filter.threshold) {
+		def = function(.Object, learner) {
       pds = list(
-        new("par.desc.disc", par.name="filter.method", 
+        new("par.desc.disc", par.name="method",
           vals=c("linear.correlation", "rank.correlation", "information.gain", "gain.ratio", 
             "symmetrical.uncertainty", "chi.squared", "random.forest.importance", "relief", "oneR")),
-        new("par.desc.double", par.name="filter.threshold")
+        new("par.desc.double", par.name="threshold")
       )
-      pvs = list(filter.threshold=filter.threshold, filter.method=filter.method)
-			callNextMethod(.Object, learner, par.descs=pds, par.vals=pvs, pack="FSelector")
+			callNextMethod(.Object, learner, par.descs=pds, par.vals=list(), pack="FSelector")
 		}
 )
 
 
 #' Fuses a base learner with a filter method. Creates a learner object, which can be
 #' used like any other learner object. 
-#' Currently only filtering to manually selected variables is supported.
-#'
+#' Internally Uses \code{\link{varfilter}} before every model fit. 
+#' 
+#' Look at package FSelector for details on the filter algorithms. 
+#' 
 #' @param learner [\code{\linkS4class{learner}} or string]\cr 
-#'        Learning algorithm. See \code{\link{learners}}.  
-#' @param vars [character]\cr 
-#'        Selected variables.  
+#'   Learning algorithm. See \code{\link{learners}}.  
+#' @param method [string] \cr
+#'   Filter method. Available are:
+#'   linear.correlation, rank.correlation, information.gain, gain.ratio, symmetrical.uncertainty, chi.squared, random.forest.importance, relief, oneR
+#' @param threshold [single double] \cr
+#'   Only features whose importance value exceed this are selected.  
 #' 
 #' @return \code{\linkS4class{learner}}.
 #' 
 #' @title Fuse learner with filter method.
 #' @export
-make.filter.wrapper = function(learner, filter.method="information.gain", filter.threshold) {
+make.filter.wrapper = function(learner, method="information.gain", threshold) {
   # todo check that for these inputs havew to be all num. or accept error in train and NA in predict?
-	new("filter.wrapper", learner=learner, filter.method=filter.method, filter.threshold=filter.threshold)
+	w = new("filter.wrapper", learner=learner)
+  set.hyper.pars(w, method=method, threshold=threshold)
 }
 
 
@@ -60,15 +65,23 @@ setMethod(
     ),
 		
 		def = function(.learner, .task, .subset,  ...) {
-		  tn = .task["target"]
-      args = list(...)
       .task = subset(.task, subset=.subset)  
-      f = varfilter(.task, args$filter.method, args$filter.threshold)
+      tn = .task["target"]
+      args = list(...)
+      
+      # remove hyperpar of wrapper
+      args$method = NULL
+      args$threshold = NULL
+      myargs = .learner["par.vals", par.top.wrapper.only=TRUE]
+      
+      vars = varfilter(.task, myargs$method, myargs$threshold)$vars
       if (length(vars) > 0) {
-        .task = subset(.task, vars=f$vars)  
-			  m = callNextMethod(.learner, .task, .subset, ...)
+        .task = subset(.task, vars=vars)  
+        # !we have already subsetted!
+			  m = callNextMethod(.learner, .task, 1:.task["size"], ...)
       } else {
-        m = new("novars", targets=.task["targets"][.subset], task.desc=.task["desc"])
+        # !we have already subsetted!
+        m = new("novars", targets=.task["targets"], task.desc=.task["desc"])
       }
       # set the vars as attribute, so we can extract it later 
       attr(m, "filter.result") = vars
