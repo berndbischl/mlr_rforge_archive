7ceceb349d383a08d6f658ac6b080389d7436094
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-05 09:39:53 +0000

    cost measure reports mean costs instaed of sum

--- src/base/measure.make.costs.r
+++ src/base/measure.make.costs.r
@@ -11,7 +11,9 @@ roxygen()
 #'   Matrix of misclassification costs. Rows and columns have to be named with class labels, order does not matter. 
 #'   Rows indicate true classes, columns predicted classes.
 #' @param task [\code{\linkS4class{classif.task}}]\cr 
-#'   Classification task. Has to be passed, so validity of costs can be checked.
+#'   Classification task. Has to be passed, so validity of matrix names can be checked.
+#' @param mean.costs [single logical] \cr
+#'   Should costs be averaged (TRUE) or summed (FALSE) over all cases in one test set prediction? Default is TRUE.
 #' 
 #' @return \code{\linkS4class{measure}} 
 #' 
@@ -23,11 +25,14 @@ roxygen()
 
 setGeneric(
   name = "make.cost.measure",
-  def = function(id, minimize, costs, task) {
+  def = function(id, minimize, costs, task, mean.costs) {
     if (missing(id))
       id = "costs"
     if (missing(minimize))
       minimize = TRUE
+    if (missing(mean.costs))
+      mean.costs = TRUE
+    check.arg(mean.costs, "logical", 1)
     standardGeneric("make.cost.measure")
   }
 )
@@ -36,11 +41,12 @@ setGeneric(
 setMethod(
   f = "make.cost.measure",
   signature = signature(id="character", minimize="logical", costs="matrix", task="classif.task"),
-  def = function(id="costs", minimize=TRUE, costs, task) {
+  def = function(id="costs", minimize=TRUE, costs, task, mean.costs) {
     check.costs(costs, task["class.levels"]) 
-    make.measure(id="costs", minimize=minimize, extra.pars=list(costs), 
+    make.measure(id="costs", minimize=minimize, extra.pars=list(costs, mean.costs), 
       fun=function(task, model, pred, extra.pars) {
         costs = extra.pars[[1]]
+        mean.costs = extra.pars[[2]]
         # cannot index with NA
         r = pred["response"]    
         if (any(is.na(r)))
@@ -48,7 +54,8 @@ setMethod(
         cc = function(truth, pred) {
           costs[truth, pred]
         }
-        Reduce(mean, Map(cc, as.character(pred["truth"]), as.character(r)))
+        y = mapply(cc, as.character(pred["truth"]), as.character(r))
+        if (mean.costs) mean(y) else sum(y)
       }
     )
   }
