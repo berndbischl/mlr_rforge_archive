b683e004a1ab162a8403f5a3b6cda3bc3d76985c
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-06 16:54:09 +0000

    myspo

--- src/base/optimize/myspo/choose.new.points.r
+++ src/base/optimize/myspo/choose.new.points.r
@@ -4,5 +4,10 @@ choose.new.points = function(n, meta.model, constr.model, curdes, cury, control)
   
   if (control["seq.method"] == "DiceOptim.CL") {
     opt.meta.model.CL(n, meta.model, constr.model, curdes, cury, control) 
-  }  
+  }  else {
+    if (class(meta.model["learner.model"])[1] == "rsm") 
+      opt.meta.model.bfgs(n, meta.model, constr.model, curdes, cury, control) 
+    else   
+      opt.meta.model.seq.des(n, meta.model, constr.model, curdes, cury, control)
+  }
 }
