0443bcf5741474599c82dda7bf1b56e738b0dfe4
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-06 01:44:27 +0000

    some garbage ea code

--- src/base/optimize/ea1pluslambda.r
+++ src/base/optimize/ea1pluslambda.r
@@ -1,27 +1,98 @@
-ea1pluslambda = function(f, par.descs, control) {
-  m = length(par.descs)
-  curx = list()
-  cury = f(curx)
-  step.sizes = list()
-  for (i in 1:m) {
-    pd = par.descs[[i]]
-    if (is(pd, "par.desc.double")) 
-      step.sizes[[pd["par.name"]]] = (pd["upper"] - pd["lower"]) / 100
+ea = function(f, names.double, names.disc, lower, upper, vals, control) {
+  asdf = function(pop) {
+    Reduce(rbind, lapply(pop, as.data.frame))
   }
-  step.sizes = as.numeric(step.sizes)
-  print(step.sizes)
   
-  offspring = list()
-  for (i in 1:m) {
-    pd = par.descs[[i]]
-    if (is(pd, "par.desc.double")) 
-      step.sizes[[pd["par.name"]]] = (pd["upper"] - pd["lower"]) / 100
+  init.pop = function() {
+    pop = list()
+    for (i in 1:control$mu) {
+      x = list()
+      for (v in names.double) {
+        x[[v]] = runif(1, min=lower[v], max=upper[v])
+        x[[paste(v,"sss")]] = runif(1, min=0, max=(upper[v]-lower[v])/10)
+      }
+      for (v in names.disc) 
+        x[[v]] = sample(vals[[v]], 1)
+      pop[[i]] = x
+    }
+    pop
+  } 
+  
+  eval.pop = function(pop) {
+    sapply(pop, function(x) {
+        x[paste(names.double, "sss")]=NULL  
+        do.call(f, x)
+    })
   }
-  #ys = sapply(1:nrow(offspring), function(i) do.call(f, data.frame.row.to.list(offspring, i)))
-  ys = f(offspring)
-  j = which.min(ys)
-  if (ys[j] < cury) {
-    cury = ys[j]
-    curx = data.frame.row.to.list(offspring, i)
+  
+  select = function(pop) {
+    pop[sample(1:length(pop), control$lambda, replace=TRUE)]
   }
+  
+  cross = function(x, y) {
+    z = list()
+    for (v in c(names.double, paste(names.double,"sss"))) 
+      z[[v]] = mean(c(x[[v]], y[[v]]))
+    for (v in names.disc) 
+      z[[v]] = sample(c(x[[v]], y[[v]]), 1)
+    z    
+  }
+  mutate = function(x) {
+    for (v in paste(names.double, "sss")) 
+      x[[v]] = x[[v]] * exp(rnorm(1,0,1))
+    for (v in names.double) 
+      x[[v]] = x[[v]] + rnorm(1,0,x[[paste(v, "sss")]])
+      #x[[v]] = x[[v]] + rnorm(1,0,step.sizes[[v]]/100)
+    for (v in names.disc) 
+      if (runif(1) < control$alpha) x[[v]] = sample(vals[[v]], 1)
+    x
+  }
+  
+  step.sizes = sapply(names.double, function(v) (upper[v] - lower[v]) / 100, USE.NAMES=FALSE)
+  
+  pop = init.pop()
+  print("init")
+  print(asdf(pop))
+  
+  for (i in 1:control$gens) {
+    parents1 = select(pop)
+    #print("parents1")
+    #print(asdf(parents1))
+    parents2 = select(pop)
+    #print("parents2")
+    #print(asdf(parents2))
+    kids = Map(cross, parents1, parents2)
+    #print("kids")
+    #print(asdf(kids))
+    kids = lapply(kids, mutate)
+    #print("mutated")
+    #print(asdf(kids))
+    pop = c(pop, kids)
+    y = eval.pop(pop)
+    # nextgen
+    o = order(y)
+    o = o[1:control$mu]
+    pop = pop[o]
+    print(mean(y[o]))
+    #print(y)
+    #print(asdf(pop))
+  }
+  return(pop=pop, y=y[o])
 }
+
+f = function(x,y,a,b) {
+  (x+y)^2
+}
+
+#set.seed(1)
+or = ea(
+  f,
+  names.double = c("x", "y"),
+  names.disc = c("a", "b"), 
+  lower = c(x=-100, y=-100),
+  upper = c(x=10, y=10),
+  vals =list(a=1:2, b=1:3),
+  control = list(mu=6, lambda=10, alpha=0.5, gens=50)
+)
+
+
