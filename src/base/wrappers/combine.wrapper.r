3c20debe3bc7e8e8c65b5300b9e3da79e50bb3bf
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-06 01:44:14 +0000

    some code for combine wrapper

---
+++ src/base/wrappers/combine.wrapper.r
@@ -0,0 +1,97 @@
+setClass(
+  "combine.wrapper",   
+  contains = c("learner"),
+  representation = representation(
+    learners = "list"
+  )
+)   
+
+#todo: names of hyperpars should start with "cw."
+
+make.combine.wrapper = function(learners) {
+  w = new("combine.wrapper")
+  w@learners=learners
+  a = as.list(rep(0.5, length(learners)))
+  names(a) = paste("", sapply(learners, function(x) x["id"]), sep="")
+  pds = lapply(names(a), function(x) new("par.desc.double", par.name=x, lower=0, upper=1))
+  w@par.descs = pds
+  w@par.vals = a
+  w@predict.type = "prob"
+  w
+}
+
+
+setMethod(
+  f = "[",
+  signature = signature("combine.wrapper"),
+  def = function(x,i,j,...,drop) {
+    if (i %in% c("oneclass", "twoclass", "multiclass", "doubles", "factors", "is.classif")) {
+      return(T)
+    }
+    callNextMethod()
+  }
+)
+
+
+#' @rdname train.learner
+
+setMethod(
+  f = "train.learner",
+  signature = signature(
+    .learner="combine.wrapper", 
+    .task="classif.task", .subset="integer" 
+  ),
+  
+  def = function(.learner, .task, .subset,  ...) {
+    learners = .learner@learners
+    models = list()
+    for (i in 1:length(learners)) {
+      w = learners[[i]]
+      models[[i]] = train(w, .task, .subset)
+    }
+    return(models)
+  }
+)
+
+#' @rdname pred.learner
+
+setMethod(
+  f = "pred.learner",
+  signature = signature(
+    .learner = "combine.wrapper", 
+    .model = "wrapped.model", 
+    .newdata = "data.frame", 
+    .type = "character" 
+  ),
+  
+  def = function(.learner, .model, .newdata, .type, ...) {
+    a = unlist(.learner["par.vals", head=TRUE])
+    models = .model["learner.model"]
+    k = length(models)
+    p = matrix(0, nrow(.newdata), ncol=.model["desc"]["class.nr"])
+    levs = .model["desc"]["class.levels"]
+    colnames(p) = levs
+    for (i in 1:k) {
+      m = models[[i]]
+      b = a[m@learner["id"]]
+      p = p + b * predict(m, newdata=.newdata,  ...)["prob"][, levs]
+    }
+    p
+  }
+)   
+
+#' @rdname to.string
+setMethod(f = "to.string",
+  signature = signature("combine.wrapper"),
+  def = function(x) {
+    return(paste(
+        "",
+        sep =""         
+      ))
+  })
+
+
+
+source("D:\\sync\\projekte\\mlr\\src\\test2.r")
+
+
