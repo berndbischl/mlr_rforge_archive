bdb0d560ce13cce37c6b4b506b8ee8e75d73e41d
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-11 13:40:37 +0000

    rename tasks

--- src/testsuite.r
+++ src/testsuite.r
@@ -42,7 +42,7 @@ binaryclass.train <- binaryclass.df[binaryclass.train.inds, ]
 binaryclass.test  <- binaryclass.df[binaryclass.test.inds, ]
 binaryclass.class.col <- 61
 binaryclass.class.levs <- levels(binaryclass.df[, binaryclass.class.col])
-binaryclass.task = make.task("binary", data=binaryclass.df, target=binaryclass.target)  
+binaryclass.task = makeClassifTask("binary", data=binaryclass.df, target=binaryclass.target)  
 
 multiclass.df <- iris
 multiclass.formula <- Species~.
@@ -52,7 +52,7 @@ multiclass.test.inds  <- setdiff(1:150, multiclass.train.inds)
 multiclass.train <- multiclass.df[multiclass.train.inds, ]
 multiclass.test  <- multiclass.df[multiclass.test.inds, ]
 multiclass.class.col <- 5
-multiclass.task = make.task("multiclass", data=multiclass.df, target=multiclass.target)  
+multiclass.task = makeClassifTask("multiclass", data=multiclass.df, target=multiclass.target)  
 
 data(BostonHousing)
 regr.df <- BostonHousing  
@@ -62,7 +62,7 @@ regr.train.inds <- seq(1, 506, 3)
 regr.test.inds  <- setdiff(1:nrow(regr.df), regr.train.inds)
 regr.train <- regr.df[regr.train.inds, ]
 regr.test  <- regr.df[regr.test.inds, ]
-regr.task <- make.task("regrtask", data=BostonHousing, target="medv")  
+regr.task <- makeRegrTask("regrtask", data=BostonHousing, target="medv")  
 
 .mlr.local$debug.seed <- 12345
 debug.seed <<- .mlr.local$debug.seed
