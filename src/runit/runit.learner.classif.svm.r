deafa4cee7148c5550a15428ff7a204e599c1ae5
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-16 18:49:58 +0000

    fix e1071 svm test

--- src/runit/runit.learner.classif.svm.r
+++ src/runit/runit.learner.classif.svm.r
@@ -25,18 +25,17 @@ test.svm.classif <- function() {
   #  parset=list(kernel="sigmoid", gamma=10))
   
   set.seed(debug.seed)
-  m <- svm(multiclass.formula, data=multiclass.train, kernel="polynomial", kpar=list(degree=3, coef0=2, scale=1.5), probability = TRUE)
+  m <- svm(multiclass.formula, data=multiclass.train, kernel="polynomial", degree=3, coef0=2, gamma=1.5)
   p <- predict(m, newdata=multiclass.test)
-  p2 <- predict(m, newdata=multiclass.test, type="prob")
+  p2 <- predict(m, newdata=multiclass.test)
   simple.test("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, p,  
     parset=list(kernel="polynomial", degree=3, coef0=2, gamma=1.5))
   #prob.test  ("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
   #  parset=list(kernel="polynomial", degree=3, coef0=2, gamma=1.5))
   
   tt <- function (formula, data, subset=1:150, ...) {
-    svm(x=formula, data=data[subset,], kernel="polynomial", kpar=list(degree=3, coef0=2, scale=1.5))
+    svm(formula, data=data[subset,], kernel="polynomial", degree=3, coef0=2, gamma=1.5)
   }
   
-  cv.test("classif.svm", multiclass.df, multiclass.target, tune.train=tt, parset=list(kernel="polynomial", degree=3, coef0=2, scale=1.5))
-  
+  cv.test("classif.svm", multiclass.df, multiclass.target, tune.train=tt, parset=list(kernel="polynomial", degree=3, coef0=2, gamma=1.5))
 }
