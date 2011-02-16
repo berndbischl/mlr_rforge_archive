815ea6e45ab1fa1ea13180bc62681134061fa2b4
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-16 18:10:15 +0000

    remove test for svm which cannot work

--- src/runit/runit.learner.classif.svm.r
+++ src/runit/runit.learner.classif.svm.r
@@ -1,3 +1,10 @@
+# we cannot do a prob test, as set.seed sems not to work on e1071 svm for the prob parameters!
+#library(e1071)
+#set.seed(1)
+#m1=svm(Species~., data=iris, probability=T)
+#set.seed(1)
+#m2=svm(Species~., data=iris, probability=T)
+#all.equal(m1, m2)
 
 test.svm.classif <- function() {
   library(e1071)
@@ -8,14 +15,14 @@ test.svm.classif <- function() {
   p1 = predict(m1, newdata=multiclass.test)
   p2 = predict(m2, newdata=multiclass.test, probability=TRUE)
   simple.test("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, p1,  parset=list(kernel="radial", gamma=20))
-  prob.test  ("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
-    parset=list(kernel="radial", gamma=20))
+  #prob.test  ("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
+  #  parset=list(kernel="radial", gamma=20))
   
   set.seed(debug.seed)
   m = svm(multiclass.formula, data=multiclass.train, kernel="sigmoid", gamma=10, probability = TRUE)
   p = predict(m, newdata=multiclass.test, probability = TRUE)
-  prob.test  ("classif.svm",multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
-    parset=list(kernel="sigmoid", gamma=10))
+  #prob.test  ("classif.svm",multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
+  #  parset=list(kernel="sigmoid", gamma=10))
   
   set.seed(debug.seed)
   m <- svm(multiclass.formula, data=multiclass.train, kernel="polynomial", kpar=list(degree=3, coef0=2, scale=1.5), probability = TRUE)
@@ -23,8 +30,8 @@ test.svm.classif <- function() {
   p2 <- predict(m, newdata=multiclass.test, type="prob")
   simple.test("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, p,  
     parset=list(kernel="polynomial", degree=3, coef0=2, gamma=1.5))
-  prob.test  ("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
-    parset=list(kernel="polynomial", degree=3, coef0=2, gamma=1.5))
+  #prob.test  ("classif.svm", multiclass.df, multiclass.target, multiclass.train.inds, attr(p2, "probabilities"), 
+  #  parset=list(kernel="polynomial", degree=3, coef0=2, gamma=1.5))
   
   tt <- function (formula, data, subset=1:150, ...) {
     svm(x=formula, data=data[subset,], kernel="polynomial", kpar=list(degree=3, coef0=2, scale=1.5))
