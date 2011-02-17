498ef020cbf915a27273e600524abd44effe986f
"Stefan" <"Stefan"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-17 13:26:10 +0000

    Adding roxygen Code

--- src/mlrEDA/summarizeNA.R
+++ src/mlrEDA/summarizeNA.R
@@ -1,24 +1,34 @@
-
+#' Characterizes only variables of a data set with missing values. So, missing values are painted 
+#' black, while other observations keep white.
+#'
+#' @param data [data.frame]\cr 
+#'   Data to summarize. Columns can be of type numeric, integer, logical, factor or character. 
+#'   Characters and logicals will be treated as factors.   
+#' @return Names of the variables with their frequency of missing values and an additional plot 
+#'   which shows the position of the missing values (color = black) for each variable with NAs.
+#' 
+#' @export
+#' @title Giving an image of a dataset with missing values
 summaryNA  <- function(dataset,show.plot=F,margin.left=4){
 	
-	zahl <- as.numeric(which(apply(is.na(dataset),2,any)))
+	num <- as.numeric(which(apply(is.na(dataset),2,any)))
 	
-	if(length(zahl) > 0){
+	if(length(num) > 0){
 		
-		cat("Variables with NAs: ",colnames(dataset)[zahl],"\n")
-		cat("Number of NAs: ",colSums(is.na(dataset[,zahl,drop=F])),"\n")
+		cat("Variables with NAs: ",colnames(dataset)[num],"\n")
+		cat("Number of NAs: ",colSums(is.na(dataset[,num,drop=F])),"\n")
 		
-		dataset.new <- dataset[,zahl,drop=F]
-		farben <- apply(dataset.new, 2, function(x) as.integer(is.na(x)))
+		dataset.new <- dataset[,num,drop=F]
+		color <- apply(dataset.new, 2, function(x) as.integer(is.na(x)))
 		
 		if(show.plot){
 			
-			image(farben,col=c("white","black"),yaxt="n")
+			image(color,col=c("white","black"),yaxt="n")
 			par(mar=c(5, margin.left, 4, 2) + 0.1)
 			abline(v=-0.001)
-			abline(h=1)
+			abline(h=1.015)
 			
-			if(length(zahl) == 1){
+			if(length(num) == 1){
 				y.type <- 0
 			} else {
 				y.type <- 0:(ncol(dataset.new)-1)/(length(dataset.new)-1)
