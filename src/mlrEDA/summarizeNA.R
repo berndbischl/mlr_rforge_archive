51fe6f3a6a9de93f2612c8dcc82524c3ff48821a
"Stefan" <"Stefan"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-17 12:32:25 +0000

    Updating function summaryNA

--- src/mlrEDA/summarizeNA.R
+++ src/mlrEDA/summarizeNA.R
@@ -1,34 +1,41 @@
-summarizeNA  <- function(dataset,show.plot=F){
-  
-  zahl <- as.numeric(which(apply(is.na(dataset),2,any)))
-  if(length(zahl) > 0){
-    cat("Variables with NAs: ",colnames(dataset)[zahl],"\n")
-    cat("Number of NAs: ",colSums(is.na(dataset[,zahl,drop=F])),"\n")
-    
-    dataset.new <- dataset[,zahl,drop=F]
-    farben <- matrix(0,ncol=ncol(dataset.new),nrow=nrow(dataset.new))
-    for(i in 1:nrow(farben)){
-      for(j in 1:ncol(farben)){
-        if(is.na(dataset.new[i,j])){
-          farben[i,j] <- 1
-        }   
-      }
-    }
-    if(show.plot){
-      par(las=1)
-      image(farben,col=c("white","black"))
-      abline(v=0)
-      abline(h=1.015)
-    }
-  }
-  else{
-    cat("There are no missing values in this dataset!!!", "\n")
-  }
+
+summaryNA  <- function(dataset,show.plot=F,margin.left=4){
+	
+	zahl <- as.numeric(which(apply(is.na(dataset),2,any)))
+	
+	if(length(zahl) > 0){
+		
+		cat("Variables with NAs: ",colnames(dataset)[zahl],"\n")
+		cat("Number of NAs: ",colSums(is.na(dataset[,zahl,drop=F])),"\n")
+		
+		dataset.new <- dataset[,zahl,drop=F]
+		farben <- apply(dataset.new, 2, function(x) as.integer(is.na(x)))
+		
+		if(show.plot){
+			
+			image(farben,col=c("white","black"),yaxt="n")
+			par(mar=c(5, margin.left, 4, 2) + 0.1)
+			abline(v=-0.001)
+			abline(h=1)
+			
+			if(length(zahl) == 1){
+				y.type <- 0
+			} else {
+				y.type <- 0:(ncol(dataset.new)-1)/(length(dataset.new)-1)
+			}
+			
+			axis(2, labels=colnames(dataset.new), at=y.type, las=2)
+		}
+	}
+	
+	else{
+		cat("There are no missing values in this dataset!!!", "\n")
+	}
 }
+
 
 library(mlbench)
 data(Soybean)
-summarizeNA(Soybean,show.plot=T)
+data(BreastCancer)
+summaryNA(Soybean,show.plot=T,margin.left=8)
 
-
-  
