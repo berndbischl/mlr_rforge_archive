65b8922913bdcf9fdb42fdd6b567986469375a74
"meinke" <"meinke"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-17 17:41:12 +0000

    improve mlrEDA, first sweave stuff

--- src/mlrEDA/summarizeNA.R
+++ src/mlrEDA/summarizeNA.R
@@ -9,43 +9,38 @@
 #' 
 #' @export
 #' @title Giving an image of a dataset with missing values
+
+
 summaryNA  <- function(dataset,show.plot=F,margin.left=4){
-	
-	num <- as.numeric(which(apply(is.na(dataset),2,any)))
-	
-	if(length(num) > 0){
-		
-		cat("Variables with NAs: ",colnames(dataset)[num],"\n")
-		cat("Number of NAs: ",colSums(is.na(dataset[,num,drop=F])),"\n")
-		
-		dataset.new <- dataset[,num,drop=F]
-		color <- apply(dataset.new, 2, function(x) as.integer(is.na(x)))
-		
-		if(show.plot){
-			
-			image(color,col=c("white","black"),yaxt="n")
-			par(mar=c(5, margin.left, 4, 2) + 0.1)
-			abline(v=-0.001)
-			abline(h=1.015)
-			
-			if(length(num) == 1){
-				y.type <- 0
-			} else {
-				y.type <- 0:(ncol(dataset.new)-1)/(length(dataset.new)-1)
-			}
-			
-			axis(2, labels=colnames(dataset.new), at=y.type, las=2)
-		}
-	}
-	
-	else{
-		cat("There are no missing values in this dataset!!!", "\n")
-	}
+  
+  num <- as.numeric(which(apply(is.na(dataset),2,any)))
+  
+  if(length(num) > 0){
+    
+    cat("Variables with NAs: ",colnames(dataset)[num],"\n")
+    cat("Number of NAs: ",colSums(is.na(dataset[,num,drop=F])),"\n")
+    
+    dataset.new <- dataset[,num,drop=F]
+    color <- apply(dataset.new, 2, function(x) as.integer(is.na(x)))
+    
+    if(show.plot){
+      
+      image(color,col=c("white","black"),yaxt="n")
+      par(mar=c(5, margin.left, 4, 2) + 0.1)
+      abline(v=-0.001)
+      abline(h=1.015)
+      
+      if(length(num) == 1){
+        y.type <- 0
+      } else {
+        y.type <- 0:(ncol(dataset.new)-1)/(length(dataset.new)-1)
+      }
+      
+      axis(2, labels=colnames(dataset.new), at=y.type, las=2)
+    }
+  }
+  
+  else{
+    cat("There are no missing values in this dataset!!!", "\n")
+  }
 }
-
-
-library(mlbench)
-data(Soybean)
-data(BreastCancer)
-summaryNA(Soybean,show.plot=T,margin.left=8)
-
