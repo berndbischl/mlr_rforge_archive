3cf9a22d24497df212fc3581090a34e3dd32b0ae
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-20 00:25:35 +0000

    fix prep.data

--- src/runit/runit.prepare.r
+++ src/runit/runit.prepare.r
@@ -1,32 +1,48 @@
 
 test.prepare <- function(){
-  	d = data.frame(a=1:3, b=c("bli", "bla", "blub"), c=c(TRUE, TRUE, FALSE), y=c(1,1,1))
+    pc = prepare.control()
+    checkEquals(length(pc@ints.as), 1)
+    checkEquals(length(pc@chars.as), 1)
+    checkEquals(length(pc@logs.as), 1)
+    checkEquals(length(pc@Dates.as), 1)
+    checkEquals(length(pc@POSIXcts.as), 1)
+
+    d = data.frame(a=1:3, b=c("bli", "bla", "blub"), c=c(TRUE, TRUE, FALSE), 
+      d=as.Date(c("1970-01-01", "1970-01-02", "1970-01-03")), 
+      e=as.POSIXct(c("1970-01-01", "1970-01-02", "1970-01-03")), 
+      y=c(1,1,1))
   	
   	pc = prepare.control(ints.as="numeric", chars.as = "factor", logs.as="factor")
     ct = makeRegrTask(data=d, target="y", control=pc) 
-    checkEquals(ct["n.feat"], c(1,0,2,0,0), checkNames=FALSE)
-    pc = prepare.control(ints.as="factor", chars.as = "factor", logs.as="factor")
+    checkEquals(ct["n.feat"], c(3,0,2,0,0), checkNames=FALSE)
+    checkTrue(is.numeric(get.data(ct)$d))
+    checkTrue(is.numeric(get.data(ct)$e))
+    checkEquals(get.data(ct)$d, get.data(ct)$e)
+    
+    pc = prepare.control(ints.as="factor", chars.as = "factor", logs.as="factor", POSIXcts.as="minutes")
     ct = makeRegrTask(data=d, target="y", control=pc) 
-    checkEquals(ct["n.feat"], c(0,0,3,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(2,0,3,0,0), checkNames=FALSE)
+    checkEquals(get.data(ct)$d, get.data(ct)$e * 60)
+    
     pc = prepare.control(ints.as="numeric", chars.as = "factor", logs.as="numeric")
     ct = makeRegrTask(data=d, target="y", control=pc) 
-    checkEquals(ct["n.feat"], c(2,0,1,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(4,0,1,0,0), checkNames=FALSE)
     pc = prepare.control(ints.as="factor", chars.as = "factor", logs.as="numeric")
     ct = makeRegrTask(data=d, target="y", control=pc) 
-    checkEquals(ct["n.feat"], c(1,0,2,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(3,0,2,0,0), checkNames=FALSE)
     
     pc = prepare.control(ints.as="numeric", chars.as = "factor", logs.as="factor")
     ct = makeClassifTask(data=d, target="b", control=pc) 
-    checkEquals(ct["n.feat"], c(2,0,1,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(4,0,1,0,0), checkNames=FALSE)
     pc = prepare.control(ints.as="factor", chars.as = "factor", logs.as="factor")
     ct = makeClassifTask(data=d, target="b", control=pc) 
-    checkEquals(ct["n.feat"], c(1,0,2,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(3,0,2,0,0), checkNames=FALSE)
     pc = prepare.control(ints.as="numeric", chars.as = "factor", logs.as="numeric")
     ct = makeClassifTask(data=d, target="b", control=pc) 
-    checkEquals(ct["n.feat"], c(3,0,0,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(5,0,0,0,0), checkNames=FALSE)
     pc = prepare.control(ints.as="factor", chars.as = "factor", logs.as="numeric")
     ct = makeClassifTask(data=d, target="b", control=pc) 
-    checkEquals(ct["n.feat"], c(2,0,1,0,0), checkNames=FALSE)
+    checkEquals(ct["n.feat"], c(4,0,1,0,0), checkNames=FALSE)
     
   	
   	# check inf accessors
