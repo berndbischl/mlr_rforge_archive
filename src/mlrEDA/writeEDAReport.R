1ef39f57bbb678c7f193fba09ab06e469803a07c
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-15 18:19:53 +0000

    EDA: reporting

--- src/mlrEDA/writeEDAReport.R
+++ src/mlrEDA/writeEDAReport.R
@@ -1,15 +1,15 @@
 
-report = function(data, target, exclude=character(0)) {
-  cns = colnames(data)
-  print(summarizeData(data))
-  
-  cols = which(!(cns %in% exclude))
-  ps = lapply(cols, function(i) plotFeatureDistrib(data[,i], cns[i], data, target))
-  do.call(grid.arrange, ps)
-  pause()
-  plotPCA(data, target)
-  pause()
-  plotMDS(data, target)
-  pause()
+report = function(outdir, data, target, exclude=character(0)) {
+  assign(".sweave.data", data, envir=.GlobalEnv)
+  assign(".sweave.target", target, envir=.GlobalEnv)
+  assign(".sweave.exclude", exclude, envir=.GlobalEnv)
+  dir = getwd()
+  setwd(outdir)
+  Sweave("d:/sync/projekte/mlr/src/mlrEDA/writeEDAReport.Rnw")
+  rm(".sweave.data", envir=.GlobalEnv)
+  rm(".sweave.target", envir=.GlobalEnv)
+  rm(".sweave.exclude", envir=.GlobalEnv)
+  setwd(dir)
 }
 
+report("src/mlrEDA", iris, "Species")
