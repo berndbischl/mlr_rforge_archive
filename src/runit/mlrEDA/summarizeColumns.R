33e4f37c026795ea3208a801eee27418a5db11fb
arnu <arnu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-31 14:53:08 +0000

    EDA: summarizeColumns runit test

--- src/runit/mlrEDA/summarizeColumns.R
+++ src/runit/mlrEDA/summarizeColumns.R
@@ -10,8 +10,8 @@ testSummarizeColumns = function(){
   data= data.frame(x=as.Date(c("2011-01-01", "2011-01-02", "2011-01-03")), y=c(1:3), 
         z=as.Date(c("2011-01-01", "2011-01-02", "2011-01-03")))
   data3 = summarizeColumns(data)
-  data4 = summarizeColumns(data, "2000-01-01")
-  data5 = summarizeColumns(data, c(x=("2001-01-01"), z=("2002-01-01")))
+  data4 = summarizeColumns(data, "2011-01-01")
+  data5 = summarizeColumns(data, c(x=("2011-01-01"), z=("2011-01-01")))
   checkEquals(data3$disp, data4$disp)
   checkEquals(data3[2,], data4[2,])
   checkEquals(data4, data5)
