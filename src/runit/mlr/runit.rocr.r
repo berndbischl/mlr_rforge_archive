4b1042852c0ed41b087450da7f965c7fdc7b6ee1
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-13 18:05:14 +0000

    mlr: small rename to as.ROCR.prediction

--- src/runit/mlr/runit.rocr.r
+++ src/runit/mlr/runit.rocr.r
@@ -2,6 +2,6 @@ test.rocr = function() {
   w = makeLearner("classif.lda", predict.type="prob")
   m = train(w, binaryclass.task)
   p = predict(m, binaryclass.task)
-  p= as.ROCR.pred(p)
+  p = as.ROCR.prediction(p)
   #ROCR.plot(p)
 }
