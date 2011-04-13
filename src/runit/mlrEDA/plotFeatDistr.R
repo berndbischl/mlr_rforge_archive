37720dc02b3454536d0ec940bea83b3b7a949cd7
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-13 18:17:57 +0000

    mlrEDA: small fix for tests

--- src/runit/mlrEDA/plotFeatDistr.R
+++ src/runit/mlrEDA/plotFeatDistr.R
@@ -1,4 +1,5 @@
 testPlotFeatDistr = function() {
+  require("ggplot2")
   plotFeatDistr(iris, "Species", "Sepal.Length")
   plotFeatDistr(iris, "Species", 1)
   plotFeatDistr(BostonHousing, "medv", "chas")
--- src/runit/mlrEDA/plotMDS.R
+++ src/runit/mlrEDA/plotMDS.R
@@ -1,4 +1,5 @@
 testPlotMDS = function() {
+  require("cluster")
   plotMDS(iris, "Species")
   plotMDS(iris, "Species", exclude=c("Sepal.Length", "Sepal.Width"))
   checkException(plotMDS(iris, "Species", exclude=c("Sepal.Length", "Sepal.Width", "Petal.Length")))
