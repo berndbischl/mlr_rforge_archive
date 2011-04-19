0868017baabeb2522269780438a147c16b93525e
olafm <olafm@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-19 13:05:24 +0000

    Add example to setId().

--- src/mlr/setId.R
+++ src/mlr/setId.R
@@ -1,36 +1,37 @@
-#' Set id of learner object. 
+#' Set id of a learner object. 
+#'
+#' Please remember that learner ids must be unique. 
 #' 
-#' @param learner [\code{\linkS4class{Learner}}]\cr 
-#'        Learner object.   
+#' @param learner [\code{\linkS4class{Learner}}] \cr
+#'        Learner object.
 #' @param id [\code{character(1)}] \cr
 #'       New id.
 #' 		    
 #' @return \code{\linkS4class{Learner}} with changed id.
+#'
+#' @example
+#' cl <- makeLearner("classif.logreg")
+#' cl <- setId(cl, "My Logistic Regression Learner")
+#' print(cl)
+#'
 #' @exportMethod setId
 #' @title Set id of learner object.
 #' @rdname setId 
 
-setGeneric(
-		name = "setId",
-		def = function(learner, id) {
-			standardGeneric("setId")			
-		}
-)
+setGeneric(name = "setId",
+           def = function(learner, id) {
+             standardGeneric("setId")			
+           })
+
 
 #' @rdname setId 
-setMethod(
-		f = "setId",
-		
-		signature = signature(
-				learner="Learner", 
-				id="character" 
-		),
-		
-		def = function(learner, id) {
-			learner@id = id
-			return(learner)
-		} 
-)
-
-
-
+setMethod(f = "setId",	
+          signature = signature(
+            learner="Learner", 
+            id="character"
+            ),          
+          def = function(learner, id) {
+            stopifnot(is.character(id) && length(id) == 1)
+            learner@id = id
+            return(learner)
+          })
