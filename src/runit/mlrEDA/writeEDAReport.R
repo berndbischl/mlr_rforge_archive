386f9a8933cbab32bb196f0cd40a044e7c3bfbb9
"meinke" <"meinke"@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-01 17:15:51 +0000

    EDA: fix in writeEDAReport.R

--- src/runit/mlrEDA/writeEDAReport.R
+++ src/runit/mlrEDA/writeEDAReport.R
@@ -1,6 +1,17 @@
-require(brew)
-d.set <- iris
-target <- "Species"
-name <- "iris"
-writeEDAReport(tempdir(),d.set,target,name)
-checkTrue("iris.html" %in% list.files(tempdir()))
+testwriteEDAReport <- function(){
+	
+	require(brew)
+	d.set = iris
+	target = "Species"
+	name = "iris"
+	writeEDAReport(tempdir(),d.set,target,name)
+	checkTrue(file.exists("iris.html"))
+	checkTrue(file.exists("iris_feat_1.png"))
+	checkTrue(file.exists("iris_feat_2.png"))
+	checkTrue(file.exists("iris_feat_3.png"))
+	checkTrue(file.exists("iris_feat_4.png"))
+	checkTrue(file.exists("iris_feat_5.png"))
+	checkTrue(file.exists("iris_mds.png"))    
+	checkTrue(file.exists("iris_pca.png"))
+	
+}
