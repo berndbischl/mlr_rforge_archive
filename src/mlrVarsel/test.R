ef1d6691fb1207179bc715779b915fa8b7f592f5
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-20 08:21:33 +0000

    varsel mco

--- src/mlrVarsel/test.R
+++ src/mlrVarsel/test.R
@@ -31,10 +31,11 @@ data(Sonar, BreastCancer)
 
 task = makeClassifTask(target="Class", data=Sonar)
 learners = list(
- # makeLearner("classif.rpart"),
- # makeLearner("classif.lda"),
- # makeLearner("classif.naiveBayes")
- makeLearner("classif.ksvm")
+ makeLearner("classif.rpart"),
+ makeLearner("classif.lda"),
+ makeLearner("classif.naiveBayes"),
+ makeLearner("classif.ksvm"),
+  makeLearner("classif.kknn")
 )
 
 res = makeResampleDesc("Holdout")
@@ -51,17 +52,24 @@ m2@aggr = list(test.mean)
 m3 = nvars
 m3@aggr = list(test.mean)
 
-ps1 = makeParameterSet(
-  makeNumericParameter("sigma", lower=1, upper=5),
-  makeNumericParameter("C", lower=7, upper=9)
+ps.ksvm = makeParameterSet(
+  makeNumericParameter("sigma", lower=-10, upper=10, trafo=function(x) 2^x),
+  makeNumericParameter("C", lower=-10, upper=10, trafo=function(x) 2^x)
+)
+
+ps.kknn = makeParameterSet(
+  makeIntegerParameter("k", lower=1L, upper=10L)
 )
 
 
 pss = list(
-  classif.ksvm = ps1
+  classif.lda = makeParameterSet(),
+  classif.naiveBayes = makeParameterSet(),
+  classif.rpart = makeParameterSet(),
+  classif.ksvm = ps.ksvm,
+  classif.kknn = ps.kknn
 )
-
-ctrl = makeVarselControlMCO(maxit=10L, mu=2L, prob.init=0.5, 
+ctrl = makeVarselControlMCO(maxit=500L, mu=50L, prob.init=0.5, 
   prob.mut.learner=0.01, prob.mut.bit=0.05, prob.cx=0.5, mut.hp.eta=10, mut.hp.prob=0.2)
 print(ctrl)
 mmv = c(mmce.test.mean=1, varcosts.test.mean=m2@extra.args[[1]](getFeatureNames(task)))
@@ -69,8 +77,10 @@ ops = varselMCO(learners, task, res, measures=list(m1, m2), control=ctrl, measur
 #dd = as.data.frame(ops[[1]])
 #print(dd$mmce.test.mean)
 #plot(dd$mmce.test.mean, dd$varcosts.test.mean)
-ops = lapply(ops, as.data.frame)
-pareto_plot(ops, y.names, y.names[1], y.names[2], shape="learner", alpha="dob", color="is_run_dominated") + geom_jitter()
+dfs = lapply(ops, as.data.frame)
+df = do.call(rbind, dfs)
+y.names = c("mmce.test.mean", "varcosts.test.mean")
+print(pareto_plot(dfs, y.names, y.names[1], y.names[2], shape="learner", alpha="dob", color="is_run_dominated") + geom_jitter())
 #plotEAF(ops, c("mmce.test.mean", "varcosts.test.mean"))
 
 #m = train(w, subset(task, vars=getFeatureNames(task)[1:2]))
--- src/mlrVarsel/varselMCO.R
+++ src/mlrVarsel/varselMCO.R
@@ -50,71 +50,107 @@ varselMCO = function(learners, task, resampling, measures, bit.names, bits.to.fe
     bit.names = getFeatureNames(task)
   if (missing(bits.to.features))
     bits.to.features = function(x, task) binary.to.vars(x, getFeatureNames(task)) 
+  learner.ns = names(bag@learners)
+  if (!setequal(names(par.sets), learner.ns)) {
+    stop("Learner ids and names of parameter sets must coincide!")
+  }
   
   ops = replicate(multi.starts,  simplify=FALSE, varselMCO2(bag, task, resampling, measures, 
-      bit.names, bits.to.features, control, measure.max.vals, par.sets))
+      bit.names, bits.to.features, control, measure.max.vals, par.sets, learner.ns))
 }
 
-varselMCO2 = function(bag, task, resampling, measures, bit.names, bits.to.features, control, measure.max.vals=NULL, par.sets) {
-  learner.ns = names(bag@learners)
-  
-  
-  initPopulation = function(learner.ns, pars.sets, bits, control) {
-    pop = list()
-    for (i in 1:control@mu) {
-      learner = sample(learner.ns, 1)
-      pop[[i]] = list (
-        learner = learner,
-        hyper.pars = sampleHyperPars(par.sets[[learner]]),
-        bits = rbinom(bits, 1, control@prob.init)
-      )
-    }
-    return(pop)
+
+initPopulation = function(learner.ns, par.sets, bits, control) {
+  pop = list()
+  for (i in 1:control@mu) {
+    learner = sample(learner.ns, 1)
+    pop[[i]] = list (
+      learner = learner,
+      hyper.pars = sampleHyperPars(par.sets[[learner]]),
+      bits = rbinom(bits, 1, control@prob.init)
+    )
   }
-  
-  crossover = function(x, y, control) {
-    print(x)
-    z = list()
-    z$learner = sample(c(x$learner, y$learner), 1)
-    p = c(control@prob.cx, 1-control@prob.cx)
-    z$bits = mapply(function(a,b) sample(c(a,b), 1, prob=p), x$bits, y$bits)
-    return(z)
+  return(pop)
+}
+
+crossover.hps = function(x, y, par.sets, control) {
+  ps = par.sets[[x$learner]]
+  low = as.numeric(lower(ps))
+  upp = as.numeric(upper(ps))
+  ns = names(x$hyper.pars)
+  n = length(x$hyper.pars)
+  hps = numeric(n)
+  for (i in seq_len(n)) {
+    sbx = sbx_operator(control@mut.hp.eta, control@mut.hp.prob, lower=low[ns[i]], upper=upp[ns[i]])
+    hps[i] = sample(sbx(matrix(c(x$hyper.pars[i], y$hyper.pars[i]), 1, 2)), 1)
   }
+  names(hps) = ns
+  return(hps)
+}  
   
-  mutate = function(x, learner.ns, par.sets, control) {
-    if (runif(1) < control@prob.mut.learner) {
-      x$learner = sample(learner.ns, 1)
-      x$hyper.pars = sampleHyperPars(par.sets[[x$learner]])
-    } else {
-      x = mutate.hp(x, control)
-    }
-    mut = sample(0:1, n, replace=TRUE, prob=c(1-control@prob.mut.bit, control@prob.mut.bit))
-    x$bits = x$bits + mut %% 2
-    return(x)
-  }  
-  
-  mutate.hp = function(x) {
-    ps = par.sets[[x$learner]]
-    print(ps)
-    low = lower(ps)
-    upp = upper(ps)
-    ns = names(x$hyper.pars)
-    for (i in seq_len(x$hyper.pars)) {
-      pm = pm_operator(mut.hp.eta, mut.hp.prob, lower=low[ns[i]], upper=upp[ns[i]])
-      x$hyper.pars[i] = pm(x$hyper.pars[i])
-    }
-    return(x)
+
+crossover = function(x, y, par.sets, control) {
+  #todo: add case for integer and test!
+  z = list()
+  z$learner = sample(c(x$learner, y$learner), 1)
+  if (z$learner == x$learner && z$learner != y$learner)
+    z$hyper.pars = x$hyper.pars
+  else if (z$learner != x$learner && z$learner == y$learner)
+    z$hyper.pars = y$hyper.pars
+  else {
+    z$hyper.pars = crossover.hps(x, y, par.sets, control)
   }
-  
-  
+  p = c(control@prob.cx, 1-control@prob.cx)
+  z$bits = mapply(function(a,b) sample(c(a,b), 1, prob=p), x$bits, y$bits)
+  return(z)
+}
+
+mutate = function(x, learner.ns, par.sets, control) {
+  # if we have more than 1 learner change it stochastically
+  if (length(learner.ns) > 1 && runif(1) < control@prob.mut.learner) {
+    x$learner = sample(setdiff(learner.ns, x$learner), 1)
+    # learner changed, sample new params
+    x$hyper.pars = sampleHyperPars(par.sets[[x$learner]])
+  } else { 
+    # learner not changed, mutate params
+    x$hyper.pars = mutate.hp(x, par.sets, control)
+  }
+  # flips feature bits
+  p = c(1-control@prob.mut.bit, control@prob.mut.bit)
+  mut = sample(0:1, length(x$bits), replace=TRUE, p)
+  x$bits = (x$bits + mut) %% 2
+  return(x)
+}  
+
+mutate.hp = function(x, par.sets, control) {
+  ps = par.sets[[x$learner]]
+  low = as.numeric(lower(ps))
+  upp = as.numeric(upper(ps))
+  hps = x$hyper.pars
+  if (is.integer(hps))
+    mode(hps) = "numeric"
+  ns = names(hps)
+  for (i in seq_along(hps)) {
+    n = ns[i]
+    pm = pm_operator(control@mut.hp.eta, control@mut.hp.prob, lower=low[n], upper=upp[n])
+    x$hyper.pars[i] = pm(hps[i])
+    if (ps@pars[[n]]@type == "integer")
+      x$hyper.pars[i] = as.integer(x$hyper.pars[i])
+  }
+  return(x$hyper.pars)
+}
+
+varselMCO2 = function(bag, task, resampling, measures, bit.names, bits.to.features, control, measure.max.vals=NULL, par.sets, learner.ns) {
   n = length(bit.names)
   m = length(measures)
   
   print(c(n, m))
   # td: scale all measures to 0,1 and always use ref(1,...,1)  
   # td: put this in varselMOCControl
-  #todo: check that "learner" is not in bit.names
-  opt.path = makeOptPathDFFromMeasures(c("learner", bit.names), measures)
+  #todo: check that "learner" and hyper.pars is not in bit.names
+  #todo: check that learners have not same hyper par names, what then?
+  hps.ns = as.character(do.call(c, lapply(par.sets, function(ps) names(ps@pars))))
+  opt.path = makeOptPathDFFromMeasures(c("learner", hps.ns, bit.names), measures)
   
   mu = control@mu
   gen = 0L
@@ -125,10 +161,9 @@ varselMCO2 = function(bag, task, resampling, measures, bit.names, bits.to.featur
   mut.hp.prob = control@mut.hp.prob
   prob.cx = control@prob.cx
   pop = initPopulation(learner.ns, par.sets, n, control)
-  my.eval.states(bag, task, resampling, measures, NULL, bits.to.features, control, opt.path, pop, dob=gen)
+  my.eval.states(bag, task, resampling, measures, NULL, bits.to.features, control, opt.path, pop, dob=gen, hps.ns=hps.ns)
   # Indices of individuals that are in the current pop.
   active = 1:mu    
-  print(pop)
   while(getLength(opt.path) < control@maxit) {
     gen = gen + 1L
     ## Variation:
@@ -139,13 +174,14 @@ varselMCO2 = function(bag, task, resampling, measures, bit.names, bits.to.featur
     p2 = list()
     p1$learner = p1a$learner    
     p2$learner = p2a$learner
-    p1$bits = unlist(p1a[-1])
-    p2$bits = unlist(p2a[-1])
-    child = crossover(p1, p2, control)
-    print(child)
-    child = mutate(child)
+    p1$hyper.pars = unlist(p1a[names(par.sets[[p1$learner]]@pars)])  
+    p2$hyper.pars = unlist(p2a[names(par.sets[[p2$learner]]@pars)])  
+    p1$bits = unlist(p1a[bit.names])
+    p2$bits = unlist(p2a[bit.names])
+    child = crossover(p1, p2, par.sets, control)
+    child = mutate(child, learner.ns, par.sets, control)
     states = list(child)
-    my.eval.states(bag, task, resampling, measures, NULL, bits.to.features, control, opt.path, states, dob=gen)
+    my.eval.states(bag, task, resampling, measures, NULL, bits.to.features, control, opt.path, states, dob=gen, hps.ns=hps.ns)
     
     active = c(active, getLength(opt.path))
     Y = as.data.frame(opt.path)[, opt.path@y.names]
@@ -163,8 +199,9 @@ varselMCO2 = function(bag, task, resampling, measures, bit.names, bits.to.featur
   return(opt.path)
 }
 
+#todo maybe add this to mlr?
 my.eval.states = function(bag, task, resampling, measures, par.set, bits.to.features, control, opt.path, pars, 
-  eol=as.integer(NA), dob=as.integer(NA)) {
+  eol=as.integer(NA), dob=as.integer(NA), hps.ns) {
   
   fun = function(bag, task, resampling, measures, par.set, bits.to.features, control, val) {
     bag = setHyperPars(bag, sel.learner=val$learner)
@@ -180,8 +217,14 @@ my.eval.states = function(bag, task, resampling, measures, par.set, bits.to.feat
     dob = rep(dob, n)
   if (length(eol) == 1)
     eol = rep(eol, n)
-  for (i in 1:n) 
-    addPathElement(opt.path, x=as.list(pars[[i]]), y=y[[i]], dob=dob[i], eol=eol[i])
+  for (i in 1:n) {
+    hps = rep(as.numeric(NA), length(hps.ns))
+    names(hps) = hps.ns
+    x = pars[[i]]
+    hps[names(x$hyper.pars)] = x$hyper.pars
+    x = c(x$learner, as.list(hps), as.list(x$bits))
+    addPathElement(opt.path, x=x, y=y[[i]], dob=dob[i], eol=eol[i])
+  }
   return(y)
 }
 
@@ -191,7 +234,11 @@ sampleHyperPars = function(par.set) {
   z = numeric(n)
   for (i in seq_len(n)) {
     pd = par.set@pars[[i]]
-    z[i] = runif(1, pd@constraints$lower, pd@constraints$upper)
+    x = runif(1, pd@constraints$lower, pd@constraints$upper)
+    if (pd@type == "integer")
+      x = as.integer(round(x))
+    # todo: should we really do the trafo here? check mlrTune!
+    z[i] = pd@trafo(x)
   }
   names(z) = names(par.set@pars)
   return(z)
