fce427393668220d8fabc2b2ea799e2c224d6b93
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-02 09:36:35 +0000

    remove garbage

--- src/testsuite.config.r
+++
@@ -1,7 +0,0 @@
-#ts.dirs = c("/home/bischl/mlr/src/runit", "/home/bischl/mlr/src/runit/parallel")  
-#ts.dirs = "/home/bischl/mlr/src/runit/parallel"
-ts.dirs = "/home/bischl/mlr/src/runit"
-ts.file.regexp ="runit.learner.classif"
-
-
-
