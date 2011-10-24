42423cc3ea4c00b2f0a9a60bd0f77bd2d4e1d427
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-24 08:16:52 +0000

    setThreshold was much too slow

--- src/mlr/setThreshold.R
+++ src/mlr/setThreshold.R
@@ -4,15 +4,14 @@
 #' For multiclass: Probabilities are divided by corresponding thresholds and the class with maximum resulting value is selected.
 #' The result of both are equivalent if in the multi-threshold case the labels are greater than 0 and sum to 1.  
 #' 
-#' @param pred [\code{\linkS4class{Prediction}} | double matrix] \cr
-#'   Prediction object or matrix of probabilities. If a matrix, column names have to be class labels.
-#' @param threshold [numeric] \cr
+#' @param pred [\code{\linkS4class{Prediction}}] \cr
+#'   Prediction object.
+#' @param threshold [\code{numeric}] \cr
 #'   Threshold to produce class labels. Has to be a named vector, where names correspond to class labels.
-#'   Only if pred is a prediction object resulting from binary classification
+#'   Only if \code{pred} is a prediction object resulting from binary classification
 #'   it can be a single numerical threshold for the positive class. 
 #' 		    
-#' @return Either a \code{\linkS4class{Prediction}} with changed threshold and corresponding response,  
-#'   or a factor if \code{pred} was a matrix. 
+#' @return \code{\linkS4class{Prediction}} with changed threshold and corresponding response.  
 #' @exportMethod setThreshold
 #' @title Set threshold of prediction object.
 #' @rdname setThreshold 
@@ -38,41 +37,25 @@ setMethod(
     if (td@type != "classif")
       stop("Threshold can only be set for classification predictions!")
     if (pred@predict.type != "prob")
-      stop("Threshold can currently only be set for type 'prob'!")
+      stop("Threshold can only be set for predict.type 'prob'!")
     levs = getClassLevels(td)
     if (length(levs) == 2 && is.numeric(threshold) && length(threshold) == 1) {
       threshold = c(threshold, 1-threshold)
       names(threshold) = c(td@positive, td@negative)
-    }
+    } 
+    if (length(threshold > 1) && !setequal(levs, names(threshold))) 
+      stop("Threshold names must correspond to classes!")
     p = getProb(pred, class=levs)
     # resort so we have same order in threshold and p
     threshold = threshold[levs] 
-    resp = sapply(1:nrow(p), function(i) vote.max.val(p[i,]/threshold, levs))
-    resp = factor(resp, levels=levs)
+    resp = factor(max.col(t(t(p) / threshold)), labels=levs)
     pred@df$response = resp
     pred@threshold = threshold
     return(pred)
   } 
 )
 
-#' @rdname setThreshold 
-setMethod(
-  f = "setThreshold",
-  
-  signature = signature(
-    pred = "matrix", 
-    threshold = "numeric" 
-  ),
-  
-  def = function(pred, threshold) {
-    levs = colnames(pred)
-    if (length(threshold) != length(levs) || !all.els.named(threshold) || any(names(threshold) != levs))
-      stop("Threshold must have same names as columns of prob matrix!")
-    # resort so we have same order in threshold and p
-    threshold = threshold[levs] 
-    resp = sapply(1:nrow(pred), function(i) vote.max.val(pred[i,]/threshold, levs))
-    factor(resp, levels=levs)
-  } 
-)
+
+
 
 
