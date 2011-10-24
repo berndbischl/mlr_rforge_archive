b0b51ebd0c31c982d567ec137e3572e94db1003a
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-24 19:43:16 +0000

    - remove object - remove getters - remove to.string - remove nonseq / seq resampling

--- src/mlr/Aggregation.R
+++ src/mlr/Aggregation.R
@@ -1,5 +1,7 @@
 #' @include aaa.r
 roxygen()
+#' @include empty.r
+roxygen()
 
 #' A aggregation method reduces the performance values of the test (and possibly the training sets) to a single
 #' value. To see all possible, implemented aggregations look at \code{\link{aggregations}}.  
