e950f4d0a68fde69df1a0aaacc7c44a08a80743f
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-24 01:06:08 +0000

    small fixes

--- src/mlrTune/myspo/SPOControl.R
+++ src/mlrTune/myspo/SPOControl.R
@@ -119,7 +119,7 @@ makeSPOControl = function(y.name="y", minimize=TRUE,
   check.arg(resample.at, "integer")
   check.arg(resample.desc, "ResampleDesc")
   
-  if (is.numeric(resample.at) && as.integer(resample.at) == resample.at)
+  if (is.numeric(resample.at) && (length(resample.at) == 0 || as.integer(resample.at) == resample.at))
     resample.at = as.integer(resample.at)
   check.arg(resample.at, "integer")
   check.arg(resample.desc, "ResampleDesc")
