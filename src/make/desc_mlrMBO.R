6854d4ffc4ad08e9aa830b6b9ac12866ad7c54f6
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-30 05:08:44 +0000

    move mbo to new package

---
+++ src/make/desc_mlrMBO.R
@@ -0,0 +1,19 @@
+writeDesc <- function(desc.path, rev.nr) {
+  sink(file=desc.path)
+  cat(
+    "Package: mlrTune\n",
+    "Type: Package\n",
+    "Title: mlr: Modelbased Optimization.\n",
+    paste("Version: 0.1.", rev.nr, "\n", sep=""),
+    "Date: 2009-01-01\n",
+    "Author: Bernd Bischl\n",
+    "Maintainer: Bernd Bischl <bernd_bischl@gmx.net>\n",
+    "Description: no\n",
+    "License: GPL (>= 2)\n",
+    "LazyLoad: yes\n",
+    "Depends: R (>= 2.11.0), methods, ParamHelpers\n",
+    "Suggests: lhs\n",
+    sep=""
+  )
+  sink(NULL)
+}
