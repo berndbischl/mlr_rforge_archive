c81180f0e992d61e6b0d5e824ac15180354714e5
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-13 18:38:38 +0000

    remove s4

--- src/mlr/getConfMatrix.R
+++ src/mlr/getConfMatrix.R
@@ -1,33 +1,15 @@
 #' Calculates confusion matrix for (possibly resampled) prediction. 
 #' Rows indicate true classes, columns predicted classes.
-#' 
+#' @title Confusion matrix.
 #' @param pred [\code{\linkS4class{Prediction}}] \cr
 #'   Result of a prediction.
-#' @param relative [logical] \cr 
+#' @param relative [\code{logical(1)}]\cr 
 #' 	If TRUE rows are normalized to show relative frequencies.
-#' 
 #' @return A confusion matrix.
-#' 
-#' @exportMethod getConfMatrix
-#' @rdname getConfMatrix
-#' @seealso \code{\link[klaR]{errormatrix}}
-#' @title Confusion matrix.
-
-setGeneric(
-  name = "getConfMatrix",
-  def = function(pred, relative) {
-    if (missing(relative))
-      relative=FALSE
-    standardGeneric("getConfMatrix")
-  }
-)
-
 #' @export
-#' @rdname getConfMatrix
-setMethod(
-  f = "getConfMatrix",
-  signature = signature(pred="Prediction", relative="logical"),
-  def = function(pred, relative) {
-    return(errormatrix(pred@df$truth, pred@df$response, relative=relative))
-  }
-)
+#' @seealso \code{\link[klaR]{errormatrix}}
+getConfMatrix = function(pred, relative=FALSE) {
+  checkArg(pred, "Prediction")
+  checkArg(relative, "logical", len=1L, na.ok=FALSE)
+  errormatrix(pred@df$truth, pred@df$response, relative=relative)
+}
