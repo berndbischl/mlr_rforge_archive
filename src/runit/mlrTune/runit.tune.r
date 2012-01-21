565708de182ee01f0b5a5a639ac6b4612945715e
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-21 02:44:56 +0000

    mies tuning

--- src/runit/mlrTune/runit.tune.r
+++ src/runit/mlrTune/runit.tune.r
@@ -124,4 +124,19 @@ test.tune.mbo = function() {
   checkEquals(getOptPathLength(tr2@path), 5)
 } 
   
+test.tune.mies = function() {
+  res = makeResampleDesc("Holdout")
+  ps1 = makeParamSet(
+    makeNumericVectorParam("cutoff", length=2, lower=0.001, upper=1, trafo=function(x) as.numeric(x / sum(x))), 
+    makeIntegerParam("ntree", lower=10, upper=1000), 
+    makeLogicalParam("replace")
+  )
+  
+  ctrl = makeTuneControlMies(budget=20, lambda=5)
+  lrn = makeLearner("classif.randomForest")
+  tr1 = tune(lrn, binaryclass.task, res, par.set=ps1, control=ctrl)
+  checkEquals(getOptPathLength(tr1@path), 10)
+  checkEquals(dim(as.data.frame(tr1@path)), c(10, 3+1+2))
+} 
+  
 
