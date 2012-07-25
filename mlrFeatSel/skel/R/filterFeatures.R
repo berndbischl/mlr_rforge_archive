9fb4bf2f4e80a79ab19db0a88b1aa0c430449076
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-25 22:16:00 +0000

    s3 rewrite

--- mlrFeatSel/skel/R/filterFeatures.R
+++ mlrFeatSel/skel/R/filterFeatures.R
@@ -1,7 +1,8 @@
 #FIXME for which tasks do the filters work?
 
 #' Filter features by using a numerical importance criterion.
-#' 
+#' Calculates numerical mportance values for all features. 
+#' Thresholding of these values can be used to select \dQuote{useful} features.
 #' Look at package FSelector for details on the filter algorithms. 
 #' 
 #' @param task [\code{\link[mlr]{SupervisedTask}}]\cr 
@@ -12,18 +13,11 @@
 #'   \dQuote{gain.ratio}, \dQuote{symmetrical.uncertainty}, \dQuote{chi.squared}, 
 #'   \dQuote{random.forest.importance}, \dQuote{relief}, \dQuote{oneR}
 #'   Default is \dQuote{random.forest.importance}.
-#' @param threshold [\code{numeric(1)}]\cr
-#'   Only features whose importance value exceed this are selected.  
-#'   Default is 1.
-#' @return A list with the following entries:
-#' \describe{
-#'   \item{vars [\code{character}]}{Selected features.}
-#'   \item{vals [\code{numeric}]}{Importance values for all features used in filter.}
-#' }
+#' @return [\code{numeric}]. A named numeric vector that contains an importance value 
+#'   for each feature.
 #' @export
-filterFeatures = function(task, method="random.forest.importance", threshold=1) {
+filterFeatures = function(task, method="random.forest.importance") {
   requirePackages("FSelector", "filterFeatures")
-  checkArg(task, "SupervisedTask") 
   checkArg(task, "SupervisedTask") 
   checkArg(method, choices=c("linear.correlation", "rank.correlation", "information.gain", 
     "gain.ratio", "symmetrical.uncertainty", "chi.squared", "random.forest.importance", 
@@ -32,11 +26,10 @@ filterFeatures = function(task, method="random.forest.importance", threshold=1) 
   f = getTaskFormula(task)
   data = getTaskData(task)
   fun = get(method, envir=getNamespace("FSelector"))
-  x = fun(f, data)  
-  val = x[,1]
-  names(val) = rownames(x)
-  vars = names(which(val > threshold))
-  return(list(vars=vars, vals=val))
+  y = fun(f, data)  
+  vals = y[,1]
+  names(vals) = rownames(y)
+  return(vals)
 } 
  
 
