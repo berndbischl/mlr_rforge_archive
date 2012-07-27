edfc2e29fd23b329a97c737db1e27ec0335d7377
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-27 18:53:38 +0000

    s3 rewrite

--- mlrTune/skel/R/tuneMBO.R
+++ mlrTune/skel/R/tuneMBO.R
@@ -8,11 +8,14 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
   mbo.control$final.evals = 0L
   cx = identity
   
-  # FIXME: ... in mbo
+  # FIXME: use ... in mbo
   tff = function(x) tunerFitnFun(x, learner=learner, task=task, resampling=resampling, measures=measures, 
     par.set=par.set, ctrl=control, opt.path=opt.path, show.info=show.info, 
     log.fun=log.fun, trafo=FALSE, convertx=cx)    
   
   or = mbo(tff, par.set, des=NULL, learner=control$learner, control=mbo.control)
-  makeOptResult(learner, control, or$x, or$y, opt.path)
+  
+  # FIXME: check this this is really ok, that we dont trafo in mlrMBO
+  x = trafoVal(or$x, par.set)
+  makeOptResult(learner, control, x, or$y, opt.path)
 }
