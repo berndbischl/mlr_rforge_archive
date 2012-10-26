f0c9f52ae7a32e2d75170b67fb6d1a020f8a39ae
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-26 12:48:55 +0000

    add some missing install_dependencies for other packages

---
+++ mlrFeatSel/install_dependencies.R
@@ -0,0 +1,14 @@
+pkgs <- c("FSelector")
+
+# Determine missing packages 
+installed.pkgs <- rownames(installed.packages())
+missing.pkgs <- setdiff(pkgs, installed.pkgs)
+
+if(length(missing.pkgs) > 0) {
+  cat("Installing the following packages:\n")
+  print(missing.pkgs)
+  install.packages(missing.pkgs, dep = TRUE, repos="http://cran.at.r-project.org")
+} else {
+  cat("All packages installed!\n")
+}
+
