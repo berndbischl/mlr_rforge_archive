816ddf87d41d48ab59846047d85c5338c1c75b7d
pascal <pascal@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-01 19:15:00 +0000

    mlrFeatSel: updated selectFeaturesGA.R and added crossover.R and mutateBits.R

--- mlrFeatSel/skel/R/mutateBits.R
+++ mlrFeatSel/skel/R/mutateBits.R
@@ -1,4 +1,4 @@
-#' Bit Mutation
+#' mutateBits
 #' 
 #' Takes a bit string and flips its elements based on a given mutation rate. 
 #' 
@@ -7,26 +7,26 @@
 #' @param rate [\code{numeric(1)}]\cr   
 #'   A number representing the probability of flipping an element of the bit string. Hence it 
 #'   should be between 0 and 1. Default is \code{1 / length(x)}.
-#' @return [\code{\link{BitMutation}}].
-#' @name BitMutation
-#' @rdname BitMutation
-#' @aliases BitMutation
+#' @return [\code{\link{mutateBits}}].
+#' @name mutateBits
+#' @rdname mutateBits
+#' @aliases mutateBits
 #' @examples
 #' x <- c(TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, TRUE)
-#' flip <- BitMutation(x, 1)
+#' flip <- mutateBits(x, 1)
 #' table(x, flip)
 #' ## All the labels are flipped.
 #' 
-#' flip2 <- BitMutation(x)
+#' flip2 <- mutateBits(x)
 #' table(x, flip2)
 #' ## The elements of x are flipped with a probability of 1/length(x).
 #' 
-#' flip3 <- BitMutation(x, rate = 0.05)
+#' flip3 <- mutateBits(x, rate = 0.05)
 #' table(x, flip3)
 #' ## The elements of x are flipped with a probability of 5%.
 NULL
 
-BitMutation = function(x, rate = 1/length(x)) {
+mutateBits = function(x, rate = 1/length(x)) {
   n = length(x)
   flip = rbinom(n, 1, rate)
   x = (x + flip) %% 2
--- mlrFeatSel/skel/R/selectFeaturesGA.R
+++ mlrFeatSel/skel/R/selectFeaturesGA.R
@@ -1,34 +1,51 @@
-# FIXME use max.features
-
-## control should be a list including init, size, rate and maxit or threshold:
-## init = number of samples for the initial population
-## size = number of samples to be generated in each GA-step
-## rate = mutation rate, i.e. probability of flipping a feature
-## maxit = maximum number of iterations
-## threshold = value for the fitness function
+#' selectFeaturesGA
+#' 
+#' A genetic algorithm for feature selection. Note that this is a (mu, lambda)-algorithm, i.e. it iteratively selects
+#' the lambda fittest elements out of a population consisting of mu parents.
+#' 
+#' @param control [\code{control}]\cr 
+#'   A control object, which includes the maximal number of iterations (\dQuote{maxit}), the rates for mutation
+#'   (\dQuote{mutation}) and crossover combinations (\dQuote{crossoverRate}, i.e. the probability of choosing an element
+#'   from the first parent), the size of the parent population (\dQuote{mu}) and the number of childrens to be selected
+#'   (\dQuote{lambda}).
+#' @return [\code{\link{selectFeaturesGA}}].
+#' @name selectFeaturesGA
+#' @rdname selectFeaturesGA
+#' @aliases selectFeaturesGA
+NULL
 
 selectFeaturesGA = function(learner, task, resampling, measures, bit.names, bits.to.features, control, opt.path, show.info) {
   fit = mlr:::measureAggrName(measures[[1]])
-  states = lapply(1:control$init, 
-	  function(i) rbinom(length(getTaskFeatureNames(task)), 1, control$rate))
+  states = lapply(1:control$mu, 
+	  function(i) rbinom(length(getTaskFeatureNames(task)), 1, 0.5))
   evalOptimizationStates(learner, task, resampling, measures, 
-	  bits.to.features, control, opt.path, show.info, states, 0L, as.integer(NA))
+	  bits.to.features, control, opt.path, show.info, states, 0L, as.integer(NA))  
+  
+  if("mutationRate" %in% names(control)) {
+    if(!("crossoverRate" %in% names(control))) {
+      control$crossoverRate = 1
+    }
+  } else {
+    control$mutationRate = 0
+  }
+  
   for(i in 1:control$maxit) {
-	  # parents = select all from OP that are alive
-		# for j in 1:lambda
-	  #   select 2 parents randomly
-	  #   CX + mutate --> kid
-	  # eval all kids  
-	  # select mu best from parent, kids; all others are set EOL = i
-		
-    index = rank(as.data.frame(opt.path)[[fit]], ties.method = "random")[1:control$size]
-    e = getOptPathEl(opt.path, index)
-    states = lapply(1:control$size, function(i) as.integer(BitMutation(as.data.frame(e$x)[i,], control$rate)))
+    parents = as.data.frame(getOptPathEl(opt.path, which(is.na(as.data.frame(opt.path)[,"eol"])))$x)
+    kids = lapply(1:control$lambda, function(z) 
+          generateKids(parents = parents[sample(1:nrow(parents), 2),], 
+                       crossoverRate = control$crossoverRate, mutationRate = control$mutationRate))
     evalOptimizationStates(learner, task, resampling, measures, 
-		  bits.to.features, control, opt.path, show.info, states, i, as.integer(NA))
+                           bits.to.features, control, opt.path, show.info, states = kids, i, as.integer(NA))
+    index = order(as.data.frame(opt.path)[[fit]])[1:control$mu]
+    setOptPathElEOL(opt.path, setdiff(which(is.na(as.data.frame(opt.path)[,"eol"])), index), i)
   }
   i = getOptPathBestIndex(opt.path, mlr:::measureAggrName(measures[[1]]), ties="random")
   e = getOptPathEl(opt.path, i)
 	makeFeatSelResult(learner, control, names(e$x)[e$x == 1], e$y, opt.path)
 }
 
+
+generateKids = function(parents, crossoverRate, mutationRate){
+  kid = crossover(as.integer(parents[1,]), as.integer(parents[2,]), crossoverRate)
+  return(mutateBits(kid, mutationRate))
+}
