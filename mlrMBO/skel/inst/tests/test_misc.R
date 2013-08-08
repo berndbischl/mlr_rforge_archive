7548aab8f58d896b01ed24c6d8edbfa440c154a3
mllg <mllg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-08 14:01:14 +0000

    small test

---
+++ mlrMBO/skel/inst/tests/test_misc.R
@@ -0,0 +1,18 @@
+context("simple optims")
+
+test_that("simple optims", {
+  if(isExpensiveExampleOk()) {
+    fit = function(x) {
+      -(x$x1 - is.null(x$x2) * 30 + 5 * (x$x3 == "a") + runif(1, 1, 5))
+    }
+
+    easy = makeParamSet(
+      makeNumericParam("x1", lower = 0, upper = 100),
+      makeNumericParam("x2", lower = 0, upper = 100, requires = quote(x1 > 50)),
+      makeDiscreteParam("x3", values = c("a", "blasdfkjaslkdjaslkdjflkjsdafj")))
+
+    surrogate = makeLearner("regr.rpart")
+    opt = mbo(fit, easy, learner = surrogate, control = makeMBOControl(seq.design.points=10))
+    expect_true(opt$x$x1 > 85 && opt$x$x2 < 50 && opt$x$x3 == "a")
+  }
+})
