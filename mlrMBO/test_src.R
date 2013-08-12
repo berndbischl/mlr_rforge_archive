08f9d113540ac883d6769f9bba06f07e00a73f8d
berndbischl <berndbischl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-12 16:00:58 +0000

    mlrMBO test code

--- mlrMBO/test_src.R
+++ mlrMBO/test_src.R
@@ -7,22 +7,3 @@ configureMlr(show.learner.output=FALSE, on.learner.error="stop")
 
 set.seed(2)
 
-obj = function(x) { sum(x^2) }
-
-ps = makeParamSet(
-  makeNumericVectorParam("x", len=2, lower=-15, upper=15)
-)
-
-#surrogate = makeLearner("regr.randomForest", ntree = 5)
-surrogate = makeLearner("regr.km", predict.type="se")
-
-ctrl = makeMBOControl(seq.loops = 2, init.design.points = 12, 
-  final.point = "best.predicted",
-  #seq.design.points = 50,
-  infill.crit = "mean",
-  infill.opt = "cmaes",
-  cmaes.control = list(maxit = 5)                      
-)
-res = mbo(makeMBOFunction(obj), ps, des=NULL, surrogate, ctrl)
-print(res)
-
