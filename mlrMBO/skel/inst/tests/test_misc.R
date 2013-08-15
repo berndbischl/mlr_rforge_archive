23d3bcb9c53daeb872bfd9d6bdf494ef5755ce78
mllg <mllg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-15 10:13:34 +0000

    complex param set test

--- mlrMBO/skel/inst/tests/test_misc.R
+++ mlrMBO/skel/inst/tests/test_misc.R
@@ -16,3 +16,53 @@ test_that("simple optims", {
     expect_true(opt$x$x1 > 85 && opt$x$x2 < 50 && opt$x$x3 == "a")
   }
 })
+
+test_that("complex paramset" , {
+  if(isExpensiveExampleOk()) {
+    ps = makeParamSet(
+      makeLogicalParam("use.clinical"),
+      makeDiscreteParam("filter", values = c("none", "var", "uni", "kratz", "top21", "fmrmr", "pamr")),
+      makeDiscreteParam("model", values = c("coxboost", "glmnet", "rfsrc", "penalized", "rpart")),
+
+      makeLogicalParam("filter.use.clinical",
+        requires = quote(filter == "uni" && isTRUE(use.clinical))),
+      makeNumericParam("filter.perc", lower=0, upper=1/3,
+        requires = quote(filter %in% c("var", "uni", "fmrmr"))),
+      makeDiscreteParam("filter.fmrmr.combine", values = c("difference", "quotient"),
+        requires = quote(filter == "fmrmr")),
+      makeDiscreteParam("filter.fmrmr.relevance", values = c("cindex"),
+        requires = quote(filter == "fmrmr")),
+      makeDiscreteParam("filter.fmrmr.redundance", values = c("mi", "pearson"),
+        requires = quote(filter == "fmrmr")),
+      makeIntegerParam("filter.pamr.ngroup.survival", lower = 2L, upper = 3L,
+        requires = quote(filter == "pamr")),
+
+      makeNumericParam("coxboost.stepsize.factor", lower=0.1, upper=2,
+        requires = quote(model == "coxboost")),
+      makeNumericParam("coxboost.penalty", lower = 0.1, upper = 300,
+        requires = quote(model == "coxboost")),
+      makeDiscreteParam("penalized.penalty", values = c("L1", "L2"),
+                        requires = quote(model == "penalized")),
+      makeNumericParam("glmnet.alpha", lower = 0, upper = 1,
+        requires = quote(model == "glmnet")),
+      makeDiscreteParam("rfsrc.splitrule", values = c("logrank", "logrankscore"),
+        requires = quote(model == "rfsrc")),
+      makeIntegerParam("rfsrc.nodesize", lower = 1L, upper = 10L,
+        requires = quote(model == "rfsrc")),
+      makeIntegerParam("rfsrc.ntree", lower = 50L, upper = 2000L,
+        requires = quote(model == "rfsrc")),
+      makeIntegerParam("rpart.minsplit", lower=1L, upper=30L,
+        requires = quote(model == "rpart")),
+      makeIntegerParam("rpart.minbucket", lower=1L, upper=15L,
+        requires = quote(model == "rpart")),
+      makeNumericParam("rpart.cp", lower=0.001, upper=0.1,
+        requires = quote(model == "rpart"))
+    )
+
+    fit = function(x) {
+      sum(sapply(x, nchar))
+    }
+
+    surrogate = makeLearner("regr.rpart")
+    opt = mbo(fit, ps, learner = surrogate, control = makeMBOControl(random.n.points=10))
+})
