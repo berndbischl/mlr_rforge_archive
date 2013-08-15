82fa7b801f3f87a0ebad4fced72a79a777262884
mllg <mllg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-15 13:46:53 +0000

    included refactorNAs in makeMBOTask

--- mlrMBO/skel/R/makeMBOTask.R
+++ mlrMBO/skel/R/makeMBOTask.R
@@ -12,6 +12,7 @@ makeMBOTask = function(design, y.name, control) {
     design = as.data.frame(lapply(design, function(x) if(is.integer(x)) as.numeric(x) else x))
   if (any(sapply(design, is.logical)))
     design = as.data.frame(lapply(design, function(x) if(is.logical(x)) as.factor(x) else x))
+  design = refactorNAs(design)
   #if (control$rank.trafo)
   #  design[,y.name] = rank(design[,y.name])
   makeRegrTask(target=y.name, data=design)
